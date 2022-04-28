372745788454bd2252b5f7549c46376fef084742
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2022-04-28 16:34:11 -0400

    winsys: add WINSYS_HANDLE_TYPE_UNBACKED
    for use with lavapipe imports
    
    Reviewed-by: Dave Airlie <airlied@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27805>

--- src/gallium/include/frontend/winsys_handle.h
+++ src/gallium/include/frontend/winsys_handle.h
@@ -18,6 +18,7 @@ extern "C" {
 #define WINSYS_HANDLE_TYPE_SHMID   3
 #define WINSYS_HANDLE_TYPE_D3D12_RES 4
 #define WINSYS_HANDLE_TYPE_WIN32_NAME 5
+#define WINSYS_HANDLE_TYPE_UNBACKED 5
 
 /**
  * For use with pipe_screen::{resource_from_handle|resource_get_handle}.

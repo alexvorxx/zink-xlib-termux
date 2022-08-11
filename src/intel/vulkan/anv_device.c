8d8d3666c6a1f85712527cead25691e58234c4b5
Rohan Garg <rohan.garg@intel.com>
2022-08-11 16:19:25 +0200

    intel/brw: Advertise fp64 atomic add's when we have 64 bit float support and a LSC
    Rework:
     * Lionel: Simplify to just checking ver >= 20.
    
    Signed-off-by: Rohan Garg <rohan.garg@intel.com>
    Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28283>

--- src/intel/vulkan/anv_device.c
+++ src/intel/vulkan/anv_device.c
@@ -746,7 +746,7 @@ get_features(const struct anv_physical_device *pdevice,
       .shaderBufferFloat32AtomicAdd =  pdevice->info.has_lsc,
       .shaderBufferFloat64Atomics =
          pdevice->info.has_64bit_float && pdevice->info.has_lsc,
-      .shaderBufferFloat64AtomicAdd =  false,
+      .shaderBufferFloat64AtomicAdd =  pdevice->info.ver >= 20,
       .shaderSharedFloat32Atomics =    true,
       .shaderSharedFloat32AtomicAdd =  false,
       .shaderSharedFloat64Atomics =    false,

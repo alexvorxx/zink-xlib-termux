767ea18517a05f9d4ecc4fdca0217cd0faf89b88
Rhys Perry <pendingchaos02@gmail.com>
2023-10-19 14:58:00 +0100

    glsl: always lower non-TCS outputs to temporaries
    It seems only radeonsi and v3d sets
    CAN_READ_OUTPUTS/SupportsReadingOutputs, and v3d has
    lower_all_io_to_temps=true. It looks like radeonsi basically lowers the
    outputs to temporaries in the backend.
    
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
    Reviewed-by: Timothy Arceri <tarceri@itsqueeze.com>
    Reviewed-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25590>

--- src/compiler/glsl/gl_nir_linker.c
+++ src/compiler/glsl/gl_nir_linker.c
@@ -1249,7 +1249,8 @@ preprocess_shader(const struct gl_constants *consts,
       NIR_PASS(_, nir, nir_lower_io_to_temporaries,
                  nir_shader_get_entrypoint(nir),
                  true, true);
-   } else if (nir->info.stage == MESA_SHADER_FRAGMENT ||
+   } else if (nir->info.stage == MESA_SHADER_TESS_EVAL ||
+              nir->info.stage == MESA_SHADER_FRAGMENT ||
               !consts->SupportsReadingOutputs) {
       NIR_PASS(_, nir, nir_lower_io_to_temporaries,
                  nir_shader_get_entrypoint(nir),

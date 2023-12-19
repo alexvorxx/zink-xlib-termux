e2b09b855959c023238921cb9b38c13656346c55
Jordan Justen <jordan.l.justen@intel.com>
2023-12-19 15:20:34 -0800

    isl: Handle ARL in isl_drm_modifier_get_score()
    Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
    Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27352>

--- src/intel/isl/isl_drm.c
+++ src/intel/isl/isl_drm.c
@@ -263,7 +263,7 @@ isl_drm_modifier_get_score(const struct intel_device_info *devinfo,
 
       return 5;
    case I915_FORMAT_MOD_4_TILED_MTL_RC_CCS:
-      if (!intel_device_info_is_mtl(devinfo))
+      if (!intel_device_info_is_mtl_or_arl(devinfo))
          return 0;
 
       if (INTEL_DEBUG(DEBUG_NO_CCS))
@@ -271,7 +271,7 @@ isl_drm_modifier_get_score(const struct intel_device_info *devinfo,
 
       return 4;
    case I915_FORMAT_MOD_4_TILED_MTL_RC_CCS_CC:
-      if (!intel_device_info_is_mtl(devinfo))
+      if (!intel_device_info_is_mtl_or_arl(devinfo))
          return 0;
 
       if (INTEL_DEBUG(DEBUG_NO_CCS) || INTEL_DEBUG(DEBUG_NO_FAST_CLEAR))

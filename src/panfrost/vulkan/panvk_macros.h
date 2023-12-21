a58268f6a9d882c10a43bc57068b1193c1a1a970
Boris Brezillon <boris.brezillon@collabora.com>
2023-12-21 12:06:25 +0100

    panvk: Add a panvk_arch_dispatch_ret() variant
    So we can dispatch calls to per-arch functions that return a value.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
    Acked-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
    Reviewed-by: Mary Guillemard <mary.guillemard@collabora.com>
    Reviewed-by: Rebecca Mckeever <rebecca.mckeever@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28170>

--- src/panfrost/vulkan/panvk_macros.h
+++ src/panfrost/vulkan/panvk_macros.h
@@ -34,6 +34,20 @@
       }                                                                        \
    } while (0)
 
+#define panvk_arch_dispatch_ret(arch, name, ret, ...)                          \
+   do {                                                                        \
+      switch (arch) {                                                          \
+      case 6:                                                                  \
+         ret = panvk_arch_name(name, v6)(__VA_ARGS__);                         \
+         break;                                                                \
+      case 7:                                                                  \
+         ret = panvk_arch_name(name, v7)(__VA_ARGS__);                         \
+         break;                                                                \
+      default:                                                                 \
+         unreachable("Unsupported architecture");                              \
+      }                                                                        \
+   } while (0)
+
 #ifdef PAN_ARCH
 #if PAN_ARCH == 6
 #define panvk_per_arch(name) panvk_arch_name(name, v6)

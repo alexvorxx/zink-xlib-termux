e28ff818695ca953b31ec15d1f371b64a4ff9e3d
Christian Gmeiner <cgmeiner@igalia.com>
2024-01-18 16:13:39 +0100

    meson: Add pest rust dependencies
    Including its dependency ucd-trie.
    
    Signed-off-by: Christian Gmeiner <cgmeiner@igalia.com>
    Reviewed-by: @LingMan
    Reviewed-by: Eric Engestrom <eric@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28869>

--- .gitlab-ci/meson/build.sh
+++ .gitlab-ci/meson/build.sh
@@ -104,7 +104,7 @@ rm -rf _build
 meson setup _build \
       --native-file=native.file \
       --wrap-mode=nofallback \
-      --force-fallback-for perfetto,syn,paste \
+      --force-fallback-for perfetto,syn,paste,pest,pest_derive,pest_generator,pest_meta \
       ${CROSS+--cross "$CROSS_FILE"} \
       -D prefix=$PWD/install \
       -D libdir=lib \

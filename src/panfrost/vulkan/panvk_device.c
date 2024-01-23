96504d51a214d26cd35beedbc0714f91d64a6b2d
Constantine Shablia <constantine.shablya@collabora.com>
2024-01-23 10:32:49 +0200

    panvk: advertise bufferDeviceAddress
    Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27209>

--- src/panfrost/vulkan/panvk_device.c
+++ src/panfrost/vulkan/panvk_device.c
@@ -245,7 +245,7 @@ panvk_get_features(const struct panvk_physical_device *device,
       .separateDepthStencilLayouts = false,
       .hostQueryReset = false,
       .timelineSemaphore = false,
-      .bufferDeviceAddress = false,
+      .bufferDeviceAddress = true,
       .bufferDeviceAddressCaptureReplay = false,
       .bufferDeviceAddressMultiDevice = false,
       .vulkanMemoryModel = false,
@@ -1389,7 +1389,7 @@ panvk_InvalidateMappedMemoryRanges(VkDevice _device, uint32_t memoryRangeCount,
 }
 
 VkDeviceAddress
-panvk_GetBufferDeviceAddress(VkDevice _device, VkBufferDeviceAddressInfo *pInfo)
+panvk_GetBufferDeviceAddress(VkDevice _device, const VkBufferDeviceAddressInfo *pInfo)
 {
    VK_FROM_HANDLE(panvk_buffer, buffer, pInfo->buffer);
 

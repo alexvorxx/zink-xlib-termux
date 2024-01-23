Grima04/mesa-turnip-kgsl

commit b1619109ca91f5b9b4f29d75479241ec82c31549

nir/lower_non_uniform: remove non_uniform flags after lowering
Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/5201>

src/compiler/nir/nir_lower_non_uniform_access.c
@@ -142,6 +142,9 @@ lower_non_uniform_tex_access(nir_builder *b, nir_tex_instr *tex)
    nir_builder_instr_insert(b, &tex->instr);
    nir_jump(b, nir_jump_break);
 
+   tex->texture_non_uniform = false;
+   tex->sampler_non_uniform = false;
+
    return true;
 }
 
@@ -169,6 +172,8 @@ lower_non_uniform_access_intrin(nir_builder *b, nir_intrinsic_instr *intrin,
 
    nir_builder_instr_insert(b, &intrin->instr);
    nir_jump(b, nir_jump_break);
+
+   nir_intrinsic_set_access(intrin, nir_intrinsic_access(intrin) & ~ACCESS_NON_UNIFORM);
 
    return true;
 }

alexvorxx/zink-xlib-termux

commit 015b0d678f8027008a5ae4cc3bb066a859839c8a

nir/lower_non_uniform: set non_uniform=false when lowering is not needed
Fixes RADV compilation of a Doom Eternal pipeline with
PIPELINE_CREATE_DISABLE_OPTIMIZATION_BIT, because
nir_opt_non_uniform_access was skipped and later passes don't expect
non-uniform access.

Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
Reviewed-by: Faith Ekstrand <faith.ekstrand@collabora.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: b1619109ca91 ("nir/lower_non_uniform: remove non_uniform flags after lowering")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27192>

src/compiler/nir/nir_lower_non_uniform_access.c
@@ -136,8 +136,12 @@ lower_non_uniform_tex_access(const nir_lower_non_uniform_access_options *options
          num_handles++;
    }
 
-   if (num_handles == 0)
+   if (num_handles == 0) {
+      /* nu_handle_init() returned false because the handles are uniform. */
+      tex->texture_non_uniform = false;
+      tex->sampler_non_uniform = false;
       return false;
+   }
 
    b->cursor = nir_instr_remove(&tex->instr);
 
@@ -177,8 +181,10 @@ lower_non_uniform_access_intrin(const nir_lower_non_uniform_access_options *opti
       return false;
 
    struct nu_handle handle;
-   if (!nu_handle_init(&handle, &intrin->src[handle_src]))
+   if (!nu_handle_init(&handle, &intrin->src[handle_src])) {
+      nir_intrinsic_set_access(intrin, nir_intrinsic_access(intrin) & ~ACCESS_NON_UNIFORM);
       return false;
+   }
 
    b->cursor = nir_instr_remove(&intrin->instr);
 
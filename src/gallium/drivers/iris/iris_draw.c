8ff26271a7ab0b8beed72e1c265946c386ea6d0b
Caio Oliveira <caio.oliveira@intel.com>
2024-01-26 07:11:34 -0800

    iris: Remove unused brw_* includes
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27476>

--- src/gallium/drivers/iris/iris_draw.c
+++ src/gallium/drivers/iris/iris_draw.c
@@ -37,7 +37,6 @@
 #include "util/u_transfer.h"
 #include "util/u_upload_mgr.h"
 #include "intel/compiler/brw_compiler.h"
-#include "intel/compiler/brw_eu_defines.h"
 #include "compiler/shader_info.h"
 #include "iris_context.h"
 #include "iris_defines.h"

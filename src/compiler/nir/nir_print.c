e3c2dc2324a7a360a279d44562fcc78cdecfb6d9
Konstantin Seurer <konstantin.seurer@gmail.com>
2024-01-27 12:48:54 +0100

    nir/print: Rename workgroup-size to workgroup_size
    Every other field uses _ instead of -.
    
    Reviewed-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27318>

--- src/compiler/nir/nir_print.c
+++ src/compiler/nir/nir_print.c
@@ -2396,7 +2396,7 @@ print_shader_info(const struct shader_info *info, FILE *fp)
    fprintf(fp, "internal: %s\n", info->internal ? "true" : "false");
 
    if (gl_shader_stage_uses_workgroup(info->stage)) {
-      fprintf(fp, "workgroup-size: %u, %u, %u%s\n",
+      fprintf(fp, "workgroup_size: %u, %u, %u%s\n",
               info->workgroup_size[0],
               info->workgroup_size[1],
               info->workgroup_size[2],

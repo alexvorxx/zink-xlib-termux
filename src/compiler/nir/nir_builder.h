0ab3b3c641fa67f1f538c3b9e21f6a1dcb802081
Gert Wollny <gert.wollny@collabora.com>
2024-01-29 21:35:24 +0100

    nir/builder: Fix compilation with gcc-13 when tsan is enabled
    ../src/compiler/nir/nir_builder.h: In function ‘nir_build_deref_follower’:
    ../src/compiler/nir/nir_builder.h:1607:1: error: control reaches end of non-void function [-Werror=return-type]
     1607 | }
    
    Fixes: 4a4e1757381c65653fd6a81cf4e969ddff926ed5
        nir: Support deref instructions in lower_var_copies
    
    Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27345>

--- src/compiler/nir/nir_builder.h
+++ src/compiler/nir/nir_builder.h
@@ -1604,6 +1604,7 @@ nir_build_deref_follower(nir_builder *b, nir_deref_instr *parent,
    default:
       unreachable("Invalid deref instruction type");
    }
+   return NULL;
 }
 
 static inline nir_def *

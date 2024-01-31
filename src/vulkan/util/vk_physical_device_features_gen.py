aerisarn/mesa-uwp

commit a7141a6f8a1112575c73281428fda0f8a798c857

vulkan: Allow beta extensions for physical device features
Reviewed-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24512>

src/vulkan/util/vk_physical_device_features_gen.py
@@ -366,7 +366,8 @@ def get_feature_structs(doc, api, beta):
             continue
 
         # Skip extensions with a define for now
-        if required[_type.attrib['name']].guard is not None:
+        guard = required[_type.attrib['name']].guard
+        if guard is not None and (guard != "VK_ENABLE_BETA_EXTENSIONS" or not beta):
             continue
 
         # find Vulkan structure type

alexvorxx/zink-xlib-termux

commit 794ec7f0a1c208143cbe4e9aa5a09b3d811824b3

vk/util: fix 'beta' check for physical device features
`--beta` is a string, not a bool (although really it should be, but that's a bigger change).

Fixes: a7141a6f8a1112575c73 ("vulkan: Allow beta extensions for physical device features")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27394>

src/vulkan/util/vk_physical_device_features_gen.py
@@ -367,7 +367,7 @@ def get_feature_structs(doc, api, beta):
 
         # Skip extensions with a define for now
         guard = required[_type.attrib['name']].guard
-        if guard is not None and (guard != "VK_ENABLE_BETA_EXTENSIONS" or not beta):
+        if guard is not None and (guard != "VK_ENABLE_BETA_EXTENSIONS" or beta != "true"):
             continue
 
         # find Vulkan structure type
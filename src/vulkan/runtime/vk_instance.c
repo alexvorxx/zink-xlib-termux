a367cd49314a993d09168e790d3090a2303a48d9
Yonggang Luo <luoyonggang@gmail.com>
2024-02-05 06:13:19 +0800

    Revert "meson/vulkan/util: allow venus to drop compiler deps"
    This reverts commit 3923d43908fa3ac2c8535e33277229038333fe8f.
    
    Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>
    Reviewed-by: Gurchetan Singh <gurchetansingh@google.com>
    Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26574>

--- src/vulkan/runtime/vk_instance.c
+++ src/vulkan/runtime/vk_instance.c
@@ -34,6 +34,8 @@
 #include "vk_debug_utils.h"
 #include "vk_physical_device.h"
 
+#include "compiler/glsl_types.h"
+
 #define VERSION_IS_1_0(version) \
    (VK_API_VERSION_MAJOR(version) == 1 && VK_API_VERSION_MINOR(version) == 0)
 
@@ -199,7 +201,7 @@ vk_instance_init(struct vk_instance *instance,
    instance->trace_frame = (uint32_t)debug_get_num_option("MESA_VK_TRACE_FRAME", 0xFFFFFFFF);
    instance->trace_trigger_file = secure_getenv("MESA_VK_TRACE_TRIGGER");
 
-   vk_compiler_cache_init();
+   glsl_type_singleton_init_or_ref();
 
    return VK_SUCCESS;
 }
@@ -219,7 +221,7 @@ vk_instance_finish(struct vk_instance *instance)
 {
    destroy_physical_devices(instance);
 
-   vk_compiler_cache_finish();
+   glsl_type_singleton_decref();
    if (unlikely(!list_is_empty(&instance->debug_utils.callbacks))) {
       list_for_each_entry_safe(struct vk_debug_utils_messenger, messenger,
                                &instance->debug_utils.callbacks, link) {

5879684a469e78219a9bdbb378df86d370960156
Jesse Natalie <jenatali@microsoft.com>
2024-02-07 10:56:00 -0800

    wgl: Add HDR pixel formats
    Reviewed-by: Sil Vilerino <sivileri@microsoft.com>
    Reviewed-by: Jose Fonseca <jose.fonseca@broadcom.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27512>

--- src/gallium/frontends/wgl/stw_pixelformat.c
+++ src/gallium/frontends/wgl/stw_pixelformat.c
@@ -76,18 +76,18 @@ struct stw_pf_depth_info
 static const struct stw_pf_color_info
 stw_pf_color[] = {
    /* no-alpha */
-   { PIPE_FORMAT_B8G8R8X8_UNORM,    { 8,  8,  8,  0}, {16,  8,  0,  0} },
-   { PIPE_FORMAT_X8R8G8B8_UNORM,    { 8,  8,  8,  0}, { 8, 16, 24,  0} },
+   { PIPE_FORMAT_B8G8R8X8_UNORM,     { 8,  8,  8,  0}, {16,  8,  0,  0} },
+   { PIPE_FORMAT_X8R8G8B8_UNORM,     { 8,  8,  8,  0}, { 8, 16, 24,  0} },
    /* alpha */
-   { PIPE_FORMAT_B8G8R8A8_UNORM,    { 8,  8,  8,  8}, {16,  8,  0, 24} },
-   { PIPE_FORMAT_A8R8G8B8_UNORM,    { 8,  8,  8,  8}, { 8, 16, 24,  0} },
+   { PIPE_FORMAT_B8G8R8A8_UNORM,     { 8,  8,  8,  8}, {16,  8,  0, 24} },
+   { PIPE_FORMAT_A8R8G8B8_UNORM,     { 8,  8,  8,  8}, { 8, 16, 24,  0} },
    /* shallow bit depths */
-   { PIPE_FORMAT_B5G6R5_UNORM,      { 5,  6,  5,  0}, {11,  5,  0,  0} },
-#if 0
-   { PIPE_FORMAT_R10G10B10A2_UNORM, {10, 10, 10,  2}, { 0, 10, 20, 30} },
-#endif
-   { PIPE_FORMAT_B5G5R5A1_UNORM,    { 5,  5,  5,  1}, {10,  5,  0, 15} },
-   { PIPE_FORMAT_B4G4R4A4_UNORM,    { 4,  4,  4,  4}, {16,  4,  0, 12} }
+   { PIPE_FORMAT_B5G6R5_UNORM,       { 5,  6,  5,  0}, {11,  5,  0,  0} },
+   { PIPE_FORMAT_B5G5R5A1_UNORM,     { 5,  5,  5,  1}, {10,  5,  0, 15} },
+   { PIPE_FORMAT_B4G4R4A4_UNORM,     { 4,  4,  4,  4}, {16,  4,  0, 12} },
+   /* HDR bit depths */
+   { PIPE_FORMAT_R16G16B16A16_FLOAT, {16, 16, 16, 16}, { 0, 16, 32, 48 }},
+   { PIPE_FORMAT_R10G10B10A2_UNORM,  {10, 10, 10,  2}, { 0, 10, 20, 30} },
 };
 
 static const struct stw_pf_color_info

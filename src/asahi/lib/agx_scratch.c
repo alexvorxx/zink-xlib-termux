a3f647fde104ca5fdfabb2694de78bbde56093f3
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2024-02-09 12:34:50 -0400

    asahi: precompile helper program
    1. avoids cluttering shaders,internal print
    2. reduces screen create overhead. this cuts average "glxinfo" runtime by about
       25%, and seems to shave maybe 0.5% off the CTS job.
    3. enforces helper program builds successfully as a build-time assertion
    4. reduces the libagx generated blob for now, since g13 binaries are more
       compact than serialized NIR
    
    Closes https://gitlab.freedesktop.org/asahi/mesa/-/issues/37
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27616>

--- src/asahi/lib/agx_scratch.c
+++ src/asahi/lib/agx_scratch.c
@@ -32,30 +32,11 @@ struct spill_size {
 struct agx_bo *
 agx_build_helper(struct agx_device *dev)
 {
-   struct agx_bo *bo;
-   struct util_dynarray binary;
-
-   util_dynarray_init(&binary, NULL);
-
-   nir_builder b = nir_builder_init_simple_shader(
-      MESA_SHADER_COMPUTE, &agx_nir_options, "Helper shader");
-
-   libagx_helper(&b);
-
-   UNUSED struct agx_uncompiled_shader_info info;
-   UNUSED struct agx_shader_info compiled_info;
-   struct agx_shader_key key = {.libagx = dev->libagx};
-
-   agx_preprocess_nir(b.shader, dev->libagx, false, &info);
-   agx_compile_shader_nir(b.shader, &key, NULL, &binary, &compiled_info);
-
-   bo = agx_bo_create(dev, binary.size,
-                      AGX_BO_READONLY | AGX_BO_EXEC | AGX_BO_LOW_VA,
-                      "Helper shader");
+   struct agx_bo *bo = agx_bo_create(
+      dev, sizeof(libagx_g13_helper),
+      AGX_BO_READONLY | AGX_BO_EXEC | AGX_BO_LOW_VA, "Helper shader");
    assert(bo);
-   memcpy(bo->ptr.cpu, binary.data, binary.size);
-   util_dynarray_fini(&binary);
-   ralloc_free(b.shader);
+   memcpy(bo->ptr.cpu, libagx_g13_helper, sizeof(libagx_g13_helper));
 
    if (dev->debug & AGX_DBG_SCRATCH)
       fprintf(stderr, "Helper: 0x%" PRIx64 "\n", bo->ptr.gpu);

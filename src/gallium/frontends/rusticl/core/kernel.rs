aerisarn/mesa-uwp

commit ea023ff5cddf91521436b6d3f24021ae35774997

rusticl/kernel: no need to reset the scratch size anymore
The scratch size gets assigned by nir_lower_vars_to_explicit_types. Now
that we only run it once on temp memory, there is no need to reset the
value.

Signed-off-by: Karol Herbst <kherbst@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27068>

src/gallium/frontends/rusticl/core/kernel.rs
@@ -509,7 +509,6 @@ fn lower_and_optimize_nir(
         !dev.samplers_as_deref(),
     );
 
-    nir.reset_scratch_size();
     nir_pass!(
         nir,
         nir_lower_vars_to_explicit_types,

src/gallium/frontends/rusticl/mesa/compiler/nir.rs
@@ -308,12 +308,6 @@ impl NirShader {
         unsafe { (*self.nir.as_ptr()).info.num_textures }
     }
 
-    pub fn reset_scratch_size(&mut self) {
-        unsafe {
-            (*self.nir.as_ptr()).scratch_size = 0;
-        }
-    }
-
     pub fn scratch_size(&self) -> u32 {
         unsafe { (*self.nir.as_ptr()).scratch_size }
     }

alexvorxx/zink-xlib-termux

commit 53df391d27c4d8410c148c490a03f72b39c20aa5

rusticl/kernel: recalculate scratch and shared memory after opts
Turns out we have to do it regardless as opts after explicit_types could
get rid of even more scratch and shared memory.

Fixes: ea023ff5cdd ("rusticl/kernel: no need to reset the scratch size anymore")
Signed-off-by: Karol Herbst <kherbst@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27634>

src/gallium/frontends/rusticl/core/kernel.rs
@@ -634,6 +634,24 @@ fn lower_and_optimize_nir(
     opt_nir(nir, dev, true);
     nir_pass!(nir, nir_lower_memcpy);
 
+    // we might have got rid of more function_temp or shared memory
+    nir.reset_scratch_size();
+    nir.reset_shared_size();
+    nir_pass!(
+        nir,
+        nir_remove_dead_variables,
+        nir_variable_mode::nir_var_function_temp | nir_variable_mode::nir_var_mem_shared,
+        &dv_opts,
+    );
+    nir_pass!(
+        nir,
+        nir_lower_vars_to_explicit_types,
+        nir_variable_mode::nir_var_function_temp
+            | nir_variable_mode::nir_var_mem_shared
+            | nir_variable_mode::nir_var_mem_generic,
+        Some(glsl_get_cl_type_size_align),
+    );
+
     nir_pass!(
         nir,
         nir_lower_explicit_io,

src/gallium/frontends/rusticl/mesa/compiler/nir.rs
@@ -308,10 +308,21 @@ impl NirShader {
         unsafe { (*self.nir.as_ptr()).info.num_textures }
     }
 
+    pub fn reset_scratch_size(&mut self) {
+        unsafe {
+            (*self.nir.as_ptr()).scratch_size = 0;
+        }
+    }
+
     pub fn scratch_size(&self) -> u32 {
         unsafe { (*self.nir.as_ptr()).scratch_size }
     }
 
+    pub fn reset_shared_size(&mut self) {
+        unsafe {
+            (*self.nir.as_ptr()).info.shared_size = 0;
+        }
+    }
     pub fn shared_size(&self) -> u32 {
         unsafe { (*self.nir.as_ptr()).info.shared_size }
     }
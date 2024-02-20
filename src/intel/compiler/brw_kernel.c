alexvorxx/zink-xlib-termux

commit b52e25d3a8eee702ff5d21e4cdb9a4dc0736282a

anv: rewrite internal shaders using OpenCL
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26797>

src/intel/shaders/meson.build
@@ -1,0 +1,63 @@
+# Copyright © 2023 Intel Corporation
+# SPDX-License-Identifier: MIT
+
+intel_float64_spv_h = custom_target(
+  'float64_spv.h',
+  input : [glsl2spirv, float64_glsl_file],
+  output : 'float64_spv.h',
+  command : [
+    prog_python, '@INPUT@', '@OUTPUT@',
+    prog_glslang,
+    '--create-entry', 'main',
+    '--vn', 'float64_spv_source',
+    '--glsl-version', '450',
+    '-Olib',
+  ]
+)
+
+intel_shader_files = files(
+  'libintel_shaders.h',
+  'generate.cl',
+  'generate_draws.cl',
+  'memcpy.cl',
+  'query_copy.cl',
+)
+
+prepended_input_args = []
+foreach input_arg : intel_shader_files
+  prepended_input_args += ['--in', input_arg]
+endforeach
+
+intel_shaders_gens = [ [ 90,   9],
+                       [110,  11],
+                       [120,  12],
+                       [125, 125],
+                       [200,  20] ]
+intel_shaders = []
+foreach gen : intel_shaders_gens
+  intel_shaders += custom_target(
+    'intel_gfx@0@_shaders_code.h'.format(gen[1]),
+    input : intel_shader_files,
+    output : 'intel_gfx@0@_shaders_code.h'.format(gen[1]),
+    command : [
+      prog_intel_clc, '--nir',
+      '--prefix', 'gfx@0@_intel_shaders'.format(gen[1]),
+      prepended_input_args, '-o', '@OUTPUT@', '--',
+      '-cl-std=cl2.0', '-D__OPENCL_VERSION__=200',
+      '-DGFX_VERx10=@0@'.format(gen[0]),
+      '-I' + join_paths(meson.current_source_dir(), '.'),
+      '-I' + join_paths(meson.source_root(), 'src'),
+      '-I' + join_paths(meson.source_root(), 'src/intel'),
+      '-I' + join_paths(meson.build_root(), 'src/intel'),
+      '-I' + join_paths(meson.source_root(), 'src/intel/genxml'),
+      '-include', 'opencl-c.h',
+    ],
+    env: ['MESA_SHADER_CACHE_DISABLE=true'],
+    depends : [dep_prog_intel_clc, gen_cl_xml_pack],
+  )
+endforeach
+
+idep_intel_shaders = declare_dependency(
+  sources : intel_shaders,
+  include_directories : include_directories('.'),
+)

meson.build
@@ -298,7 +298,8 @@ with_any_broadcom = [
 if ['x86_64'].contains(host_machine.cpu_family()) and \
    get_option('intel-clc') != 'system'
   # Require intel-clc with Anv & Iris (for internal shaders)
-  with_intel_clc = get_option('intel-clc') == 'enabled'
+  with_intel_clc = get_option('intel-clc') == 'enabled' or \
+                   with_intel_vk
 else
   with_intel_clc = false
 endif

src/intel/genxml/meson.build
@@ -119,16 +119,12 @@ endforeach
 
 genX_cl_included_symbols = [
   # instructions
-  'MI_BATCH_BUFFER_START',
-  '3DSTATE_CONSTANT_ALL',
-  '3DSTATE_CONSTANT_VS',
-  '3DSTATE_RASTER',
   '3DSTATE_INDEX_BUFFER',
   '3DSTATE_VERTEX_BUFFERS',
   '3DPRIMITIVE',
   '3DPRIMITIVE_EXTENDED',
+  'MI_BATCH_BUFFER_START',
   # structures
-  '3DSTATE_CONSTANT_BODY',
   'VERTEX_BUFFER_STATE',
 ]
 

src/intel/meson.build
@@ -28,6 +28,7 @@ endif
 subdir('isl')
 subdir('common')
 subdir('compiler')
+subdir('shaders')
 if with_intel_hasvk or with_intel_vk or with_gallium_crocus or with_gallium_iris or with_intel_tools
   subdir('perf')
 endif

src/intel/vulkan/meson.build
@@ -18,8 +18,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-subdir('shaders')
-
 inc_anv = include_directories('.')
 
 anv_flags = [
@@ -110,6 +108,7 @@ anv_per_hw_ver_files = files(
   'genX_gfx_state.c',
   'genX_gpu_memcpy.c',
   'genX_init_state.c',
+  'genX_internal_kernels.c',
   'genX_pipeline.c',
   'genX_query.c',
   'genX_simple_shader.c',
@@ -121,7 +120,7 @@ endif
 foreach _gfx_ver : ['90', '110', '120', '125', '200']
   libanv_per_hw_ver_libs += static_library(
     'anv_per_hw_ver@0@'.format(_gfx_ver),
-    [anv_per_hw_ver_files, anv_entrypoints[0], anv_internal_spvs, ],
+    [anv_per_hw_ver_files, anv_entrypoints[0]],
     include_directories : [
       inc_include, inc_src, inc_intel,
     ],
@@ -131,7 +130,7 @@ foreach _gfx_ver : ['90', '110', '120', '125', '200']
       dep_libdrm, dep_valgrind, idep_nir_headers, idep_genxml,
       idep_vulkan_util_headers, idep_vulkan_wsi_headers,
       idep_vulkan_runtime_headers, idep_intel_driver_ds_headers,
-      idep_grl,
+      idep_grl, idep_intel_shaders,
     ],
   )
 endforeach
@@ -207,6 +206,7 @@ anv_deps = [
   idep_vulkan_util_headers,
   idep_vulkan_runtime_headers,
   idep_vulkan_wsi_headers,
+  idep_intel_shaders,
 ]
 
 if with_platform_x11
@@ -234,8 +234,7 @@ libanv_common = static_library(
   'anv_common',
   [
     libanv_files, anv_entrypoints, sha1_h,
-    gen_xml_pack, float64_spv_h,
-    anv_internal_spvs,
+    gen_xml_pack, intel_float64_spv_h,
   ],
   include_directories : [
     inc_include, inc_src, inc_intel,
@@ -298,7 +297,7 @@ if with_tests
     dependencies : [
       dep_thread, dep_dl, dep_m, anv_deps,
       idep_nir, idep_vulkan_util, idep_vulkan_wsi, idep_vulkan_runtime,
-      idep_mesautil, idep_intel_dev,
+      idep_mesautil, idep_intel_dev, idep_intel_shaders,
     ],
     c_args : anv_flags,
     gnu_symbol_visibility : 'hidden',
@@ -328,6 +327,7 @@ if with_tests
         idep_gtest, dep_libdrm, dep_thread, dep_m, dep_valgrind,
         idep_vulkan_util, idep_vulkan_wsi_headers,
         idep_vulkan_runtime, idep_intel_driver_ds, idep_intel_dev,
+        idep_intel_shaders,
       ],
       include_directories : [
         inc_include, inc_src, inc_intel,

src/intel/vulkan/shaders/meson.build
@@ -1,63 +1,0 @@
-# Copyright © 2022 Intel Corporation
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-float64_spv_h = custom_target(
-  'float64_spv.h',
-  input : [glsl2spirv, float64_glsl_file],
-  output : 'float64_spv.h',
-  command : [
-    prog_python, '@INPUT@', '@OUTPUT@',
-    prog_glslang,
-    '--create-entry', 'main',
-    '--vn', 'float64_spv_source',
-    '--glsl-version', '450',
-    '-Olib',
-  ]
-)
-
-anv_internal_shaders = [
-  [ 'generated_draws.glsl',       'frag' ],
-  [ 'query_copy_compute.glsl',    'comp' ],
-  [ 'query_copy_fragment.glsl',   'frag' ],
-  [ 'memcpy_compute.glsl',        'comp' ],
-]
-
-anv_internal_spvs = []
-foreach item : anv_internal_shaders
-  f = item[0]
-  stage = item[1]
-  spv_filename = f.replace('.glsl', '_spv.h')
-  src_name = f.replace('.glsl', '_spv_source')
-  anv_internal_spvs += custom_target(
-    spv_filename,
-    input : [glsl2spirv, f, files('common_generated_draws.glsl',
-                                  'common_query_copy.glsl',
-                                  'interface.h')],
-    output : spv_filename,
-    command : [
-      prog_python, '@INPUT0@', '@INPUT1@', '@OUTPUT@',
-      prog_glslang,
-      '--vn', src_name,
-      '--glsl-version', '450',
-      '--stage', stage,
-      '-I' + meson.current_source_dir(),
-      '-D_IN_SHADER_=1',
-    ])
-endforeach

src/intel/shaders/generate.cl
@@ -1,0 +1,206 @@
+/* Copyright © 2023 Intel Corporation
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "libintel_shaders.h"
+
+void genX(write_3DSTATE_VERTEX_BUFFERS)(global void *dst_ptr,
+                                        uint32_t buffer_count)
+{
+   struct GENX(3DSTATE_VERTEX_BUFFERS) v = {
+      GENX(3DSTATE_VERTEX_BUFFERS_header),
+   };
+   v.DWordLength = 1 + (buffer_count * 4) -
+                   GENX(3DSTATE_VERTEX_BUFFERS_length_bias);
+   GENX(3DSTATE_VERTEX_BUFFERS_pack)(dst_ptr, &v);
+}
+
+void genX(write_VERTEX_BUFFER_STATE)(global void *dst_ptr,
+                                     uint32_t mocs,
+                                     uint32_t buffer_idx,
+                                     uint64_t address,
+                                     uint32_t size,
+                                     uint32_t stride)
+{
+   bool buffer_null = address == 0;
+   struct GENX(VERTEX_BUFFER_STATE) v = {
+      .BufferPitch = stride,
+      .NullVertexBuffer = address == 0,
+      .AddressModifyEnable = true,
+      .MOCS = mocs,
+#if GFX_VER >= 12
+      .L3BypassDisable = true,
+#endif
+      .VertexBufferIndex = buffer_idx,
+      .BufferStartingAddress = address,
+      .BufferSize = size,
+   };
+   GENX(VERTEX_BUFFER_STATE_pack)(dst_ptr, &v);
+}
+
+#if GFX_VER == 9
+void genX(write_3DPRIMITIVE)(global void *dst_ptr,
+                             bool is_predicated,
+                             bool is_indexed,
+                             bool uses_tbimr,
+                             uint32_t vertex_count_per_instance,
+                             uint32_t start_vertex_location,
+                             uint32_t instance_count,
+                             uint32_t start_instance_location,
+                             uint32_t base_vertex_location)
+{
+   struct GENX(3DPRIMITIVE) v = {
+      GENX(3DPRIMITIVE_header),
+#if GFX_VERx10 >= 125
+      .TBIMREnable = uses_tbimr,
+#endif
+      .PredicateEnable = is_predicated,
+      .VertexAccessType = is_indexed ? RANDOM : SEQUENTIAL,
+      .VertexCountPerInstance = vertex_count_per_instance,
+      .StartVertexLocation = start_vertex_location,
+      .InstanceCount = instance_count,
+      .StartInstanceLocation = start_instance_location,
+      .BaseVertexLocation = base_vertex_location,
+   };
+   GENX(3DPRIMITIVE_pack)(dst_ptr, &v);
+}
+#endif
+
+#if GFX_VER >= 11
+void genX(write_3DPRIMITIVE_EXTENDED)(global void *dst_ptr,
+                                      bool is_predicated,
+                                      bool is_indexed,
+                                      bool uses_tbimr,
+                                      uint32_t vertex_count_per_instance,
+                                      uint32_t start_vertex_location,
+                                      uint32_t instance_count,
+                                      uint32_t start_instance_location,
+                                      uint32_t base_vertex_location,
+                                      uint32_t param_base_vertex,
+                                      uint32_t param_base_instance,
+                                      uint32_t param_draw_id)
+{
+   struct GENX(3DPRIMITIVE_EXTENDED) v = {
+      GENX(3DPRIMITIVE_EXTENDED_header),
+#if GFX_VERx10 >= 125
+      .TBIMREnable = uses_tbimr,
+#endif
+      .PredicateEnable = is_predicated,
+      .VertexAccessType = is_indexed ? RANDOM : SEQUENTIAL,
+      .VertexCountPerInstance = vertex_count_per_instance,
+      .StartVertexLocation = start_vertex_location,
+      .InstanceCount = instance_count,
+      .StartInstanceLocation = start_instance_location,
+      .BaseVertexLocation = base_vertex_location,
+      .ExtendedParameter0 = param_base_vertex,
+      .ExtendedParameter1 = param_base_instance,
+      .ExtendedParameter2 = param_draw_id,
+   };
+   GENX(3DPRIMITIVE_EXTENDED_pack)(dst_ptr, &v);
+}
+#endif
+
+void genX(write_MI_BATCH_BUFFER_START)(global void *dst_ptr, uint64_t addr)
+{
+   struct GENX(MI_BATCH_BUFFER_START) v = {
+      GENX(MI_BATCH_BUFFER_START_header),
+      .AddressSpaceIndicator = ASI_PPGTT,
+      .BatchBufferStartAddress = addr,
+   };
+   GENX(MI_BATCH_BUFFER_START_pack)(dst_ptr, &v);
+}
+
+void genX(write_draw)(global uint32_t *dst_ptr,
+                      global void *indirect_ptr,
+                      global uint32_t *draw_id_ptr,
+                      uint32_t draw_id,
+                      uint32_t instance_multiplier,
+                      bool is_indexed,
+                      bool is_predicated,
+                      bool uses_tbimr,
+                      bool uses_base,
+                      bool uses_drawid,
+                      uint32_t mocs)
+{
+#if GFX_VER == 9
+   if (uses_base || uses_drawid) {
+      uint32_t vertex_buffer_count =
+         (uses_base ? 1 : 0) + (uses_drawid ? 1 : 0);
+      genX(write_3DSTATE_VERTEX_BUFFERS)(dst_ptr, vertex_buffer_count);
+      dst_ptr += 1; /* GENX(3DSTATE_VERTEX_BUFFERS_length); */
+      if (uses_base) {
+         uint64_t base_addr = (uint64_t)indirect_ptr + (is_indexed ? 12 : 8);
+         genX(write_VERTEX_BUFFER_STATE)(dst_ptr, mocs, 31, base_addr, 8, 0);
+         dst_ptr += GENX(VERTEX_BUFFER_STATE_length);
+      }
+      if (uses_drawid) {
+         *draw_id_ptr = draw_id;
+         genX(write_VERTEX_BUFFER_STATE)(dst_ptr, mocs, 32,
+                                         (uint64_t)draw_id_ptr, 4, 0);
+         dst_ptr += GENX(VERTEX_BUFFER_STATE_length);
+      }
+   }
+
+   if (is_indexed) {
+      VkDrawIndexedIndirectCommand data =
+         *((global VkDrawIndexedIndirectCommand *)indirect_ptr);
+
+      genX(write_3DPRIMITIVE)(dst_ptr,
+                              is_predicated,
+                              is_indexed,
+                              uses_tbimr,
+                              data.indexCount,
+                              data.firstIndex,
+                              data.instanceCount * instance_multiplier,
+                              data.firstInstance,
+                              data.vertexOffset);
+   } else {
+      VkDrawIndirectCommand data =
+         *((global VkDrawIndirectCommand *)indirect_ptr);
+
+      genX(write_3DPRIMITIVE)(dst_ptr,
+                              is_predicated,
+                              is_indexed,
+                              uses_tbimr,
+                              data.vertexCount,
+                              data.firstVertex,
+                              data.instanceCount * instance_multiplier,
+                              data.firstInstance,
+                              0 /* base_vertex_location */);
+   }
+#else
+   if (is_indexed) {
+      VkDrawIndexedIndirectCommand data =
+         *((global VkDrawIndexedIndirectCommand *)indirect_ptr);
+
+      genX(write_3DPRIMITIVE_EXTENDED)(dst_ptr,
+                                       is_predicated,
+                                       is_indexed,
+                                       uses_tbimr,
+                                       data.indexCount,
+                                       data.firstIndex,
+                                       data.instanceCount * instance_multiplier,
+                                       data.firstInstance,
+                                       data.vertexOffset,
+                                       data.vertexOffset,
+                                       data.firstInstance,
+                                       draw_id);
+   } else {
+      VkDrawIndirectCommand data =
+         *((global VkDrawIndirectCommand *)indirect_ptr);
+
+      genX(write_3DPRIMITIVE_EXTENDED)(dst_ptr,
+                                       is_predicated,
+                                       is_indexed,
+                                       uses_tbimr,
+                                       data.vertexCount,
+                                       data.firstVertex,
+                                       data.instanceCount * instance_multiplier,
+                                       data.firstInstance,
+                                       0 /* base_vertex_location */,
+                                       data.firstVertex,
+                                       data.firstInstance,
+                                       draw_id);
+   }
+#endif
+}

commit 0eb3c850c6e11f2134920805f54d629e8f39abe0

intel/clc: workaround LLVM17 opaque pointers
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Acked-by: Caio Oliveira <caio.oliveira@intel.com>
Fixes: b52e25d3a8 ("anv: rewrite internal shaders using OpenCL")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27637>

src/intel/compiler/brw_kernel.c
@@ -31,6 +31,7 @@
 #include "compiler/spirv/nir_spirv.h"
 #include "dev/intel_debug.h"
 #include "util/u_atomic.h"
+#include "util/u_dynarray.h"
 
 static const nir_shader *
 load_clc_shader(struct brw_compiler *compiler, struct disk_cache *disk_cache,
@@ -455,9 +456,139 @@ brw_kernel_from_spirv(struct brw_compiler *compiler,
    return kernel->code != NULL;
 }
 
+static nir_def *
+rebuild_value_from_store(struct util_dynarray *stores,
+                         nir_def *value, unsigned read_offset)
+{
+   unsigned read_size = value->num_components * value->bit_size / 8;
+
+   util_dynarray_foreach(stores, nir_intrinsic_instr *, _store) {
+      nir_intrinsic_instr *store = *_store;
+
+      unsigned write_offset = nir_src_as_uint(store->src[1]);
+      unsigned write_size = nir_src_num_components(store->src[0]) *
+                            nir_src_bit_size(store->src[0]) / 8;
+      if (write_offset <= read_offset &&
+          (write_offset + write_size) >= (read_offset + read_size)) {
+         assert(nir_block_dominates(store->instr.block, value->parent_instr->block));
+         assert(write_size == read_size);
+         return store->src[0].ssa;
+      }
+   }
+   unreachable("Matching scratch store not found");
+}
+
+/**
+ * Remove temporary variables stored to scratch to be then reloaded
+ * immediately. Remap the load to the store SSA value.
+ *
+ * This workaround is only meant to be applied to shaders in src/intel/shaders
+ * were we know there should be no issue. More complex cases might not work
+ * with this approach.
+ */
+static bool
+nir_remove_llvm17_scratch(nir_shader *nir)
+{
+   struct util_dynarray scratch_stores;
+   void *mem_ctx = ralloc_context(NULL);
+
+   util_dynarray_init(&scratch_stores, mem_ctx);
+
+   nir_foreach_function_impl(func, nir) {
+      nir_foreach_block(block, func) {
+         nir_foreach_instr(instr, block) {
+            if (instr->type != nir_instr_type_intrinsic)
+               continue;
+
+            nir_intrinsic_instr *intrin = nir_instr_as_intrinsic(instr);
+
+            if (intrin->intrinsic != nir_intrinsic_store_scratch)
+               continue;
+
+            nir_const_value *offset = nir_src_as_const_value(intrin->src[1]);
+            if (offset != NULL) {
+               util_dynarray_append(&scratch_stores, nir_intrinsic_instr *, intrin);
+            }
+         }
+      }
+   }
+
+   bool progress = false;
+   if (util_dynarray_num_elements(&scratch_stores, nir_intrinsic_instr *) > 0) {
+      nir_foreach_function_impl(func, nir) {
+         nir_foreach_block(block, func) {
+            nir_foreach_instr_safe(instr, block) {
+               if (instr->type != nir_instr_type_intrinsic)
+                  continue;
+
+               nir_intrinsic_instr *intrin = nir_instr_as_intrinsic(instr);
+
+               if (intrin->intrinsic != nir_intrinsic_load_scratch)
+                  continue;
+
+               nir_const_value *offset = nir_src_as_const_value(intrin->src[0]);
+               if (offset == NULL)
+                  continue;
+
+               nir_def_rewrite_uses(&intrin->def,
+                                    rebuild_value_from_store(
+                                       &scratch_stores, &intrin->def,
+                                       nir_src_as_uint(intrin->src[0])));
+               nir_instr_remove(instr);
+
+               progress = true;
+            }
+         }
+      }
+   }
+
+   util_dynarray_foreach(&scratch_stores, nir_intrinsic_instr *, _store) {
+      nir_intrinsic_instr *store = *_store;
+      nir_instr_remove(&store->instr);
+   }
+
+   /* Quick sanity check */
+   assert(util_dynarray_num_elements(&scratch_stores, nir_intrinsic_instr *) == 0 ||
+          progress);
+
+   ralloc_free(mem_ctx);
+
+   return progress;
+}
+
+static void
+cleanup_llvm17_scratch(nir_shader *nir)
+{
+   {
+      bool progress;
+      do {
+         progress = false;
+         NIR_PASS(progress, nir, nir_copy_prop);
+         NIR_PASS(progress, nir, nir_opt_dce);
+         NIR_PASS(progress, nir, nir_opt_constant_folding);
+         NIR_PASS(progress, nir, nir_opt_cse);
+         NIR_PASS(progress, nir, nir_opt_algebraic);
+      } while (progress);
+   }
+
+   nir_remove_llvm17_scratch(nir);
+
+   {
+      bool progress;
+      do {
+         progress = false;
+         NIR_PASS(progress, nir, nir_copy_prop);
+         NIR_PASS(progress, nir, nir_opt_dce);
+         NIR_PASS(progress, nir, nir_opt_constant_folding);
+         NIR_PASS(progress, nir, nir_opt_cse);
+         NIR_PASS(progress, nir, nir_opt_algebraic);
+      } while (progress);
+   }
+}
 
 nir_shader *
-brw_nir_from_spirv(void *mem_ctx, const uint32_t *spirv, size_t spirv_size)
+brw_nir_from_spirv(void *mem_ctx, const uint32_t *spirv, size_t spirv_size,
+                   bool llvm17_wa)
 {
    struct spirv_to_nir_options spirv_options = {
       .environment = NIR_SPIRV_OPENCL,
@@ -610,6 +741,12 @@ brw_nir_from_spirv(void *mem_ctx, const uint32_t *spirv, size_t spirv_size)
               nir_address_format_32bit_offset_as_64bit);
 
    NIR_PASS_V(nir, nir_lower_system_values);
+
+   /* Hopefully we can drop this once lower_vars_to_ssa has improved to not
+    * lower everything to scratch.
+    */
+   if (llvm17_wa)
+      cleanup_llvm17_scratch(nir);
 
    /* Lower again, this time after dead-variables to get more compact variable
     * layouts.

src/intel/compiler/brw_kernel.h
@@ -68,7 +68,8 @@ brw_kernel_from_spirv(struct brw_compiler *compiler,
                       char **error_str);
 
 nir_shader *
-brw_nir_from_spirv(void *mem_ctx, const uint32_t *spirv, size_t spirv_size);
+brw_nir_from_spirv(void *mem_ctx, const uint32_t *spirv, size_t spirv_size,
+                   bool llvm17_wa);
 
 #ifdef __cplusplus
 } /* extern "C" */

src/intel/compiler/intel_clc.c
@@ -278,6 +278,7 @@ print_usage(char *exec_name, FILE *f)
 "Options:\n"
 "  -h  --help              Print this help.\n"
 "  -e, --entrypoint <name> Specify the entry-point name.\n"
+"  -L, --llvm17-wa         Enable LLVM 17 workarounds for opaque pointers"
 "  -p, --platform <name>   Specify the target platform name.\n"
 "      --prefix <prefix>   Prefix for variable names in generated C code.\n"
 "  -o, --out <filename>    Specify the output filename.\n"
@@ -286,6 +287,7 @@ print_usage(char *exec_name, FILE *f)
 "  -n, --nir               Specify whether to output serialized NIR instead of ISA.\n"
 "  -t, --text <filename>   Specify the output filename for the parsed text\n"
 "  -v, --verbose           Print more information during compilation.\n"
+"  -M, --llvm-version      Print LLVM version.\n"
    , exec_name);
 }
 
@@ -301,6 +303,7 @@ struct intel_clc_params {
 
    bool output_nir;
    bool print_info;
+   bool llvm17_wa;
 
    void *mem_ctx;
 
@@ -357,7 +360,8 @@ output_nir(const struct intel_clc_params *params, struct clc_binary *binary)
                                 &spirv_options);
 
    nir_shader *nir = brw_nir_from_spirv(params->mem_ctx,
-                                        binary->data, binary->size);
+                                        binary->data, binary->size,
+                                        params->llvm17_wa);
    if (!nir) {
       fprintf(stderr, "Failed to generate NIR out of SPIRV\n");
       return -1;
@@ -435,6 +439,12 @@ output_isa(const struct intel_clc_params *params, struct clc_binary *binary)
    return 0;
 }
 
+static void
+print_llvm_version(FILE *out)
+{
+   fprintf(out, "%s\n", MESA_LLVM_VERSION_STRING);
+}
+
 int main(int argc, char **argv)
 {
    int exit_code = 0;
@@ -442,16 +452,18 @@ int main(int argc, char **argv)
    process_intel_debug_variable();
 
    static struct option long_options[] ={
-      {"help",       no_argument,         0, 'h'},
-      {"entrypoint", required_argument,   0, 'e'},
-      {"platform",   required_argument,   0, 'p'},
-      {"prefix",     required_argument,   0, OPT_PREFIX},
-      {"in",         required_argument,   0, 'i'},
-      {"out",        required_argument,   0, 'o'},
-      {"spv",        required_argument,   0, 's'},
-      {"text",       required_argument,   0, 't'},
-      {"nir",        no_argument,         0, 'n'},
-      {"verbose",    no_argument,         0, 'v'},
+      {"help",         no_argument,         0, 'h'},
+      {"entrypoint",   required_argument,   0, 'e'},
+      {"platform",     required_argument,   0, 'p'},
+      {"prefix",       required_argument,   0, OPT_PREFIX},
+      {"in",           required_argument,   0, 'i'},
+      {"out",          required_argument,   0, 'o'},
+      {"spv",          required_argument,   0, 's'},
+      {"text",         required_argument,   0, 't'},
+      {"nir",          no_argument,         0, 'n'},
+      {"llvm17-wa",    no_argument,         0, 'L'},
+      {"llvm-version", no_argument,         0, 'M'},
+      {"verbose",      no_argument,         0, 'v'},
       {0, 0, 0, 0}
    };
 
@@ -470,7 +482,7 @@ int main(int argc, char **argv)
    util_dynarray_init(&input_files, params.mem_ctx);
 
    int ch;
-   while ((ch = getopt_long(argc, argv, "he:p:s:t:i:no:v", long_options, NULL)) != -1)
+   while ((ch = getopt_long(argc, argv, "he:p:s:t:i:no:MLv", long_options, NULL)) != -1)
    {
       switch (ch)
       {
@@ -501,6 +513,12 @@ int main(int argc, char **argv)
       case 'v':
          params.print_info = true;
          break;
+      case 'L':
+         params.llvm17_wa = true;
+         break;
+      case 'M':
+         print_llvm_version(stdout);
+         return EXIT_SUCCESS;
       case OPT_PREFIX:
          params.prefix = optarg;
          break;

src/intel/shaders/meson.build
@@ -29,6 +29,18 @@ foreach input_arg : intel_shader_files
   prepended_input_args += ['--in', input_arg]
 endforeach
 
+intel_shaders_clc_wa_args = []
+if with_intel_clc
+  if chosen_llvm_version_major >= 17
+    intel_shaders_clc_wa_args += ['--llvm17-wa']
+  endif
+else
+  _intel_clc_llvm_version = run_command(prog_intel_clc, '-M')
+  if _intel_clc_llvm_version.stdout().strip().version_compare('>= 17.0')
+    intel_shaders_clc_wa_args += ['--llvm17-wa']
+  endif
+endif
+
 intel_shaders_gens = [ [ 80,   8],
                        [ 90,   9],
                        [110,  11],
@@ -42,7 +54,7 @@ foreach gen : intel_shaders_gens
     input : intel_shader_files,
     output : 'intel_gfx@0@_shaders_code.h'.format(gen[1]),
     command : [
-      prog_intel_clc, '--nir',
+      prog_intel_clc, intel_shaders_clc_wa_args, '--nir',
       '--prefix', 'gfx@0@_intel_shaders'.format(gen[1]),
       prepended_input_args, '-o', '@OUTPUT@', '--',
       '-cl-std=cl2.0', '-D__OPENCL_VERSION__=200',
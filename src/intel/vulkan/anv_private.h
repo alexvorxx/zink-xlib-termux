aerisarn/mesa-uwp

commit 1e081bd68069d27ba6eb347a1344007131a6da93

anv: split 3DSTATE_TE packing between static & dynamic parts
We can reduce the amount of packing we do by only packing the dynamic
part.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24536>

src/intel/vulkan/anv_private.h
@@ -3588,6 +3588,7 @@ struct anv_graphics_pipeline {
       uint32_t                                  wm[2];
       uint32_t                                  streamout_state[5];
       uint32_t                                  gs[10];
+      uint32_t                                  te[4];
    } partial;
 };
 

src/intel/vulkan/genX_pipeline.c
@@ -1340,6 +1340,70 @@ emit_3dstate_hs_ds(struct anv_graphics_pipeline *pipeline,
    memcpy(dw, &pipeline->final.ds, sizeof(pipeline->final.ds));
 }
 
+static UNUSED bool
+geom_or_tess_prim_id_used(struct anv_graphics_pipeline *pipeline)
+{
+   const struct brw_tcs_prog_data *tcs_prog_data =
+      anv_pipeline_has_stage(pipeline, MESA_SHADER_TESS_CTRL) ?
+      get_tcs_prog_data(pipeline) : NULL;
+   const struct brw_tes_prog_data *tes_prog_data =
+      anv_pipeline_has_stage(pipeline, MESA_SHADER_TESS_EVAL) ?
+      get_tes_prog_data(pipeline) : NULL;
+   const struct brw_gs_prog_data *gs_prog_data =
+      anv_pipeline_has_stage(pipeline, MESA_SHADER_GEOMETRY) ?
+      get_gs_prog_data(pipeline) : NULL;
+
+   return (tcs_prog_data && tcs_prog_data->include_primitive_id) ||
+          (tes_prog_data && tes_prog_data->include_primitive_id) ||
+          (gs_prog_data && gs_prog_data->include_primitive_id);
+}
+
+static void
+emit_3dstate_te(struct anv_graphics_pipeline *pipeline)
+{
+   struct GENX(3DSTATE_TE) te = {
+      GENX(3DSTATE_TE_header),
+   };
+
+   if (anv_pipeline_has_stage(pipeline, MESA_SHADER_TESS_EVAL)) {
+      const struct brw_tes_prog_data *tes_prog_data =
+         get_tes_prog_data(pipeline);
+
+      te.Partitioning = tes_prog_data->partitioning;
+      te.TEDomain = tes_prog_data->domain;
+      te.TEEnable = true;
+      te.MaximumTessellationFactorOdd = 63.0;
+      te.MaximumTessellationFactorNotOdd = 64.0;
+#if GFX_VERx10 >= 125
+      const struct anv_device *device = pipeline->base.base.device;
+      if (intel_needs_workaround(device->info, 22012699309))
+         te.TessellationDistributionMode = TEDMODE_RR_STRICT;
+      else
+         te.TessellationDistributionMode = TEDMODE_RR_FREE;
+
+      if (intel_needs_workaround(device->info, 14015055625)) {
+         /* Wa_14015055625:
+          *
+          * Disable Tessellation Distribution when primitive Id is enabled.
+          */
+         if (pipeline->primitive_id_override ||
+             geom_or_tess_prim_id_used(pipeline))
+            te.TessellationDistributionMode = TEDMODE_OFF;
+      }
+
+      te.TessellationDistributionLevel = TEDLEVEL_PATCH;
+      /* 64_TRIANGLES */
+      te.SmallPatchThreshold = 3;
+      /* 1K_TRIANGLES */
+      te.TargetBlockSize = 8;
+      /* 1K_TRIANGLES */
+      te.LocalBOPAccumulatorThreshold = 1;
+#endif
+   }
+
+   GENX(3DSTATE_TE_pack)(NULL, pipeline->partial.te, &te);
+}
+
 static void
 emit_3dstate_gs(struct anv_graphics_pipeline *pipeline)
 {
@@ -1835,6 +1899,7 @@ genX(graphics_pipeline_emit)(struct anv_graphics_pipeline *pipeline,
 
       emit_3dstate_vs(pipeline);
       emit_3dstate_hs_ds(pipeline, state->ts);
+      emit_3dstate_te(pipeline);
       emit_3dstate_gs(pipeline);
 
       emit_3dstate_vf_statistics(pipeline);

src/intel/vulkan/genX_gfx_state.c
@@ -205,24 +205,6 @@ want_stencil_pma_fix(struct anv_cmd_buffer *cmd_buffer,
           wm_prog_data->computed_depth_mode != PSCDEPTH_OFF;
 }
 
-static UNUSED bool
-geom_or_tess_prim_id_used(struct anv_graphics_pipeline *pipeline)
-{
-   const struct brw_tcs_prog_data *tcs_prog_data =
-      anv_pipeline_has_stage(pipeline, MESA_SHADER_TESS_CTRL) ?
-      get_tcs_prog_data(pipeline) : NULL;
-   const struct brw_tes_prog_data *tes_prog_data =
-      anv_pipeline_has_stage(pipeline, MESA_SHADER_TESS_EVAL) ?
-      get_tes_prog_data(pipeline) : NULL;
-   const struct brw_gs_prog_data *gs_prog_data =
-      anv_pipeline_has_stage(pipeline, MESA_SHADER_GEOMETRY) ?
-      get_gs_prog_data(pipeline) : NULL;
-
-   return (tcs_prog_data && tcs_prog_data->include_primitive_id) ||
-          (tes_prog_data && tes_prog_data->include_primitive_id) ||
-          (gs_prog_data && gs_prog_data->include_primitive_id);
-}
-
 static void
 genX(cmd_emit_te)(struct anv_cmd_buffer *cmd_buffer)
 {
@@ -233,40 +215,15 @@ genX(cmd_emit_te)(struct anv_cmd_buffer *cmd_buffer)
 
    if (!tes_prog_data ||
        !anv_pipeline_has_stage(pipeline, MESA_SHADER_TESS_EVAL)) {
-      anv_batch_emit(&cmd_buffer->batch, GENX(3DSTATE_TE), te);
+      uint32_t *dw =
+         anv_batch_emitn(&cmd_buffer->batch, GENX(3DSTATE_TE_length),
+                         GENX(3DSTATE_TE));
+      memcpy(dw, &pipeline->partial.te, sizeof(pipeline->partial.te));
       return;
    }
 
-   anv_batch_emit(&cmd_buffer->batch, GENX(3DSTATE_TE), te) {
-      te.Partitioning = tes_prog_data->partitioning;
-      te.TEDomain = tes_prog_data->domain;
-      te.TEEnable = true;
-      te.MaximumTessellationFactorOdd = 63.0;
-      te.MaximumTessellationFactorNotOdd = 64.0;
-#if GFX_VERx10 >= 125
-      if (intel_needs_workaround(cmd_buffer->device->info, 22012699309))
-         te.TessellationDistributionMode = TEDMODE_RR_STRICT;
-      else
-         te.TessellationDistributionMode = TEDMODE_RR_FREE;
-
-      if (intel_needs_workaround(cmd_buffer->device->info, 14015055625)) {
-         /* Wa_14015055625:
-          *
-          * Disable Tessellation Distribution when primitive Id is enabled.
-          */
-         if (pipeline->primitive_id_override ||
-             geom_or_tess_prim_id_used(pipeline))
-            te.TessellationDistributionMode = TEDMODE_OFF;
-      }
-
-      te.TessellationDistributionLevel = TEDLEVEL_PATCH;
-      /* 64_TRIANGLES */
-      te.SmallPatchThreshold = 3;
-      /* 1K_TRIANGLES */
-      te.TargetBlockSize = 8;
-      /* 1K_TRIANGLES */
-      te.LocalBOPAccumulatorThreshold = 1;
-#endif
+   anv_batch_emit_merge(&cmd_buffer->batch, GENX(3DSTATE_TE),
+                        pipeline->partial.te, te) {
       if (dyn->ts.domain_origin == VK_TESSELLATION_DOMAIN_ORIGIN_LOWER_LEFT) {
          te.OutputTopology = tes_prog_data->output_topology;
       } else {

alexvorxx/zink-xlib-termux

commit 96ab1d65cc27975ffa8206c07e898d796da6816c

anv: fix emission of Wa_14015055625
When moving the static part, I missed that the
pipeline->primitive_id_override field isn't set yet when we check it
to emit 3DSTATE_TE.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 1e081bd680 ("anv: split 3DSTATE_TE packing between static & dynamic parts")
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27692>

src/intel/vulkan/anv_private.h
@@ -4300,7 +4300,6 @@ struct anv_graphics_pipeline {
    bool                                         kill_pixel;
    bool                                         force_fragment_thread_dispatch;
    bool                                         uses_xfb;
-   bool                                         primitive_id_override;
 
    /* Number of VERTEX_ELEMENT_STATE input elements used by the shader */
    uint32_t                                     vs_input_elements;

src/intel/vulkan/genX_pipeline.c
@@ -513,6 +513,20 @@ emit_urb_setup(struct anv_graphics_pipeline *pipeline,
 
 }
 
+static bool
+sbe_primitive_id_override(struct anv_graphics_pipeline *pipeline)
+{
+   const struct brw_wm_prog_data *wm_prog_data = get_wm_prog_data(pipeline);
+   if (!wm_prog_data)
+      return false;
+
+   const struct intel_vue_map *fs_input_map =
+      &anv_pipeline_get_last_vue_prog_data(pipeline)->vue_map;
+
+   return (wm_prog_data->inputs & VARYING_BIT_PRIMITIVE_ID) &&
+          fs_input_map->varying_to_slot[VARYING_SLOT_PRIMITIVE_ID] == -1;
+}
+
 static void
 emit_3dstate_sbe(struct anv_graphics_pipeline *pipeline)
 {
@@ -615,15 +629,13 @@ emit_3dstate_sbe(struct anv_graphics_pipeline *pipeline)
          /* Ask the hardware to supply PrimitiveID if the fragment shader
           * reads it but a previous stage didn't write one.
           */
-         if ((wm_prog_data->inputs & VARYING_BIT_PRIMITIVE_ID) &&
-             fs_input_map->varying_to_slot[VARYING_SLOT_PRIMITIVE_ID] == -1) {
+         if (sbe_primitive_id_override(pipeline)) {
             sbe.PrimitiveIDOverrideAttributeSelect =
                wm_prog_data->urb_setup[VARYING_SLOT_PRIMITIVE_ID];
             sbe.PrimitiveIDOverrideComponentX = true;
             sbe.PrimitiveIDOverrideComponentY = true;
             sbe.PrimitiveIDOverrideComponentZ = true;
             sbe.PrimitiveIDOverrideComponentW = true;
-            pipeline->primitive_id_override = true;
          }
       } else {
          assert(anv_pipeline_is_mesh(pipeline));
@@ -1423,7 +1435,7 @@ emit_3dstate_te(struct anv_graphics_pipeline *pipeline)
              *
              * Disable Tessellation Distribution when primitive Id is enabled.
              */
-            if (pipeline->primitive_id_override ||
+            if (sbe_primitive_id_override(pipeline) ||
                 geom_or_tess_prim_id_used(pipeline))
                te.TessellationDistributionMode = TEDMODE_OFF;
          }
3b150bcba873d204a8ef726b35b5d23979187c8d
Christian Gmeiner <cgmeiner@igalia.com>
2024-02-28 09:08:07 +0100

    etnaviv: Introduce common etna_core_info
    This struct will be used in different places outside of the gallium
    driver.
    
    Signed-off-by: Christian Gmeiner <cgmeiner@igalia.com>
    Reviewed-by: Lucas Stach <l.stach@pengutronix.de>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28574>

---
+++ src/etnaviv/common/etna_core_info.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright © 2024 Igalia S.L.
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+struct etna_core_info {
+   uint32_t model;
+   uint32_t revision;
+};

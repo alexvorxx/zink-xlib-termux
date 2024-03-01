44fb57b82709a3ef653d5ee71d557f62d173ed05
Ian Romanick <ian.d.romanick@intel.com>
2024-03-01 11:54:01 -0800

    intel/elk: Don't call nir_opt_remove_phis before nir_convert_from_ssa
    shader-db:
    
    All platforms had similar results. (Ivy Bridge shown)
    total instructions in shared programs: 15831424 -> 15831637 (<.01%)
    instructions in affected programs: 38880 -> 39093 (0.55%)
    helped: 0 / HURT: 179
    
    total cycles in shared programs: 432140353 -> 432170199 (<.01%)
    cycles in affected programs: 11798080 -> 11827926 (0.25%)
    helped: 77 / HURT: 123
    
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28136>

--- src/intel/compiler/elk/elk_nir.c
+++ src/intel/compiler/elk/elk_nir.c
@@ -1529,7 +1529,6 @@ elk_postprocess_nir(nir_shader *nir, const struct elk_compiler *compiler,
     */
    NIR_PASS(_, nir, nir_convert_to_lcssa, true, true);
    NIR_PASS_V(nir, nir_divergence_analysis);
-   OPT(nir_opt_remove_phis);
 
    OPT(nir_convert_from_ssa, true);
 

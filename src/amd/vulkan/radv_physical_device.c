898f28f790dd56201260ec9720ac58ecdd6402c4
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2024-03-04 08:52:56 +0100

    radv: allow RADV_PERFTEST=shader_object on all GFX9 GPUs
    Renoir used to hangs in CI but it's fixed now.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28069>

--- src/amd/vulkan/radv_physical_device.c
+++ src/amd/vulkan/radv_physical_device.c
@@ -98,9 +98,7 @@ radv_calibrated_timestamps_enabled(const struct radv_physical_device *pdevice)
 static bool
 radv_shader_object_enabled(const struct radv_physical_device *pdevice)
 {
-   /* FIXME: Fix GPU hangs on Renoir. */
-   return (pdevice->rad_info.gfx_level != GFX9 || pdevice->rad_info.family == CHIP_VEGA10) && !pdevice->use_llvm &&
-          pdevice->instance->perftest_flags & RADV_PERFTEST_SHADER_OBJECT;
+   return !pdevice->use_llvm && pdevice->instance->perftest_flags & RADV_PERFTEST_SHADER_OBJECT;
 }
 
 bool

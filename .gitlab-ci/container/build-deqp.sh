alexvorxx/zink-xlib-termux

commit 9ab02222d042d88425168fa4e8446523c5a47c4c

ci/deqp: backport Implement support for the EGL_EXT_config_select_group extension GL-CTS patch
This will be useful for finishing the
https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/9989

Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28060>

.gitlab-ci/container/build-deqp.sh
@@ -53,6 +53,8 @@ fi
 
 # shellcheck disable=SC2034
 gl_cts_commits_to_backport=(
+  # Implement support for the EGL_EXT_config_select_group extension 
+  88ba9ac270db5be600b1ecacbc6d9db0c55d5be4
 )
 
 # shellcheck disable=SC2034

.gitlab-ci/image-tags.yml
@@ -25,9 +25,9 @@ variables:
    DEBIAN_X86_64_TEST_ANDROID_IMAGE_PATH: "debian/x86_64_test-android"
 
    DEBIAN_X86_64_TEST_ANDROID_TAG: "20240302-gl-gles"
-   DEBIAN_X86_64_TEST_GL_TAG: "20240308-piglit-1e"
+   DEBIAN_X86_64_TEST_GL_TAG: "20240309-glcts"
    DEBIAN_X86_64_TEST_VK_TAG: "20240308-piglit-1e"
-   KERNEL_ROOTFS_TAG: "20240308-piglit-1e"
+   KERNEL_ROOTFS_TAG: "20240309-glcts"
 
    ALPINE_X86_64_BUILD_TAG: "20240208-libclc-5"
    ALPINE_X86_64_LAVA_SSH_TAG: "20230626-v1"

commit a5010f5f03963fb8e0ab82848cd67ebd3fa42c00

ci/deqp: add EGL patch for correct suite (GLES, not GL)
Fixes: 9ab02222d042 ("ci/deqp: backport Implement support for the EGL_EXT_config_select_group extension GL-CTS patch")

Signed-off-by: David Heidelberg <david.heidelberg@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28091>

.gitlab-ci/container/build-deqp.sh
@@ -53,8 +53,6 @@ fi
 
 # shellcheck disable=SC2034
 gl_cts_commits_to_backport=(
-  # Implement support for the EGL_EXT_config_select_group extension 
-  88ba9ac270db5be600b1ecacbc6d9db0c55d5be4
 )
 
 # shellcheck disable=SC2034
@@ -69,7 +67,10 @@ if [ "${DEQP_TARGET}" = 'android' ]; then
 fi
 
 # shellcheck disable=SC2034
+# GLES builds also EGL
 gles_cts_commits_to_backport=(
+  # Implement support for the EGL_EXT_config_select_group extension
+  88ba9ac270db5be600b1ecacbc6d9db0c55d5be4
 )
 
 # shellcheck disable=SC2034

.gitlab-ci/image-tags.yml
@@ -25,9 +25,9 @@ variables:
    DEBIAN_X86_64_TEST_ANDROID_IMAGE_PATH: "debian/x86_64_test-android"
 
    DEBIAN_X86_64_TEST_ANDROID_TAG: "20240302-gl-gles"
-   DEBIAN_X86_64_TEST_GL_TAG: "20240309-glcts"
+   DEBIAN_X86_64_TEST_GL_TAG: "20240309-glescts"
    DEBIAN_X86_64_TEST_VK_TAG: "20240308-piglit-1e"
-   KERNEL_ROOTFS_TAG: "20240309-glcts"
+   KERNEL_ROOTFS_TAG: "20240309-glescts"
 
    ALPINE_X86_64_BUILD_TAG: "20240208-libclc-5"
    ALPINE_X86_64_LAVA_SSH_TAG: "20230626-v1"
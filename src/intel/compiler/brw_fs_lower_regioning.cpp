7a24f29fbbb9e95053699ef0e5e19999655fee73
Kenneth Graunke <kenneth@whitecape.org>
2024-03-09 22:58:04 -0800

    intel/brw: Fix lower_regioning for BROADCAST, MOV_INDIRECT on Q types
    For BROADCAST and MOV_INDIRECT, required_exec_type was returning
    brw_int_type(type_sz(t), false), which is an unsigned type.  However,
    get_exec_type(inst) returns the original type for either Q or UQ.
    This meant that has_invalid_exec_type would detect a mismatch and
    trigger lowering.
    
    That lowering would insert new 64-bit MOVs, which would need to be
    lowered on platforms which don't support Q/UQ.  Except, we already
    ran that lowering pass earlier.  So, the unlowered Q/UQ MOVs would
    reach the software scoreboarding pass, and trigger failures in the
    inferred_exec_pipe() function, as no pipe is available to handle
    64-bit integer operations.
    
    It turns out that we don't need the region lowering pass to do
    anything for these opcodes.  The generator code for both BROADCAST
    and MOV_INDIRECT already handle decomposing Q/UQ operations into
    32-bit MOVs when they're not supported.  And, it also implicitly
    converts to integer types, even for floating point sources.  The
    inferred_exec_pipe function already special cases them to note
    that they'll always be handled on the integer pipe, so that matches.
    
    Just drop the region lowering code for these opcodes.
    
    Cc: mesa-stable
    Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28458>

--- src/intel/compiler/brw_fs_lower_regioning.cpp
+++ src/intel/compiler/brw_fs_lower_regioning.cpp
@@ -189,16 +189,6 @@ namespace {
          else
             return brw_int_type(type_sz(t), false);
 
-      case SHADER_OPCODE_BROADCAST:
-      case SHADER_OPCODE_MOV_INDIRECT:
-         if (((intel_device_info_is_9lp(devinfo) ||
-               devinfo->verx10 >= 125) && type_sz(inst->src[0].type) > 4) ||
-             (devinfo->verx10 >= 125 &&
-              brw_reg_type_is_floating_point(inst->src[0].type)))
-            return brw_int_type(type_sz(t), false);
-         else
-            return t;
-
       default:
          return t;
       }

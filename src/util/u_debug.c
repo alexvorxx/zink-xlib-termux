aerisarn/mesa-uwp

commit 0c42c79edfb031509c9c0cf833d52a6f4c064356

utils/u_debug: Fix parse of "all,<something else>
If debug string is something like "all,file=..." the all case would
never be executed, this can be fixed by comparing up to strlen("all").

Signed-off-by: José Roberto de Souza <jose.souza@intel.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26882>

src/util/u_debug.c
@@ -423,7 +423,7 @@ parse_debug_string(const char *debug,
 
    if (debug != NULL) {
       for (; control->string != NULL; control++) {
-         if (!strcmp(debug, "all")) {
+         if (!strncmp(debug, "all", strlen("all"))) {
             flag |= control->flag;
 
          } else {

alexvorxx/zink-xlib-termux

commit 433a3c262a06c15c2884149d450043ad0ffdb32a

util/u_debug: fix parsing of "all" again
The current implementation is incorrect if the string starts with "all"
like "RADV_DEBUG=allbos".

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/10741
Fixes: 0c42c79edfb ("utils/u_debug: Fix parse of "all,<something else>")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28051>

src/util/u_debug.c
@@ -423,18 +423,17 @@ parse_debug_string(const char *debug,
 
    if (debug != NULL) {
       for (; control->string != NULL; control++) {
-         if (!strncmp(debug, "all", strlen("all"))) {
-            flag |= control->flag;
+         const char *s = debug;
+         unsigned n;
 
-         } else {
-            const char *s = debug;
-            unsigned n;
+         for (; n = strcspn(s, ", "), *s; s += MAX2(1, n)) {
+            if (!n)
+               continue;
 
-            for (; n = strcspn(s, ", "), *s; s += MAX2(1, n)) {
-               if (strlen(control->string) == n &&
-                   !strncmp(control->string, s, n))
-                  flag |= control->flag;
-            }
+            if (!strncmp("all", s, n) ||
+                (strlen(control->string) == n &&
+                !strncmp(control->string, s, n)))
+               flag |= control->flag;
          }
       }
    }
5c49f3c1aa30e935ed346d67de92400c6df9dc63
Joshua Ashton <joshua@froggi.es>
2024-03-14 04:08:36 +0000

    lavapipe: Enable EXT_swapchain_maintenance1
    This was missing, this is implemented in common code.
    
    Signed-off-by: Joshua Ashton <joshua@froggi.es>
    Acked-by: Faith Ekstrand <faith.ekstrand@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28275>

--- src/gallium/frontends/lavapipe/lvp_device.c
+++ src/gallium/frontends/lavapipe/lvp_device.c
@@ -75,6 +75,7 @@ static const struct vk_instance_extension_table lvp_instance_extensions_supporte
    .KHR_get_surface_capabilities2            = true,
    .KHR_surface                              = true,
    .KHR_surface_protected_capabilities       = true,
+   .EXT_surface_maintenance1                 = true,
 #endif
 #ifdef VK_USE_PLATFORM_WAYLAND_KHR
    .KHR_wayland_surface                      = true,
@@ -224,6 +225,9 @@ static const struct vk_device_extension_table lvp_device_extensions_supported = 
    .EXT_shader_subgroup_vote              = true,
    .EXT_shader_viewport_index_layer       = true,
    .EXT_subgroup_size_control             = true,
+#ifdef LVP_USE_WSI_PLATFORM
+   .EXT_swapchain_maintenance1            = true,
+#endif
    .EXT_texel_buffer_alignment            = true,
    .EXT_transform_feedback                = true,
    .EXT_vertex_attribute_divisor          = true,
@@ -642,6 +646,11 @@ lvp_get_features(const struct lvp_physical_device *pdevice,
       /* VK_AMDX_shader_enqueue */
 #ifdef VK_ENABLE_BETA_EXTENSIONS
       .shaderEnqueue = true,
+#endif
+
+#ifdef LVP_USE_WSI_PLATFORM
+      /* VK_EXT_swapchain_maintenance1 */
+      .swapchainMaintenance1 = true,
 #endif
    };
 }

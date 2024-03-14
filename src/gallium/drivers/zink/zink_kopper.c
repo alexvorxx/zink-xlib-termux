alexvorxx/zink-xlib-termux

commit 8fb83c0935948c3bf0717e0a3d9ea3ba4cdf4f6c

zink: only update swapchain readback cache on create if necessary
minor optimization

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27553>

src/gallium/drivers/zink/zink_kopper.c
@@ -877,7 +877,7 @@ zink_kopper_acquire_readback(struct zink_context *ctx, struct zink_resource *res
    VkResult ret = VK_SUCCESS;
 
    if (++cdt->readback_counter >= ZINK_READBACK_THRESHOLD) {
-      if (kopper_ensure_readback(screen, res))
+      if (kopper_ensure_readback(screen, res) && cswap->images[res->obj->dt_idx].readback_needs_update)
          zink_kopper_readback_update(ctx, res);
    }
    /* if this hasn't been presented or if it has data, use this as the readback target */

commit b46479d443ccb5b54ea5564e905b730761a379f7

zink: don't deref swapchain image array with UINT32_MAX
this indicates an unacquired swapchain

Fixes: 8fb83c09359 ("zink: only update swapchain readback cache on create if necessary")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28160>

src/gallium/drivers/zink/zink_kopper.c
@@ -889,7 +889,8 @@ zink_kopper_acquire_readback(struct zink_context *ctx, struct zink_resource *res
    VkResult ret = VK_SUCCESS;
 
    if (++cdt->readback_counter >= ZINK_READBACK_THRESHOLD) {
-      if (kopper_ensure_readback(screen, res) && cswap->images[res->obj->dt_idx].readback_needs_update)
+      if (kopper_ensure_readback(screen, res) &&
+          res->obj->dt_idx != UINT32_MAX && cswap->images[res->obj->dt_idx].readback_needs_update)
          zink_kopper_readback_update(ctx, res);
    }
    /* if this hasn't been presented or if it has data, use this as the readback target */
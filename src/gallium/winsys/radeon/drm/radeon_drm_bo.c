aerisarn/mesa-uwp

commit 4a078e693e913921d11e30988dc6e923e7cc27b7

r300,r600,radeon/winsys: always pass the winsys to radeon_bo_reference
This will allow the removal of pb_cache_entry::mgr.

Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26643>

src/gallium/winsys/radeon/drm/radeon_drm_bo.c
@@ -48,7 +48,7 @@ static bool radeon_real_bo_is_busy(struct radeon_bo *bo)
                               &args, sizeof(args)) != 0;
 }
 
-static bool radeon_bo_is_busy(struct radeon_bo *bo)
+static bool radeon_bo_is_busy(struct radeon_winsys *rws, struct radeon_bo *bo)
 {
    unsigned num_idle;
    bool busy = false;
@@ -62,7 +62,7 @@ static bool radeon_bo_is_busy(struct radeon_bo *bo)
          busy = true;
          break;
       }
-      radeon_ws_bo_reference(&bo->u.slab.fences[num_idle], NULL);
+      radeon_ws_bo_reference(rws, &bo->u.slab.fences[num_idle], NULL);
    }
    memmove(&bo->u.slab.fences[0], &bo->u.slab.fences[num_idle],
          (bo->u.slab.num_fences - num_idle) * sizeof(bo->u.slab.fences[0]));
@@ -81,7 +81,7 @@ static void radeon_real_bo_wait_idle(struct radeon_bo *bo)
                           &args, sizeof(args)) == -EBUSY);
 }
 
-static void radeon_bo_wait_idle(struct radeon_bo *bo)
+static void radeon_bo_wait_idle(struct radeon_winsys *rws, struct radeon_bo *bo)
 {
    if (bo->handle) {
       radeon_real_bo_wait_idle(bo);
@@ -89,7 +89,7 @@ static void radeon_bo_wait_idle(struct radeon_bo *bo)
       mtx_lock(&bo->rws->bo_fence_lock);
       while (bo->u.slab.num_fences) {
          struct radeon_bo *fence = NULL;
-         radeon_ws_bo_reference(&fence, bo->u.slab.fences[0]);
+         radeon_ws_bo_reference(rws, &fence, bo->u.slab.fences[0]);
          mtx_unlock(&bo->rws->bo_fence_lock);
 
          /* Wait without holding the fence lock. */
@@ -97,12 +97,12 @@ static void radeon_bo_wait_idle(struct radeon_bo *bo)
 
          mtx_lock(&bo->rws->bo_fence_lock);
          if (bo->u.slab.num_fences && fence == bo->u.slab.fences[0]) {
-            radeon_ws_bo_reference(&bo->u.slab.fences[0], NULL);
+            radeon_ws_bo_reference(rws, &bo->u.slab.fences[0], NULL);
             memmove(&bo->u.slab.fences[0], &bo->u.slab.fences[1],
                   (bo->u.slab.num_fences - 1) * sizeof(bo->u.slab.fences[0]));
             bo->u.slab.num_fences--;
          }
-         radeon_ws_bo_reference(&fence, NULL);
+         radeon_ws_bo_reference(rws, &fence, NULL);
       }
       mtx_unlock(&bo->rws->bo_fence_lock);
    }
@@ -117,7 +117,7 @@ static bool radeon_bo_wait(struct radeon_winsys *rws,
 
    /* No timeout. Just query. */
    if (timeout == 0)
-      return !bo->num_active_ioctls && !radeon_bo_is_busy(bo);
+      return !bo->num_active_ioctls && !radeon_bo_is_busy(rws, bo);
 
    abs_timeout = os_time_get_absolute_timeout(timeout);
 
@@ -127,12 +127,12 @@ static bool radeon_bo_wait(struct radeon_winsys *rws,
 
    /* Infinite timeout. */
    if (abs_timeout == OS_TIMEOUT_INFINITE) {
-      radeon_bo_wait_idle(bo);
+      radeon_bo_wait_idle(rws, bo);
       return true;
    }
 
    /* Other timeouts need to be emulated with a loop. */
-   while (radeon_bo_is_busy(bo)) {
+   while (radeon_bo_is_busy(rws, bo)) {
       if (os_time_get_nano() >= abs_timeout)
          return false;
       os_time_sleep(10);
@@ -704,7 +704,7 @@ static struct radeon_bo *radeon_create_bo(struct radeon_drm_winsys *rws,
                _mesa_hash_table_u64_search(rws->bo_vas, va.offset);
 
          mtx_unlock(&rws->bo_handles_mutex);
-         pb_reference(&b, &old_bo->base);
+         radeon_bo_reference(&rws->base, &b, &old_bo->base);
          return radeon_bo(b);
       }
 
@@ -804,7 +804,7 @@ struct pb_slab *radeon_bo_slab_alloc(void *priv, unsigned heap,
    return &slab->base;
 
 fail_buffer:
-   radeon_ws_bo_reference(&slab->buffer, NULL);
+   radeon_ws_bo_reference(&ws->base, &slab->buffer, NULL);
 fail:
    FREE(slab);
    return NULL;
@@ -812,17 +812,18 @@ fail:
 
 void radeon_bo_slab_free(void *priv, struct pb_slab *pslab)
 {
+   struct radeon_winsys *rws = (struct radeon_winsys *)priv;
    struct radeon_slab *slab = (struct radeon_slab *)pslab;
 
    for (unsigned i = 0; i < slab->base.num_entries; ++i) {
       struct radeon_bo *bo = &slab->entries[i];
       for (unsigned j = 0; j < bo->u.slab.num_fences; ++j)
-         radeon_ws_bo_reference(&bo->u.slab.fences[j], NULL);
+         radeon_ws_bo_reference(rws, &bo->u.slab.fences[j], NULL);
       FREE(bo->u.slab.fences);
    }
 
    FREE(slab->entries);
-   radeon_ws_bo_reference(&slab->buffer, NULL);
+   radeon_ws_bo_reference(rws, &slab->buffer, NULL);
    FREE(slab);
 }
 
@@ -1153,7 +1154,7 @@ static struct pb_buffer *radeon_winsys_bo_from_ptr(struct radeon_winsys *rws,
                _mesa_hash_table_u64_search(ws->bo_vas, va.offset);
 
          mtx_unlock(&ws->bo_handles_mutex);
-         pb_reference(&b, &old_bo->base);
+         radeon_bo_reference(rws, &b, &old_bo->base);
          return b;
       }
 
@@ -1284,7 +1285,7 @@ done:
                _mesa_hash_table_u64_search(ws->bo_vas, va.offset);
 
          mtx_unlock(&ws->bo_handles_mutex);
-         pb_reference(&b, &old_bo->base);
+         radeon_bo_reference(rws, &b, &old_bo->base);
          return b;
       }
 

src/gallium/drivers/r300/r300_context.c
@@ -64,7 +64,7 @@ static void r300_release_referenced_objects(struct r300_context *r300)
 
     /* Manually-created vertex buffers. */
     pipe_vertex_buffer_unreference(&r300->dummy_vb);
-    pb_reference(&r300->vbo, NULL);
+    radeon_bo_reference(r300->rws, &r300->vbo, NULL);
 
     r300->context.delete_depth_stencil_alpha_state(&r300->context,
                                                    r300->dsa_decompress_zmask);

src/gallium/drivers/r300/r300_query.c
@@ -73,9 +73,10 @@ static struct pipe_query *r300_create_query(struct pipe_context *pipe,
 static void r300_destroy_query(struct pipe_context* pipe,
                                struct pipe_query* query)
 {
+    struct r300_context *r300 = r300_context(pipe);
     struct r300_query* q = r300_query(query);
 
-    pb_reference(&q->buf, NULL);
+    radeon_bo_reference(r300->rws, &q->buf, NULL);
     FREE(query);
 }
 
@@ -120,7 +121,7 @@ static bool r300_end_query(struct pipe_context* pipe,
     struct r300_query *q = r300_query(query);
 
     if (q->type == PIPE_QUERY_GPU_FINISHED) {
-        pb_reference(&q->buf, NULL);
+        radeon_bo_reference(r300->rws, &q->buf, NULL);
         r300_flush(pipe, PIPE_FLUSH_ASYNC,
                    (struct pipe_fence_handle**)&q->buf);
         return true;

src/gallium/drivers/r300/r300_render.c
@@ -951,7 +951,7 @@ static bool r300_render_allocate_vertices(struct vbuf_render* render,
     DBG(r300, DBG_DRAW, "r300: render_allocate_vertices (size: %d)\n", size);
 
     if (!r300->vbo || size + r300->draw_vbo_offset > r300->vbo->size) {
-	pb_reference(&r300->vbo, NULL);
+	radeon_bo_reference(r300->rws, &r300->vbo, NULL);
         r300->vbo = NULL;
         r300render->vbo_ptr = NULL;
 

src/gallium/drivers/r300/r300_screen_buffer.c
@@ -53,17 +53,18 @@ void r300_upload_index_buffer(struct r300_context *r300,
 void r300_resource_destroy(struct pipe_screen *screen,
                            struct pipe_resource *buf)
 {
+   struct r300_screen *rscreen = r300_screen(screen);
+
    if (buf->target == PIPE_BUFFER) {
       struct r300_resource *rbuf = r300_resource(buf);
 
       align_free(rbuf->malloced_buffer);
 
       if (rbuf->buf)
-         pb_reference(&rbuf->buf, NULL);
+         radeon_bo_reference(rscreen->rws, &rbuf->buf, NULL);
 
       FREE(rbuf);
    } else {
-      struct r300_screen *rscreen = r300_screen(screen);
       struct r300_resource* tex = (struct r300_resource*)buf;
 
       if (tex->tex.cmask_dwords) {
@@ -73,7 +74,7 @@ void r300_resource_destroy(struct pipe_screen *screen,
           }
           mtx_unlock(&rscreen->cmask_mutex);
       }
-      pb_reference(&tex->buf, NULL);
+      radeon_bo_reference(rscreen->rws, &tex->buf, NULL);
       FREE(tex);
    }
 }
@@ -122,7 +123,7 @@ r300_buffer_transfer_map( struct pipe_context *context,
                                                RADEON_FLAG_NO_INTERPROCESS_SHARING);
             if (new_buf) {
                 /* Discard the old buffer. */
-                pb_reference(&rbuf->buf, NULL);
+                radeon_bo_reference(r300->rws, &rbuf->buf, NULL);
                 rbuf->buf = new_buf;
 
                 /* We changed the buffer, now we need to bind it where the old one was bound. */

src/gallium/drivers/r300/r300_texture.c
@@ -1109,7 +1109,7 @@ r300_texture_create_object(struct r300_screen *rscreen,
 fail:
     FREE(tex);
     if (buffer)
-        pb_reference(&buffer, NULL);
+        radeon_bo_reference(rscreen->rws, &buffer, NULL);
     return NULL;
 }
 

src/gallium/drivers/r600/r600_buffer_common.c
@@ -188,7 +188,7 @@ bool r600_alloc_resource(struct r600_common_screen *rscreen,
 	else
 		res->gpu_address = 0;
 
-	pb_reference(&old_buf, NULL);
+	radeon_bo_reference(rscreen->ws, &old_buf, NULL);
 
 	util_range_set_empty(&res->valid_buffer_range);
 
@@ -203,12 +203,13 @@ bool r600_alloc_resource(struct r600_common_screen *rscreen,
 
 void r600_buffer_destroy(struct pipe_screen *screen, struct pipe_resource *buf)
 {
+	struct r600_screen *rscreen = (struct r600_screen*)screen;
 	struct r600_resource *rbuffer = r600_resource(buf);
 
 	threaded_resource_deinit(buf);
 	util_range_destroy(&rbuffer->valid_buffer_range);
 	pipe_resource_reference((struct pipe_resource**)&rbuffer->immed_buffer, NULL);
-	pb_reference(&rbuffer->buf, NULL);
+	radeon_bo_reference(rscreen->b.ws, &rbuffer->buf, NULL);
 	FREE(rbuffer);
 }
 
@@ -251,7 +252,7 @@ void r600_replace_buffer_storage(struct pipe_context *ctx,
 	struct r600_resource *rsrc = r600_resource(src);
 	uint64_t old_gpu_address = rdst->gpu_address;
 
-	pb_reference(&rdst->buf, rsrc->buf);
+	radeon_bo_reference(rctx->ws, &rdst->buf, rsrc->buf);
 	rdst->gpu_address = rsrc->gpu_address;
 	rdst->b.b.bind = rsrc->b.b.bind;
 	rdst->flags = rsrc->flags;

src/gallium/drivers/r600/r600_texture.c
@@ -405,7 +405,7 @@ static void r600_reallocate_texture_inplace(struct r600_common_context *rctx,
 
 	/* Replace the structure fields of rtex. */
 	rtex->resource.b.b.bind = templ.bind;
-	pb_reference(&rtex->resource.buf, new_tex->resource.buf);
+	radeon_bo_reference(rctx->ws, &rtex->resource.buf, new_tex->resource.buf);
 	rtex->resource.gpu_address = new_tex->resource.gpu_address;
 	rtex->resource.vram_usage = new_tex->resource.vram_usage;
 	rtex->resource.gart_usage = new_tex->resource.gart_usage;
@@ -576,6 +576,7 @@ static bool r600_texture_get_handle(struct pipe_screen* screen,
 
 void r600_texture_destroy(struct pipe_screen *screen, struct pipe_resource *ptex)
 {
+	struct r600_common_screen *rscreen = (struct r600_common_screen*)screen;
 	struct r600_texture *rtex = (struct r600_texture*)ptex;
 	struct r600_resource *resource = &rtex->resource;
 
@@ -585,7 +586,7 @@ void r600_texture_destroy(struct pipe_screen *screen, struct pipe_resource *ptex
 	if (rtex->cmask_buffer != &rtex->resource) {
 	    r600_resource_reference(&rtex->cmask_buffer, NULL);
 	}
-	pb_reference(&resource->buf, NULL);
+	radeon_bo_reference(rscreen->ws, &resource->buf, NULL);
 	FREE(rtex);
 }
 
@@ -1802,9 +1803,10 @@ static void
 r600_memobj_destroy(struct pipe_screen *screen,
 		    struct pipe_memory_object *_memobj)
 {
+	struct r600_common_screen *rscreen = (struct r600_common_screen*)screen;
 	struct r600_memory_object *memobj = (struct r600_memory_object *)_memobj;
 
-	pb_reference(&memobj->buf, NULL);
+	radeon_bo_reference(rscreen->ws, &memobj->buf, NULL);
 	free(memobj);
 }
 
@@ -1870,7 +1872,7 @@ r600_texture_from_memobj(struct pipe_screen *screen,
 	/* r600_texture_create_object doesn't increment refcount of
 	 * memobj->buf, so increment it here.
 	 */
-	pb_reference(&buf, memobj->buf);
+	radeon_bo_reference(rscreen->ws, &buf, memobj->buf);
 
 	rtex->resource.b.is_shared = true;
 	rtex->resource.external_usage = PIPE_HANDLE_USAGE_FRAMEBUFFER_WRITE;

alexvorxx/zink-xlib-termux

commit 99017891cad55972c26c0ca8354f48e722b37a66

winsys/radeon: pass priv instead NULL to radeon_bo_can_reclaim
This fixes a NULL pointer issue.

Fixes: 4a078e693e9 ("r300,r600,radeon/winsys: always pass the winsys to radeon_bo_reference")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/10613
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28240>

src/gallium/winsys/radeon/drm/radeon_drm_bo.c
@@ -729,7 +729,7 @@ bool radeon_bo_can_reclaim_slab(void *priv, struct pb_slab_entry *entry)
 {
    struct radeon_bo *bo = container_of(entry, struct radeon_bo, u.slab.entry);
 
-   return radeon_bo_can_reclaim(NULL, &bo->base);
+   return radeon_bo_can_reclaim(priv, &bo->base);
 }
 
 static void radeon_bo_slab_destroy(void *winsys, struct pb_buffer_lean *_buf)
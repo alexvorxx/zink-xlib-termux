75861c64b8282c8e6a3c59cc22f062f9a15bb3de
Faith Ekstrand <faith.ekstrand@collabora.com>
2024-03-20 19:17:55 -0500

    nir: Add a lower_terminate_to_demote pass
    Reviewed-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28300>

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -6293,6 +6293,8 @@ bool nir_lower_discard_if(nir_shader *shader, nir_lower_discard_if_options optio
 bool nir_lower_discard_or_demote(nir_shader *shader,
                                  bool force_correct_quad_ops_after_discard);
 
+bool nir_lower_terminate_to_demote(nir_shader *nir);
+
 bool nir_lower_memory_model(nir_shader *shader);
 
 bool nir_lower_goto_ifs(nir_shader *shader);

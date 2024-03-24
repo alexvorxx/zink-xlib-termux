alexvorxx/zink-xlib-termux

commit 78ea3bb43dc37013c3ab87c261ef5987d5e876fa

ci/deqp: use the proper gl/gles releases for deqp-gl*, deqp-gles*, deqp-egl
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27473>

.gitlab-ci/deqp-runner.sh
@@ -62,13 +62,13 @@ if [ -z "$DEQP_SUITE" ]; then
        MUSTPASS=/deqp/mustpass/vk-master.txt
        DEQP=/deqp/external/vulkancts/modules/vulkan/deqp-vk
     elif [ "$DEQP_VER" = "gles2" ] || [ "$DEQP_VER" = "gles3" ] || [ "$DEQP_VER" = "gles31" ] || [ "$DEQP_VER" = "egl" ]; then
-       MUSTPASS=/deqp/mustpass/$DEQP_VER-master.txt
+       MUSTPASS=/deqp/mustpass/$DEQP_VER-main.txt
        DEQP=/deqp/modules/$DEQP_VER/deqp-$DEQP_VER
     elif [ "$DEQP_VER" = "gles2-khr" ] || [ "$DEQP_VER" = "gles3-khr" ] || [ "$DEQP_VER" = "gles31-khr" ] || [ "$DEQP_VER" = "gles32-khr" ]; then
-       MUSTPASS=/deqp/mustpass/$DEQP_VER-master.txt
+       MUSTPASS=/deqp/mustpass/$DEQP_VER-main.txt
        DEQP=/deqp/external/openglcts/modules/glcts
     else
-       MUSTPASS=/deqp/mustpass/$DEQP_VER-master.txt
+       MUSTPASS=/deqp/mustpass/$DEQP_VER-main.txt
        DEQP=/deqp/external/openglcts/modules/glcts
     fi
 

commit c73c244a6c2ee62952c64ba958a508786907e156

ci/deqp-runner: print deqp-gles version log as well
Fixes: 78ea3bb43dc37013c3ab ("ci/deqp: use the proper gl/gles releases for deqp-gl*, deqp-gles*, deqp-egl")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28356>

.gitlab-ci/deqp-runner.sh
@@ -169,7 +169,7 @@ fi
 uncollapsed_section_switch deqp "deqp: deqp-runner"
 
 # Print the detailed version with the list of backports and local patches
-for api in vk gl; do
+for api in vk gl gles; do
   deqp_version_log=/deqp/version-$api
   if [ -r "$deqp_version_log" ]; then
     cat "$deqp_version_log"
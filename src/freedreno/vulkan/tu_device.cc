20f4cd88e14642da210dc0e44bea615fa0780f1a
Valentine Burley <valentine.burley@gmail.com>
2024-03-24 16:17:21 +0000

    tu: Advertise VK_KHR_maintenance6
    Signed-off-by: Valentine Burley <valentine.burley@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28360>

--- src/freedreno/vulkan/tu_device.cc
+++ src/freedreno/vulkan/tu_device.cc
@@ -176,6 +176,7 @@ get_device_extensions(const struct tu_physical_device *device,
       .KHR_maintenance3 = true,
       .KHR_maintenance4 = true,
       .KHR_maintenance5 = true,
+      .KHR_maintenance6 = true,
       .KHR_map_memory2 = true,
       .KHR_multiview = TU_DEBUG(NOCONFORM) ? true : device->info->a6xx.has_hw_multiview,
       .KHR_performance_query = TU_DEBUG(PERFC),
@@ -453,6 +454,9 @@ tu_get_features(struct tu_physical_device *pdevice,
 
    /* VK_KHR_maintenance5 */
    features->maintenance5 = true;
+
+   /* VK_KHR_maintenance6 */
+   features->maintenance6 = true;
 
    /* VK_KHR_performance_query */
    features->performanceCounterQueryPools = true;
@@ -1105,6 +1109,11 @@ tu_get_properties(struct tu_physical_device *pdevice,
    props->polygonModePointSize = true;
    props->nonStrictWideLinesUseParallelogram = false;
    props->nonStrictSinglePixelWideLinesUseParallelogram = false;
+
+   /* VK_KHR_maintenance6 */
+   props->blockTexelViewCompatibleMultipleLayers = true;
+   props->maxCombinedImageSamplerDescriptorCount = 1;
+   props->fragmentShadingRateClampCombinerInputs = false; /* TODO */
 }
 
 static const struct vk_pipeline_cache_object_ops *const cache_import_ops[] = {

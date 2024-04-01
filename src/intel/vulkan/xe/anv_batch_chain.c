aerisarn/mesa-uwp

commit 18bd00c024fc0f11a36da5b025b89a26ebd635e1

anv/trtt: don't wait/signal syncobjs using the CPU anymore
Pass them as part of the TR-TT batch. This is what a lot of the
previous commits were building up to.

Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Signed-off-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25512>

src/intel/vulkan/xe/anv_batch_chain.c
@@ -195,16 +195,28 @@ xe_execute_trtt_batch(struct anv_sparse_submission *submit,
    if (drmSyncobjCreate(device->fd, 0, &syncobj_handle))
       return vk_errorf(device, VK_ERROR_UNKNOWN, "Unable to create sync obj");
 
-   struct drm_xe_sync sync = {
+   struct drm_xe_sync extra_sync = {
       .flags = DRM_XE_SYNC_SYNCOBJ | DRM_XE_SYNC_SIGNAL,
       .handle = syncobj_handle,
    };
+
+   struct drm_xe_sync *xe_syncs = NULL;
+   uint32_t xe_syncs_count = 0;
+   result = xe_exec_process_syncs(queue, submit->wait_count, submit->waits,
+                                  submit->signal_count, submit->signals,
+                                  1, &extra_sync,
+                                  NULL, /* utrace_submit */
+                                  false, /* is_companion_rcs_queue */
+                                  &xe_syncs, &xe_syncs_count);
+   if (result != VK_SUCCESS)
+      goto exec_error;
+
    struct drm_xe_exec exec = {
       .exec_queue_id = queue->exec_queue_id,
-      .num_batch_buffer = 1,
+      .num_syncs = xe_syncs_count,
+      .syncs = (uintptr_t)xe_syncs,
       .address = batch_bo->offset,
-      .num_syncs = 1,
-      .syncs = (uintptr_t)&sync,
+      .num_batch_buffer = 1,
    };
 
    if (!device->info->no_hw) {

src/intel/vulkan/i915/anv_batch_chain.c
@@ -992,6 +992,22 @@ i915_execute_trtt_batch(struct anv_sparse_submission *submit,
    };
    VkResult result;
 
+   for (uint32_t i = 0; i < submit->wait_count; i++) {
+      result = anv_execbuf_add_sync(device, &execbuf, submit->waits[i].sync,
+                                    false /* is_signal */,
+                                    submit->waits[i].wait_value);
+      if (result != VK_SUCCESS)
+         goto out;
+   }
+
+   for (uint32_t i = 0; i < submit->signal_count; i++) {
+      result = anv_execbuf_add_sync(device, &execbuf, submit->signals[i].sync,
+                                    true /* is_signal */,
+                                    submit->signals[i].signal_value);
+      if (result != VK_SUCCESS)
+         goto out;
+   }
+
    result = anv_execbuf_add_bo(device, &execbuf, device->workaround_bo, NULL,
                                0);
    if (result != VK_SUCCESS)
@@ -1024,6 +1040,7 @@ i915_execute_trtt_batch(struct anv_sparse_submission *submit,
       .rsvd1 = context_id,
       .rsvd2 = 0,
    };
+   setup_execbuf_fence_params(&execbuf);
 
    int ret = queue->device->info->no_hw ? 0 :
       anv_gem_execbuffer(device, &execbuf.execbuf);

src/intel/vulkan/anv_sparse.c
@@ -581,35 +581,8 @@ anv_sparse_bind_trtt(struct anv_device *device,
                 sparse_submit->binds_len, trtt_submit.l3l2_binds_len,
                 trtt_submit.l1_binds_len);
 
-   /* TODO: make both the syncs and signals be passed as part of the vm_bind
-    * ioctl so they can be waited asynchronously. For now this doesn't matter
-    * as we're doing synchronous vm_bind, but later when we make it async this
-    * will make a difference.
-    */
-   result = vk_sync_wait_many(&device->vk, sparse_submit->wait_count,
-                              sparse_submit->waits, VK_SYNC_WAIT_COMPLETE,
-                              INT64_MAX);
-   if (result != VK_SUCCESS) {
-      result = vk_queue_set_lost(&sparse_submit->queue->vk,
-                                 "vk_sync_wait failed");
-      goto out;
-   }
-
-   if (trtt_submit.l3l2_binds_len || trtt_submit.l1_binds_len) {
+   if (trtt_submit.l3l2_binds_len || trtt_submit.l1_binds_len)
       result = anv_genX(device->info, write_trtt_entries)(&trtt_submit);
-      if (result != VK_SUCCESS)
-         goto out;
-   }
-
-   for (uint32_t i = 0; i < sparse_submit->signal_count; i++) {
-      struct vk_sync_signal *s = &sparse_submit->signals[i];
-      result = vk_sync_signal(&device->vk, s->sync, s->signal_value);
-      if (result != VK_SUCCESS) {
-         result = vk_queue_set_lost(&sparse_submit->queue->vk,
-                                    "vk_sync_signal failed");
-         goto out;
-      }
-   }
 
 out:
    pthread_mutex_unlock(&trtt->mutex);

alexvorxx/zink-xlib-termux

commit 38af7254e207ebb5303ad97f87b5460392383ef4

anv/xe: don't leak xe_syncs during trtt submission
==134077== 96 bytes in 1 blocks are definitely lost in loss record 1 of 3
==134077==    at 0x4840808: malloc (in /usr/libexec/valgrind/vgpreload_memcheck-amd64-linux.so)
==134077==    by 0x6D6F690: vk_default_alloc (vk_alloc.c:26)
==134077==    by 0x52EEEBE: vk_alloc (vk_alloc.h:48)
==134077==    by 0x52EEEEE: vk_zalloc (vk_alloc.h:56)
==134077==    by 0x52EF47E: xe_exec_process_syncs (anv_batch_chain.c:132)
==134077==    by 0x52EF8F6: xe_execute_trtt_batch (anv_batch_chain.c:215)
==134077==    by 0x5301670: anv_queue_submit_trtt_batch (anv_batch_chain.c:1697)
==134077==    by 0x603D135: gfx125_write_trtt_entries (genX_cmd_buffer.c:6091)
==134077==    by 0x5370B44: anv_sparse_bind_trtt (anv_sparse.c:595)
==134077==    by 0x5370CFC: anv_sparse_bind (anv_sparse.c:629)
==134077==    by 0x5370E6E: anv_init_sparse_bindings (anv_sparse.c:670)
==134077==    by 0x5328037: anv_CreateBuffer (anv_device.c:5071)

Note to backporters: this is only for when xe.ko is being used and
ANV_SPARSE_USE_TRTT=1 is exported. This is not the regular code path.

Fixes: 18bd00c024fc ("anv/trtt: don't wait/signal syncobjs using the CPU anymore")
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Signed-off-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28455>

src/intel/vulkan/xe/anv_batch_chain.c
@@ -201,7 +201,7 @@ xe_execute_trtt_batch(struct anv_sparse_submission *submit,
    struct anv_queue *queue = submit->queue;
    struct anv_device *device = queue->device;
    struct anv_trtt *trtt = &device->trtt;
-   VkResult result;
+   VkResult result = VK_SUCCESS;
 
    struct drm_xe_sync extra_sync = {
       .type = DRM_XE_SYNC_TYPE_TIMELINE_SYNCOBJ,
@@ -230,18 +230,22 @@ xe_execute_trtt_batch(struct anv_sparse_submission *submit,
    };
 
    if (!device->info->no_hw) {
-      if (intel_ioctl(device->fd, DRM_IOCTL_XE_EXEC, &exec))
-         return vk_device_set_lost(&device->vk, "XE_EXEC failed: %m");
+      if (intel_ioctl(device->fd, DRM_IOCTL_XE_EXEC, &exec)) {
+         result = vk_device_set_lost(&device->vk, "XE_EXEC failed: %m");
+         goto out;
+      }
    }
 
    if (queue->sync) {
       result = vk_sync_wait(&device->vk, queue->sync, 0,
                             VK_SYNC_WAIT_COMPLETE, UINT64_MAX);
       if (result != VK_SUCCESS)
-         return vk_queue_set_lost(&queue->vk, "trtt sync wait failed");
+         result = vk_queue_set_lost(&queue->vk, "trtt sync wait failed");
    }
 
-   return VK_SUCCESS;
+out:
+   vk_free(&device->vk.alloc, xe_syncs);
+   return result;
 }
 
 VkResult
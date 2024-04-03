37cafd53ef88d353017b2910796d34b8caa07075
Sil Vilerino <sivileri@microsoft.com>
2024-04-03 08:23:01 -0400

    d3d12: Fix util_blitter_destroy destruction ordering
    Fixes: 55e377e9652 ("d3d12: Add partial media, compute, graphics support with CORE and GENERIC feature levels")
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28546>

--- src/gallium/drivers/d3d12/d3d12_context_common.cpp
+++ src/gallium/drivers/d3d12/d3d12_context_common.cpp
@@ -74,6 +74,14 @@ d3d12_context_destroy(struct pipe_context *pctx)
       screen->context_id_list[screen->context_id_count++] = ctx->id;
    mtx_unlock(&screen->submit_mutex);
 
+
+#ifdef HAVE_GALLIUM_D3D12_GRAPHICS
+   if ((screen->max_feature_level >= D3D_FEATURE_LEVEL_11_0) && !(ctx->flags & PIPE_CONTEXT_MEDIA_ONLY)) {
+      util_blitter_destroy(ctx->blitter); // Must be called before d3d12_destroy_batch
+   }
+#endif // HAVE_GALLIUM_D3D12_GRAPHICS
+
+   // Batch must be destroyed before the rest of the state objects below
    d3d12_end_batch(ctx, d3d12_current_batch(ctx));
    for (unsigned i = 0; i < ARRAY_SIZE(ctx->batches); ++i)
       d3d12_destroy_batch(ctx, &ctx->batches[i]);
@@ -98,7 +106,6 @@ d3d12_context_destroy(struct pipe_context *pctx)
          pctx->destroy_query(pctx, ctx->timestamp_query);
 
       util_unreference_framebuffer_state(&ctx->fb);
-      util_blitter_destroy(ctx->blitter);
       d3d12_compute_pipeline_state_cache_destroy(ctx);
       d3d12_root_signature_cache_destroy(ctx);
       d3d12_cmd_signature_cache_destroy(ctx);

4d832f8433a43a54a4591732d7239de25ff95089
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2024-04-08 18:41:17 -0400

    asahi: add missing rgba4 format
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29179>

--- src/asahi/lib/agx_formats.c
+++ src/asahi/lib/agx_formats.c
@@ -30,6 +30,7 @@ const struct agx_pixel_format_entry agx_pixel_format[PIPE_FORMAT_COUNT] = {
    AGX_FMT(R4G4B4A4_UNORM,          R4G4B4A4,              UNORM, F16),
    AGX_FMT(B4G4R4A4_UNORM,          R4G4B4A4,              UNORM, F16),
    AGX_FMT(A4B4G4R4_UNORM,          R4G4B4A4,              UNORM, F16),
+   AGX_FMT(A4R4G4B4_UNORM,          R4G4B4A4,              UNORM, F16),
 
    AGX_FMT(R8_UNORM,                R8,                    UNORM, U8NORM),
    AGX_FMT(R8G8_UNORM,              R8G8,                  UNORM, U8NORM),

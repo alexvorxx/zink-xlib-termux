alexvorxx/zink-xlib-termux

commit 09e5b3da7659083f655f975e349f0569e2b37d9e

ci: drop unused VK_ICD_FILENAMES passthrough from jobs to tests
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28516>

.gitlab-ci/common/generate-env.sh
@@ -117,7 +117,6 @@ for var in \
     WAFFLE_PLATFORM \
     VK_CPU \
     VK_DRIVER \
-    VK_ICD_FILENAMES \
     VKD3D_PROTON_RESULTS \
     VKD3D_CONFIG \
     VKD3D_TEST_EXCLUDE \

commit f783ca5926c7241b258dccc4a8bd5b79db9e7c8b

ci: Add VK_DRIVER_FILES passthrough from jobs to tests
Allows to specify the driver in use when running Vulkan tests under crosvm.

Note that this is specifically used in virglrenderer CI.

Fixes: 09e5b3da7659083f655f975e349f0569e2b37d9e
Signed-off-by: Corentin Noël <corentin.noel@collabora.com>
Reviewed-by: Eric Engestrom <eric@igalia.com>
Reviewed-by: David Heidelberg <david.heidelberg@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28609>

.gitlab-ci/common/generate-env.sh
@@ -117,6 +117,8 @@ VARS=(
     WAFFLE_PLATFORM
     VK_CPU
     VK_DRIVER
+    # required by virglrender CI
+    VK_DRIVER_FILES
     VKD3D_PROTON_RESULTS
     VKD3D_CONFIG
     VKD3D_TEST_EXCLUDE
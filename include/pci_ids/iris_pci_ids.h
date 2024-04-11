aerisarn/mesa-uwp

commit 48ff68820e83565154d8cfb86587c0134f0deb98

intel/dev: Enable MTL PCI ids
Ref: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/include/drm/i915_pciids.h?h=v6.0-rc4#n736
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/18481>

include/pci_ids/iris_pci_ids.h
@@ -245,8 +245,8 @@ CHIPSET(0x56b3, dg2_g12, "DG2", "Intel(R) Graphics")
 CHIPSET(0x56c0, dg2_g10, "ATS-M", "Intel(R) Data Center GPU Flex Series 170 Graphics")
 CHIPSET(0x56c1, dg2_g11, "ATS-M", "Intel(R) Data Center GPU Flex Series 140 Graphics")
 
-/* CHIPSET(0x7d40, mtl_m, "MTL", "Intel(R) Graphics") */
-/* CHIPSET(0x7d45, mtl_p, "MTL", "Intel(R) Graphics") */
-/* CHIPSET(0x7d55, mtl_p, "MTL", "Intel(R) Graphics") */
-/* CHIPSET(0x7d60, mtl_m, "MTL", "Intel(R) Graphics") */
-/* CHIPSET(0x7dd5, mtl_p, "MTL", "Intel(R) Graphics") */
+CHIPSET(0x7d40, mtl_m, "MTL", "Intel(R) Graphics")
+CHIPSET(0x7d45, mtl_p, "MTL", "Intel(R) Graphics")
+CHIPSET(0x7d55, mtl_p, "MTL", "Intel(R) Graphics")
+CHIPSET(0x7d60, mtl_m, "MTL", "Intel(R) Graphics")
+CHIPSET(0x7dd5, mtl_p, "MTL", "Intel(R) Graphics")

src/intel/dev/intel_device_info.c
@@ -1101,12 +1101,12 @@ static const struct intel_device_info intel_device_info_dg2_g12 = {
    .has_mesh_shading = true,                                    \
    .has_ray_tracing = true
 
-UNUSED static const struct intel_device_info intel_device_info_mtl_m = {
+static const struct intel_device_info intel_device_info_mtl_m = {
    MTL_FEATURES,
    .platform = INTEL_PLATFORM_MTL_M,
 };
 
-UNUSED static const struct intel_device_info intel_device_info_mtl_p = {
+static const struct intel_device_info intel_device_info_mtl_p = {
    MTL_FEATURES,
    .platform = INTEL_PLATFORM_MTL_P,
 };

alexvorxx/zink-xlib-termux

commit 094a0a2ccbdd97e98009b453731d88c43d244b31

intel/dev: 0x7d45 is mtl-u not mtl-h
Ref: https://ark.intel.com/content/www/us/en/ark/products/237327/intel-core-ultra-7-processor-155u-12m-cache-up-to-4-80-ghz.html
Ref: Core Ultra Processor Datasheet, Doc. No.: 792044, Rev.: 002
Fixes: 48ff68820e8 ("intel/dev: Enable MTL PCI ids")
Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27973>

include/pci_ids/iris_pci_ids.h
@@ -258,7 +258,7 @@ CHIPSET(0x56c1, atsm_g11, "ATS-M", "Intel(R) Data Center GPU Flex 140")
 CHIPSET(0x56c2, atsm_g10, "ATS-M", "Intel(R) Data Center GPU Flex 170V")
 
 CHIPSET(0x7d40, mtl_u, "MTL", "Intel(R) Graphics")
-CHIPSET(0x7d45, mtl_h, "MTL", "Intel(R) Graphics")
+CHIPSET(0x7d45, mtl_u, "MTL", "Intel(R) Graphics")
 CHIPSET(0x7d55, mtl_h, "MTL", "Intel(R) Arc(tm) Graphics")
 CHIPSET(0x7d60, mtl_u, "MTL", "Intel(R) Graphics")
 CHIPSET(0x7dd5, mtl_h, "MTL", "Intel(R) Graphics")
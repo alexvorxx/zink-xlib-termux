aerisarn/mesa-uwp

commit 12a47b84b7386e1aac6c4b0d36ad198705432378

egl/dri2: trigger drawable invalidation from surface queries for zink
this mimics dri3 behavior and avoids scenarios where renderbuffers can
get out of sync with their resources

fixes #6744

Acked-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22858>

src/egl/drivers/dri2/egl_dri2.c
@@ -1031,7 +1031,7 @@ dri2_setup_extensions(_EGLDisplay *disp)
 
    extensions = dri2_dpy->core->getExtensions(dri2_dpy->dri_screen_render_gpu);
 
-   if (dri2_dpy->image_driver || dri2_dpy->dri2) {
+   if (dri2_dpy->image_driver || dri2_dpy->dri2 || disp->Options.Zink) {
       if (!loader_bind_extensions(dri2_dpy, dri2_core_extensions, ARRAY_SIZE(dri2_core_extensions), extensions))
          return EGL_FALSE;
    } else {

src/egl/drivers/dri2/platform_x11.c
@@ -464,8 +464,11 @@ dri2_query_surface(_EGLDisplay *disp, _EGLSurface *surf,
    case EGL_WIDTH:
    case EGL_HEIGHT:
       if (x11_get_drawable_info(drawable, &x, &y, &w, &h, dri2_surf)) {
+         bool changed = surf->Width != w || surf->Height != h;
          surf->Width = w;
          surf->Height = h;
+         if (changed && dri2_dpy->flush)
+            dri2_dpy->flush->invalidate(drawable);
       }
       break;
    default:

alexvorxx/zink-xlib-termux

commit 223aedfa5dba263101a91314186be80861dbd3cf

egl/dri2: don't bind dri2 for zink
I'm not sure why zink would want dri2 here instead of kopper,
I'm sure it's some side effect of something else, let zink
use the kopper paths.

This fixes:
dEQP-GL45-ES3.info.vendor
on zink on nvk with GL cts using EGL.

Reviewed-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Fixes: 12a47b84b738 ("egl/dri2: trigger drawable invalidation from surface queries for zink")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28707>

src/egl/drivers/dri2/egl_dri2.c
@@ -948,7 +948,7 @@ dri2_setup_extensions(_EGLDisplay *disp)
 
    extensions = dri2_dpy->core->getExtensions(dri2_dpy->dri_screen_render_gpu);
 
-   if (dri2_dpy->image_driver || dri2_dpy->dri2 || disp->Options.Zink) {
+   if (dri2_dpy->image_driver || dri2_dpy->dri2) {
       if (!loader_bind_extensions(dri2_dpy, dri2_core_extensions,
                                   ARRAY_SIZE(dri2_core_extensions), extensions))
          return EGL_FALSE;
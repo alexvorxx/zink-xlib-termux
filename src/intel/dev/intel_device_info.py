90b223331f1a5b2d787fce4b9e470f83552774fc
José Roberto de Souza <jose.souza@intel.com>
2024-04-16 07:43:32 -0700

    intel/dev: Add compressed PAT entry
    This will be used in Xe2+ to store images compressed in memory.
    
    Still missing add the compressed PAT index and attributes to
    LNL intel_device_info.
    
    Reviewed-by: Rohan Garg <rohan.garg@intel.com>
    Signed-off-by: José Roberto de Souza <jose.souza@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28833>

--- src/intel/dev/intel_device_info.py
+++ src/intel/dev/intel_device_info.py
@@ -229,6 +229,9 @@ Struct("intel_device_info_pat_desc",
         Member("intel_device_info_pat_entry", "scanout",
                comment="scanout and external BOs"),
 
+        Member("intel_device_info_pat_entry", "compressed",
+               comment="Only supported in Xe2, compressed + WC"),
+
         Member("intel_device_info_pat_entry", "writeback_incoherent",
                comment=("BOs without special needs, can be WB not coherent "
                         "or WC it depends on the platforms and KMD")),

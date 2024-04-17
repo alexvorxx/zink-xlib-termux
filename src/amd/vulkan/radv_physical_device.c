763b4e992c7f8963ab7b5f34021852ac5167fb27
Joshua Ashton <joshua@froggi.es>
2024-04-17 04:18:05 +0100

    radv: Enable KHR_video_queue if encode is enabled
    KHR_video_encode_queue is dependent on KHR_video_queue.
    
    Not exposing this makes encode-only usecases not functional,
    eg. SteamVR Link.
    
    Signed-off-by: Joshua Ashton <joshua@froggi.es>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28779>

--- src/amd/vulkan/radv_physical_device.c
+++ src/amd/vulkan/radv_physical_device.c
@@ -569,7 +569,7 @@ radv_physical_device_get_supported_extensions(const struct radv_physical_device 
       .KHR_uniform_buffer_standard_layout = true,
       .KHR_variable_pointers = true,
       .KHR_vertex_attribute_divisor = true,
-      .KHR_video_queue = !!(instance->perftest_flags & RADV_PERFTEST_VIDEO_DECODE),
+      .KHR_video_queue = !!(instance->perftest_flags & RADV_PERFTEST_VIDEO_DECODE) || pdev->video_encode_enabled,
       .KHR_video_decode_av1 = (pdev->info.vcn_ip_version >= VCN_3_0_0 && pdev->info.vcn_ip_version != VCN_3_0_33 &&
                                VIDEO_CODEC_AV1DEC && !!(instance->perftest_flags & RADV_PERFTEST_VIDEO_DECODE)),
       .KHR_video_decode_queue = !!(instance->perftest_flags & RADV_PERFTEST_VIDEO_DECODE),

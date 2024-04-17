1f07f5a79b4b09eee58594fffd6ebe61bdf32c5d
David Rosca <nowrep@gmail.com>
2024-04-17 09:19:03 +0200

    radv/video: Report maxBitrate in encode capabilities
    Some cards can do higher bitrate, but 1000 Mbit/s should be high enough
    for any practical use. It's also the value that AMF reports as max bitrate.
    
    Fixes: 54d499818ca ("radv/video: add initial support for encoding with h264.")
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28736>

--- src/amd/vulkan/radv_video.c
+++ src/amd/vulkan/radv_video.c
@@ -559,7 +559,7 @@ radv_GetPhysicalDeviceVideoCapabilitiesKHR(VkPhysicalDevice physicalDevice, cons
                                       VK_VIDEO_ENCODE_RATE_CONTROL_MODE_CBR_BIT_KHR |
                                       VK_VIDEO_ENCODE_RATE_CONTROL_MODE_VBR_BIT_KHR;
          enc_caps->maxRateControlLayers = RADV_ENC_MAX_RATE_LAYER;
-         enc_caps->maxBitrate = 0;
+         enc_caps->maxBitrate = 1000000000;
          enc_caps->maxQualityLevels = 2;
          enc_caps->encodeInputPictureGranularity.width = 1;
          enc_caps->encodeInputPictureGranularity.height = 1;

alexvorxx/zink-xlib-termux

commit dbfb96f08f82ab847163128868166f2a93e85f9e

radv: Remove I/O variables after nir_lower_io.
They are not needed anymore.

Both NIR and RADV shader info can be fully deduced from I/O
intrinsics, so there is no need to keep the variables.

Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28506>

src/amd/vulkan/nir/radv_nir_lower_io.c
@@ -103,6 +103,8 @@ radv_nir_lower_io(struct radv_device *device, nir_shader *nir)
        */
       nir_recompute_io_bases(nir, nir_var_shader_in);
    }
+
+   NIR_PASS_V(nir, nir_remove_dead_variables, nir_var_shader_in | nir_var_shader_out, NULL);
 }
 
 /* IO slot layout for stages that aren't linked. */

commit afb08ee244bafb54795cd864d3d0fb210d39bee1

radv: Run DCE before deleting I/O variables.
Apparently, nir_lower_io leaves dead code in shaders, which
prevented us from deleting the IO variables properly.

Fixes: dbfb96f08f82ab847163128868166f2a93e85f9e
Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>
Reviewed-by: Georg Lehmann <dadschoorse@gmail.com>
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28764>

src/amd/vulkan/nir/radv_nir_lower_io.c
@@ -87,6 +87,7 @@ radv_nir_lower_io(struct radv_device *device, nir_shader *nir)
       nir_recompute_io_bases(nir, nir_var_shader_in);
    }
 
+   NIR_PASS_V(nir, nir_opt_dce);
    NIR_PASS_V(nir, nir_remove_dead_variables, nir_var_shader_in | nir_var_shader_out, NULL);
 }
 

src/amd/compiler/tests/test_isel.cpp
@@ -21,14 +21,14 @@ BEGIN_TEST(isel.interp.simple)
       layout(location = 0) in vec4 in_color;
       layout(location = 0) out vec4 out_color;
       void main() {
-         //>> v1: %a_tmp = v_interp_p1_f32 %bx, %pm:m0 attr0.w
+         //>> v1: %b_tmp = v_interp_p1_f32 %bx, %pm:m0 attr0.z
+         //! v1: %b = v_interp_p2_f32 %by, %pm:m0, (kill)%b_tmp attr0.z
+         //! v1: %a_tmp = v_interp_p1_f32 %bx, %pm:m0 attr0.w
          //! v1: %a = v_interp_p2_f32 %by, %pm:m0, (kill)%a_tmp attr0.w
          //! v1: %r_tmp = v_interp_p1_f32 %bx, %pm:m0 attr0.x
          //! v1: %r = v_interp_p2_f32 %by, %pm:m0, (kill)%r_tmp attr0.x
-         //! v1: %g_tmp = v_interp_p1_f32 %bx, %pm:m0 attr0.y
-         //! v1: %g = v_interp_p2_f32 %by, %pm:m0, (kill)%g_tmp attr0.y
-         //! v1: %b_tmp = v_interp_p1_f32 (kill)%bx, %pm:m0 attr0.z
-         //! v1: %b = v_interp_p2_f32 (kill)%by, (kill)%pm:m0, (kill)%b_tmp attr0.z
+         //! v1: %g_tmp = v_interp_p1_f32 (kill)%bx, %pm:m0 attr0.y
+         //! v1: %g = v_interp_p2_f32 (kill)%by, (kill)%pm:m0, (kill)%g_tmp attr0.y
          //! exp (kill)%r, (kill)%g, (kill)%b, (kill)%a mrt0
          out_color = in_color;
       }
599e8bf9212a1ef2f85a62ce48339aea0d365072
Martin Roukala (né Peres) <martin.roukala@mupuf.org>
2024-04-22 14:54:53 +0300

    ci/valve: remove the traces runner
    This script is severely outdated and has had no use in literal years.
    
    Signed-off-by: Martin Roukala (né Peres) <martin.roukala@mupuf.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28902>

--- .gitlab-ci/prepare-artifacts.sh
+++ .gitlab-ci/prepare-artifacts.sh
@@ -38,7 +38,6 @@ cp -Rp .gitlab-ci/fossilize-runner.sh install/
 cp -Rp .gitlab-ci/crosvm-init.sh install/
 cp -Rp .gitlab-ci/*.txt install/
 cp -Rp .gitlab-ci/report-flakes.py install/
-cp -Rp .gitlab-ci/valve install/
 cp -Rp .gitlab-ci/vkd3d-proton install/
 cp -Rp .gitlab-ci/setup-test-env.sh install/
 cp -Rp .gitlab-ci/*-runner.sh install/

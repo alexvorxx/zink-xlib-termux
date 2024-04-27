1dfb46155212354e0be4f7ffdff5536aa774b544
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2024-04-27 11:33:53 -0400

    asahi: add AGX_TEXTURE_FLAG_CLAMP_TO_0 flag
    for border colour emulation
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29607>

--- src/asahi/lib/agx_nir_passes.h
+++ src/asahi/lib/agx_nir_passes.h
@@ -16,6 +16,9 @@ struct nir_instr;
 /* Texture backend flags */
 #define AGX_TEXTURE_FLAG_NO_CLAMP (1 << 0)
 
+/* Indicates that the sampler should be overriden to clamp to 0 instead of 1 */
+#define AGX_TEXTURE_FLAG_CLAMP_TO_0 (1 << 1)
+
 /* Texel buffers lowered to (at most) 16384x16384 2D textures */
 #define AGX_TEXTURE_BUFFER_WIDTH      16384
 #define AGX_TEXTURE_BUFFER_MAX_HEIGHT 16384

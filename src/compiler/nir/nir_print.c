alexvorxx/zink-xlib-termux

commit a83fd26d0effc8e65ff601c2ad3314058b98f107

nir/print: stop trying to match i/o vars using base/driver_location
this is broken

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28552>

src/compiler/nir/nir_print.c
@@ -1632,12 +1632,15 @@ print_intrinsic_instr(nir_intrinsic_instr *instr, print_state *state)
    }
 
    nir_foreach_variable_with_modes(var, state->shader, var_mode) {
-      if ((var->data.driver_location == nir_intrinsic_base(instr)) &&
-          (instr->intrinsic == nir_intrinsic_load_uniform ||
-           (nir_intrinsic_component(instr) >= var->data.location_frac &&
+      if (!var->name)
+         continue;
+      if (((instr->intrinsic == nir_intrinsic_load_uniform &&
+            var->data.driver_location == nir_intrinsic_base(instr)) ||
+           (instr->intrinsic != nir_intrinsic_load_uniform &&
+            var->data.location == nir_intrinsic_io_semantics(instr).location)) &&
+          ((nir_intrinsic_component(instr) >= var->data.location_frac &&
             nir_intrinsic_component(instr) <
-               (var->data.location_frac + glsl_get_components(var->type)))) &&
-          var->name) {
+               (var->data.location_frac + glsl_get_components(var->type))))) {
          fprintf(fp, "  // %s", var->name);
          break;
       }

commit ea863c0c1ccbc26ad6f9e1e6fbc834ebcd85bc3c

nir/print: Do not access invalid indices of load_uniform
load_uniform does not have io_semantics and component.

Fixes: a83fd26 ("nir/print: stop trying to match i/o vars using base/driver_location")
Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Reviewed-by: Christian Gmeiner <cgmeiner@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28962>

src/compiler/nir/nir_print.c
@@ -1639,13 +1639,17 @@ print_intrinsic_instr(nir_intrinsic_instr *instr, print_state *state)
    nir_foreach_variable_with_modes(var, state->shader, var_mode) {
       if (!var->name)
          continue;
-      if (((instr->intrinsic == nir_intrinsic_load_uniform &&
-            var->data.driver_location == nir_intrinsic_base(instr)) ||
-           (instr->intrinsic != nir_intrinsic_load_uniform &&
-            var->data.location == nir_intrinsic_io_semantics(instr).location)) &&
-          ((nir_intrinsic_component(instr) >= var->data.location_frac &&
-            nir_intrinsic_component(instr) <
-               (var->data.location_frac + glsl_get_components(var->type))))) {
+      
+      bool match;
+      if (instr->intrinsic == nir_intrinsic_load_uniform) {
+         match = var->data.driver_location == nir_intrinsic_base(instr);
+      } else {
+         match = nir_intrinsic_component(instr) >= var->data.location_frac &&
+                 nir_intrinsic_component(instr) <
+                    (var->data.location_frac + glsl_get_components(var->type));
+      }
+
+      if (match) {
          fprintf(fp, "  // %s", var->name);
          break;
       }
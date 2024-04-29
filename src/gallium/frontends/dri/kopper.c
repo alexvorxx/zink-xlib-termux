alexvorxx/zink-xlib-termux

commit 2a8c6cf7ac4cb88149ca8e99ba163fcecc055032

kopper: set drawable buffer age
this fixes buffer age reporting with lavapipe

Acked-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27703>

src/gallium/frontends/dri/kopper.c
@@ -420,6 +420,8 @@ kopper_allocate_textures(struct dri_context *ctx,
 
    resized = (drawable->old_w != width ||
               drawable->old_h != height);
+   if (resized)
+      drawable->buffer_age = 0;
 
    /* Wait for glthread to finish because we can't use pipe_context from
     * multiple threads.
@@ -837,6 +839,7 @@ kopperSwapBuffersWithDamage(__DRIdrawable *dPriv, uint32_t flush_flags, int nrec
    }
 
    kopper_copy_to_front(ctx->st->pipe, drawable, ptex, nrects, stack_boxes);
+   drawable->buffer_age = 1;
    if (drawable->is_window && !zink_kopper_check(ptex))
       return -1;
    if (!drawable->textures[ST_ATTACHMENT_FRONT_LEFT]) {

commit ad39355e837829a9d3ed36c3e60cce55a131d0b6

kopper: don't set drawable buffer age
this is broken

Fixes: 2a8c6cf7ac4 ("kopper: set drawable buffer age")

Acked-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28904>

src/gallium/frontends/dri/kopper.c
@@ -428,8 +428,6 @@ kopper_allocate_textures(struct dri_context *ctx,
 
    resized = (drawable->old_w != width ||
               drawable->old_h != height);
-   if (resized)
-      drawable->buffer_age = 0;
 
    /* Wait for glthread to finish because we can't use pipe_context from
     * multiple threads.
@@ -845,7 +843,6 @@ kopperSwapBuffersWithDamage(__DRIdrawable *dPriv, uint32_t flush_flags, int nrec
    }
 
    kopper_copy_to_front(ctx->st->pipe, drawable, ptex, nrects, stack_boxes);
-   drawable->buffer_age = 1;
    if (drawable->is_window && !zink_kopper_check(ptex))
       return -1;
    if (!drawable->textures[ST_ATTACHMENT_FRONT_LEFT]) {
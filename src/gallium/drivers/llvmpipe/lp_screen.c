alexvorxx/zink-xlib-termux

commit c7634c25e4fe78cf993de65dd184d7155564eca2

llvmpipe: Fix build error with clang-18
We don't actually care about the truncation, so tell clang to stfu.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/10973
Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29110>

src/gallium/drivers/llvmpipe/lp_screen.c
@@ -598,7 +598,13 @@ static void
 llvmpipe_get_device_uuid(struct pipe_screen *pscreen, char *uuid)
 {
    memset(uuid, 0, PIPE_UUID_SIZE);
+#pragma GCC diagnostic push
+#if defined(__clang__)
+#pragma GCC diagnostic ignored "-Wunknown-warning-option"
+#pragma GCC diagnostic ignored "-Wformat-truncation"
+#endif /* __clang__ */
    snprintf(uuid, PIPE_UUID_SIZE, "mesa" PACKAGE_VERSION);
+#pragma GCC diagnostic pop
 }
 
 

commit 32f2b5d245fcaadc540cbac9f4780cc199b1ae0d

llvmpipe: wrap the push/pull in the ifdef as well
Fixes: c7634c25e4fe78cf993d ("llvmpipe: Fix build error with clang-18")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29138>

src/gallium/drivers/llvmpipe/lp_screen.c
@@ -598,13 +598,15 @@ static void
 llvmpipe_get_device_uuid(struct pipe_screen *pscreen, char *uuid)
 {
    memset(uuid, 0, PIPE_UUID_SIZE);
-#pragma GCC diagnostic push
 #if defined(__clang__)
+#pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunknown-warning-option"
 #pragma GCC diagnostic ignored "-Wformat-truncation"
 #endif /* __clang__ */
    snprintf(uuid, PIPE_UUID_SIZE, "mesa" PACKAGE_VERSION);
+#if defined(__clang__)
 #pragma GCC diagnostic pop
+#endif /* __clang__ */
 }
 
 
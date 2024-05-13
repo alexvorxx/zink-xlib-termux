7dd2b9e11ae860607a533b0fbf71c51496063089
Eric Engestrom <eric@igalia.com>
2024-05-13 17:09:22 +0200

    ci: hardcode `-vkd3d` namespace for VKD3D_PROTON_RESULTS
    This means we can rely on these files always having that name.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29170>

--- .gitlab-ci/vkd3d-proton/run.sh
+++ .gitlab-ci/vkd3d-proton/run.sh
@@ -67,8 +67,8 @@ then
     grep "Test failed" "$RESULTS"/vkd3d-proton.log > "$RESULTSFILE"
 
     # Gather the list expected failures
-    if [ -f "$INSTALL/$VKD3D_PROTON_RESULTS.txt" ]; then
-        cp "$INSTALL/$VKD3D_PROTON_RESULTS.txt" \
+    if [ -f "$INSTALL/$VKD3D_PROTON_RESULTS-vkd3d.txt" ]; then
+        cp "$INSTALL/$VKD3D_PROTON_RESULTS-vkd3d.txt" \
            ".gitlab-ci/vkd3d-proton/$VKD3D_PROTON_RESULTS.txt.baseline"
     else
         touch ".gitlab-ci/vkd3d-proton/$VKD3D_PROTON_RESULTS.txt.baseline"

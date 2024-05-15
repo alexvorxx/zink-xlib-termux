alexvorxx/zink-xlib-termux

commit 660a47ecbf693601c25660b5fac87121ff0fe8bc

tu: support VK_EXT_legacy_vertex_attributes
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29086>

src/freedreno/vulkan/tu_device.cc
@@ -242,6 +242,7 @@ get_device_extensions(const struct tu_physical_device *device,
       .EXT_image_view_min_lod = true,
       .EXT_index_type_uint8 = true,
       .EXT_inline_uniform_block = true,
+      .EXT_legacy_vertex_attributes = true,
       .EXT_line_rasterization = true,
       .EXT_load_store_op_none = true,
       .EXT_map_memory_placed = true,
@@ -1066,6 +1067,9 @@ tu_get_properties(struct tu_physical_device *pdevice,
    props->resourceDescriptorBufferAddressSpaceSize = ~0ull;
    props->descriptorBufferAddressSpaceSize = ~0ull;
    props->combinedImageSamplerDensityMapDescriptorSize = 2 * A6XX_TEX_CONST_DWORDS * 4;
+
+   /* VK_EXT_legacy_vertex_attributes */
+   props->nativeUnalignedPerformance = true,
 
    /* VK_EXT_fragment_density_map*/
    props->minFragmentDensityTexelSize = (VkExtent2D) { MIN_FDM_TEXEL_SIZE, MIN_FDM_TEXEL_SIZE };

commit 03d8620c4cd34de0181fac75a7345ab44a0eb87d

tu: Add missing VK_EXT_legacy_vertex_attributes feature
This was missed during enablement.

Fixes: 660a47ecbf69 ("tu: support VK_EXT_legacy_vertex_attributes")

Signed-off-by: Valentine Burley <valentine.burley@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29206>

src/freedreno/vulkan/tu_device.cc
@@ -566,6 +566,9 @@ tu_get_features(struct tu_physical_device *pdevice,
    /* VK_EXT_image_view_min_lod */
    features->minLod = true;
 
+   /* VK_EXT_legacy_vertex_attributes */
+   features->legacyVertexAttributes = true;
+
    /* VK_EXT_map_memory_placed */
    features->memoryMapPlaced = true,
    features->memoryMapRangePlaced = false,
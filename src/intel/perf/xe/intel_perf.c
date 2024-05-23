7b5bf6dcca7027c112f39b7515043a779fdc593e
José Roberto de Souza <jose.souza@intel.com>
2024-05-23 14:07:19 -0700

    intel/perf: Return LNL OA sample format
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Signed-off-by: José Roberto de Souza <jose.souza@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29529>

--- src/intel/perf/xe/intel_perf.c
+++ src/intel/perf/xe/intel_perf.c
@@ -18,15 +18,27 @@
 
 uint64_t xe_perf_get_oa_format(struct intel_perf_config *perf)
 {
-   uint64_t fmt = FIELD_PREP_ULL(DRM_XE_OA_FORMAT_MASK_FMT_TYPE, DRM_XE_OA_FMT_TYPE_OAG);
+   uint64_t fmt;
 
-   /* same as I915_OA_FORMAT_A24u40_A14u32_B8_C8 and
-    * I915_OA_FORMAT_A32u40_A4u32_B8_C8 returned for gfx 125+ and gfx 120
-    * respectively.
-    */
-   fmt |= FIELD_PREP_ULL(DRM_XE_OA_FORMAT_MASK_COUNTER_SEL, 5);
-   fmt |= FIELD_PREP_ULL(DRM_XE_OA_FORMAT_MASK_COUNTER_SIZE, 0);
-   fmt |= FIELD_PREP_ULL(DRM_XE_OA_FORMAT_MASK_BC_REPORT, 0);
+   if (perf->devinfo->verx10 >= 200) {
+      /* BSpec: 60942
+       * PEC64u64
+       */
+      fmt = FIELD_PREP_ULL(DRM_XE_OA_FORMAT_MASK_FMT_TYPE, DRM_XE_OA_FMT_TYPE_PEC);
+      fmt |= FIELD_PREP_ULL(DRM_XE_OA_FORMAT_MASK_COUNTER_SEL, 1);
+      fmt |= FIELD_PREP_ULL(DRM_XE_OA_FORMAT_MASK_COUNTER_SIZE, 1);
+      fmt |= FIELD_PREP_ULL(DRM_XE_OA_FORMAT_MASK_BC_REPORT, 0);
+   } else {
+      /* BSpec: 52198
+       * same as I915_OA_FORMAT_A24u40_A14u32_B8_C8 and
+       * I915_OA_FORMAT_A32u40_A4u32_B8_C8 returned for gfx 125+ and gfx 120
+       * respectively.
+       */
+      fmt = FIELD_PREP_ULL(DRM_XE_OA_FORMAT_MASK_FMT_TYPE, DRM_XE_OA_FMT_TYPE_OAG);
+      fmt |= FIELD_PREP_ULL(DRM_XE_OA_FORMAT_MASK_COUNTER_SEL, 5);
+      fmt |= FIELD_PREP_ULL(DRM_XE_OA_FORMAT_MASK_COUNTER_SIZE, 0);
+      fmt |= FIELD_PREP_ULL(DRM_XE_OA_FORMAT_MASK_BC_REPORT, 0);
+   }
 
    return fmt;
 }

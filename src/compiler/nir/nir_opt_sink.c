d5da4348517fa93581614f8d10accc25641c8bac
Karol Herbst <kherbst@redhat.com>
2024-05-28 23:43:29 +0200

    nir/opt_sink: add load_kernel_input
    Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25362>

--- src/compiler/nir/nir_opt_sink.c
+++ src/compiler/nir/nir_opt_sink.c
@@ -112,6 +112,7 @@ nir_can_move_instr(nir_instr *instr, nir_move_options options)
       case nir_intrinsic_load_pixel_coord:
          return options & nir_move_load_input;
       case nir_intrinsic_load_uniform:
+      case nir_intrinsic_load_kernel_input:
          return options & nir_move_load_uniform;
       case nir_intrinsic_inverse_ballot:
          return options & nir_move_copies;

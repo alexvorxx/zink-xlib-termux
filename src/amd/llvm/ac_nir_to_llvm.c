3dfc8b3bcf40358b28a96304f4a32ef7eaf9790d
Georg Lehmann <dadschoorse@gmail.com>
2024-05-29 15:12:01 +0200

    ac/llvm: implement ford, funord, fneo, fequ, fltu, fgeu
    Reviewed-by: Timur Kristóf <timur.kristof@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29467>

--- src/amd/llvm/ac_nir_to_llvm.c
+++ src/amd/llvm/ac_nir_to_llvm.c
@@ -689,11 +689,29 @@ static bool visit_alu(struct ac_nir_context *ctx, const nir_alu_instr *instr)
    case nir_op_fneu:
       result = emit_float_cmp(&ctx->ac, LLVMRealUNE, src[0], src[1]);
       break;
+   case nir_op_fequ:
+      result = emit_float_cmp(&ctx->ac, LLVMRealUEQ, src[0], src[1]);
+      break;
+   case nir_op_fneo:
+      result = emit_float_cmp(&ctx->ac, LLVMRealONE, src[0], src[1]);
+      break;
    case nir_op_flt:
       result = emit_float_cmp(&ctx->ac, LLVMRealOLT, src[0], src[1]);
       break;
    case nir_op_fge:
       result = emit_float_cmp(&ctx->ac, LLVMRealOGE, src[0], src[1]);
+      break;
+   case nir_op_fltu:
+      result = emit_float_cmp(&ctx->ac, LLVMRealULT, src[0], src[1]);
+      break;
+   case nir_op_fgeu:
+      result = emit_float_cmp(&ctx->ac, LLVMRealUGE, src[0], src[1]);
+      break;
+   case nir_op_funord:
+      result = emit_float_cmp(&ctx->ac, LLVMRealUNO, src[0], src[1]);
+      break;
+   case nir_op_ford:
+      result = emit_float_cmp(&ctx->ac, LLVMRealORD, src[0], src[1]);
       break;
    case nir_op_fabs:
       result =

3260d6c87748ba1f644e607a472e5c3badfa515b
Leo Liu <leo.liu@amd.com>
2024-05-31 13:13:15 -0400

    radeon/vcn: enable dpb to use pipe video buffer with swizzle mode
    Signed-off-by: Leo Liu <leo.liu@amd.com>
    Reviewed-by: Ruijing Dong <ruijing.dong@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29541>

--- src/gallium/drivers/radeonsi/radeon_vcn_dec.c
+++ src/gallium/drivers/radeonsi/radeon_vcn_dec.c
@@ -3437,6 +3437,8 @@ struct pipe_video_codec *radeon_create_decoder(struct pipe_context *context,
       dec->jpg_reg.version = RDECODE_JPEG_REG_VER_V3;
       dec->addr_gfx_mode = RDECODE_ARRAY_MODE_ADDRLIB_SEL_GFX11;
       dec->av1_version = RDECODE_AV1_VER_1;
+      if (radeon_has_video_preferred_gfx12_swizzle_mode(dec, &dec->dpb_modifier))
+         dec->dpb_use_surf = true;
       break;
    default:
       RVID_ERR("VCN is not supported.\n");

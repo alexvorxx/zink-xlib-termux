2d4e4450999d7632fab11aeda0450ce79d95da0d
Faith Ekstrand <faith.ekstrand@collabora.com>
2024-06-03 18:30:04 -0500

    nak/calc_instr_deps: Rewrite calc_delays() again
    This time we take into account WaR and WaW dependencies and not just RaW
    dependencies.  The NVIDIA ISA is actually quite dynamic and the not
    everything is nicely pipelined such that writes always happen at
    consistent cycles.  There are exact rules, of course, but we don't know
    what those are so we need to make some worst-case assumptions.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29591>

--- src/nouveau/compiler/nak/ir.rs
+++ src/nouveau/compiler/nak/ir.rs
@@ -5832,39 +5832,6 @@ impl Instr {
         }
     }
 
-    /// Minimum latency before another instruction can execute
-    pub fn get_exec_latency(&self, sm: u8) -> u32 {
-        match &self.op {
-            Op::Bar(_) | Op::MemBar(_) => {
-                if sm >= 80 {
-                    6
-                } else {
-                    5
-                }
-            }
-            Op::CCtl(_op) => {
-                // CCTL.C needs 8, CCTL.I needs 11
-                11
-            }
-            // Op::DepBar(_) => 4,
-            _ => 1, // TODO: co-issue
-        }
-    }
-
-    pub fn get_dst_latency(&self, sm: u8, dst_idx: usize) -> u32 {
-        debug_assert!(self.has_fixed_latency(sm));
-        let file = match self.dsts()[dst_idx] {
-            Dst::None => return 0,
-            Dst::SSA(vec) => vec.file().unwrap(),
-            Dst::Reg(reg) => reg.file(),
-        };
-        if file.is_predicate() {
-            13
-        } else {
-            6
-        }
-    }
-
     pub fn needs_yield(&self) -> bool {
         matches!(&self.op, Op::Bar(_) | Op::BSync(_))
     }

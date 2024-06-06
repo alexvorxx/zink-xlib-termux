alexvorxx/zink-xlib-termux

commit 4f26303dbb16d667e0073f8aef62cb97330f251d

v3dv: add debug option to disable custom pipeline caches for meta operations
Included as a new option of the existing V3DV_ENABLE_PIPELINE_CACHE
environment variable.

Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29218>

src/broadcom/vulkan/v3dv_meta_clear.c
@@ -278,15 +278,16 @@ create_depth_clear_pipeline_layout(struct v3dv_device *device,
 void
 v3dv_meta_clear_init(struct v3dv_device *device)
 {
-   device->meta.color_clear.cache =
-      _mesa_hash_table_create(NULL, u64_hash, u64_compare);
+   if (device->instance->meta_cache_enabled) {
+      device->meta.color_clear.cache =
+         _mesa_hash_table_create(NULL, u64_hash, u64_compare);
+
+      device->meta.depth_clear.cache =
+         _mesa_hash_table_create(NULL, u64_hash, u64_compare);
+   }
 
    create_color_clear_pipeline_layout(device,
                                       &device->meta.color_clear.p_layout);
-
-   device->meta.depth_clear.cache =
-      _mesa_hash_table_create(NULL, u64_hash, u64_compare);
-
    create_depth_clear_pipeline_layout(device,
                                       &device->meta.depth_clear.p_layout);
 }
@@ -296,22 +297,24 @@ v3dv_meta_clear_finish(struct v3dv_device *device)
 {
    VkDevice _device = v3dv_device_to_handle(device);
 
-   hash_table_foreach(device->meta.color_clear.cache, entry) {
-      struct v3dv_meta_color_clear_pipeline *item = entry->data;
-      destroy_color_clear_pipeline(_device, (uintptr_t)item, &device->vk.alloc);
+   if (device->instance->meta_cache_enabled) {
+      hash_table_foreach(device->meta.color_clear.cache, entry) {
+         struct v3dv_meta_color_clear_pipeline *item = entry->data;
+         destroy_color_clear_pipeline(_device, (uintptr_t)item, &device->vk.alloc);
+      }
+      _mesa_hash_table_destroy(device->meta.color_clear.cache, NULL);
+
+      hash_table_foreach(device->meta.depth_clear.cache, entry) {
+         struct v3dv_meta_depth_clear_pipeline *item = entry->data;
+         destroy_depth_clear_pipeline(_device, item, &device->vk.alloc);
+      }
+      _mesa_hash_table_destroy(device->meta.depth_clear.cache, NULL);
    }
-   _mesa_hash_table_destroy(device->meta.color_clear.cache, NULL);
 
    if (device->meta.color_clear.p_layout) {
       v3dv_DestroyPipelineLayout(_device, device->meta.color_clear.p_layout,
                                  &device->vk.alloc);
    }
-
-   hash_table_foreach(device->meta.depth_clear.cache, entry) {
-      struct v3dv_meta_depth_clear_pipeline *item = entry->data;
-      destroy_depth_clear_pipeline(_device, item, &device->vk.alloc);
-   }
-   _mesa_hash_table_destroy(device->meta.depth_clear.cache, NULL);
 
    if (device->meta.depth_clear.p_layout) {
       v3dv_DestroyPipelineLayout(_device, device->meta.depth_clear.p_layout,
@@ -826,7 +829,8 @@ get_color_clear_pipeline(struct v3dv_device *device,
     * vkQuake3, the fact that we are not caching here doesn't seem to have
     * any significant impact in performance, so it might not be worth it.
     */
-   const bool can_cache_pipeline = (pass == NULL);
+   const bool can_cache_pipeline =
+      (pass == NULL) && (device->instance->meta_cache_enabled);
 
    uint64_t key;
    if (can_cache_pipeline) {
@@ -924,15 +928,18 @@ get_depth_clear_pipeline(struct v3dv_device *device,
    const VkFormat format = pass->attachments[attachment_idx].desc.format;
    assert(vk_format_is_depth_or_stencil(format));
 
-   const uint64_t key =
-      get_depth_clear_pipeline_cache_key(aspects, format, samples, is_layered);
-   mtx_lock(&device->meta.mtx);
-   struct hash_entry *entry =
-      _mesa_hash_table_search(device->meta.depth_clear.cache, &key);
-   if (entry) {
-      mtx_unlock(&device->meta.mtx);
-      *pipeline = entry->data;
-      return VK_SUCCESS;
+   uint64_t key;
+   if (device->instance->meta_cache_enabled) {
+      key = get_depth_clear_pipeline_cache_key(aspects, format, samples,
+                                               is_layered);
+      mtx_lock(&device->meta.mtx);
+      struct hash_entry *entry =
+         _mesa_hash_table_search(device->meta.depth_clear.cache, &key);
+      if (entry) {
+         mtx_unlock(&device->meta.mtx);
+         *pipeline = entry->data;
+         return VK_SUCCESS;
+      }
    }
 
    *pipeline = vk_zalloc2(&device->vk.alloc, NULL, sizeof(**pipeline), 8,
@@ -954,15 +961,18 @@ get_depth_clear_pipeline(struct v3dv_device *device,
    if (result != VK_SUCCESS)
       goto fail;
 
-   (*pipeline)->key = key;
-   _mesa_hash_table_insert(device->meta.depth_clear.cache,
-                           &(*pipeline)->key, *pipeline);
+   if (device->instance->meta_cache_enabled) {
+      (*pipeline)->key = key;
+      _mesa_hash_table_insert(device->meta.depth_clear.cache,
+                              &(*pipeline)->key, *pipeline);
+      mtx_unlock(&device->meta.mtx);
+   }
 
-   mtx_unlock(&device->meta.mtx);
    return VK_SUCCESS;
 
 fail:
-   mtx_unlock(&device->meta.mtx);
+   if (device->instance->meta_cache_enabled)
+      mtx_unlock(&device->meta.mtx);
 
    VkDevice _device = v3dv_device_to_handle(device);
    if (*pipeline) {

src/broadcom/vulkan/v3dv_meta_copy.c
@@ -2433,17 +2433,19 @@ get_copy_texel_buffer_pipeline(
    bool ok = true;
 
    uint8_t key[V3DV_META_TEXEL_BUFFER_COPY_CACHE_KEY_SIZE];
-   get_texel_buffer_copy_pipeline_cache_key(format, cmask, cswizzle, is_layered,
-                                            key);
+   if (device->instance->meta_cache_enabled) {
+      get_texel_buffer_copy_pipeline_cache_key(format, cmask, cswizzle, is_layered,
+                                               key);
 
-   mtx_lock(&device->meta.mtx);
-   struct hash_entry *entry =
-      _mesa_hash_table_search(device->meta.texel_buffer_copy.cache[image_type],
-                              key);
-   if (entry) {
-      mtx_unlock(&device->meta.mtx);
-      *pipeline = entry->data;
-      return true;
+      mtx_lock(&device->meta.mtx);
+      struct hash_entry *entry =
+         _mesa_hash_table_search(device->meta.texel_buffer_copy.cache[image_type],
+                                 key);
+      if (entry) {
+         mtx_unlock(&device->meta.mtx);
+         *pipeline = entry->data;
+         return true;
+      }
    }
 
    *pipeline = vk_zalloc2(&device->vk.alloc, NULL, sizeof(**pipeline), 8,
@@ -2468,16 +2470,19 @@ get_copy_texel_buffer_pipeline(
    if (!ok)
       goto fail;
 
-   uint8_t *dupkey = malloc(V3DV_META_TEXEL_BUFFER_COPY_CACHE_KEY_SIZE);
-   memcpy(dupkey, key, V3DV_META_TEXEL_BUFFER_COPY_CACHE_KEY_SIZE);
-   _mesa_hash_table_insert(device->meta.texel_buffer_copy.cache[image_type],
-                           dupkey, *pipeline);
+   if (device->instance->meta_cache_enabled) {
+      uint8_t *dupkey = malloc(V3DV_META_TEXEL_BUFFER_COPY_CACHE_KEY_SIZE);
+      memcpy(dupkey, key, V3DV_META_TEXEL_BUFFER_COPY_CACHE_KEY_SIZE);
+      _mesa_hash_table_insert(device->meta.texel_buffer_copy.cache[image_type],
+                              dupkey, *pipeline);
+      mtx_unlock(&device->meta.mtx);
+   }
 
-   mtx_unlock(&device->meta.mtx);
    return true;
 
 fail:
-   mtx_unlock(&device->meta.mtx);
+   if (device->instance->meta_cache_enabled)
+      mtx_unlock(&device->meta.mtx);
 
    VkDevice _device = v3dv_device_to_handle(device);
    if (*pipeline) {
@@ -4086,15 +4091,17 @@ get_blit_pipeline(struct v3dv_device *device,
    bool ok = true;
 
    uint8_t key[V3DV_META_BLIT_CACHE_KEY_SIZE];
-   get_blit_pipeline_cache_key(dst_format, src_format, cmask,
-                               dst_samples, src_samples, key);
-   mtx_lock(&device->meta.mtx);
-   struct hash_entry *entry =
-      _mesa_hash_table_search(device->meta.blit.cache[src_type], &key);
-   if (entry) {
-      mtx_unlock(&device->meta.mtx);
-      *pipeline = entry->data;
-      return true;
+   if (device->instance->meta_cache_enabled) {
+      get_blit_pipeline_cache_key(dst_format, src_format, cmask,
+                                  dst_samples, src_samples, key);
+      mtx_lock(&device->meta.mtx);
+      struct hash_entry *entry =
+         _mesa_hash_table_search(device->meta.blit.cache[src_type], &key);
+      if (entry) {
+         mtx_unlock(&device->meta.mtx);
+         *pipeline = entry->data;
+         return true;
+      }
    }
 
    *pipeline = vk_zalloc2(&device->vk.alloc, NULL, sizeof(**pipeline), 8,
@@ -4125,15 +4132,17 @@ get_blit_pipeline(struct v3dv_device *device,
    if (!ok)
       goto fail;
 
-   memcpy((*pipeline)->key, key, sizeof((*pipeline)->key));
-   _mesa_hash_table_insert(device->meta.blit.cache[src_type],
-                           &(*pipeline)->key, *pipeline);
-
-   mtx_unlock(&device->meta.mtx);
+   if (device->instance->meta_cache_enabled) {
+      memcpy((*pipeline)->key, key, sizeof((*pipeline)->key));
+      _mesa_hash_table_insert(device->meta.blit.cache[src_type],
+                              &(*pipeline)->key, *pipeline);
+      mtx_unlock(&device->meta.mtx);
+   }
    return true;
 
 fail:
-   mtx_unlock(&device->meta.mtx);
+   if (device->instance->meta_cache_enabled)
+      mtx_unlock(&device->meta.mtx);
 
    VkDevice _device = v3dv_device_to_handle(device);
    if (*pipeline) {

src/broadcom/vulkan/v3dv_device.c
@@ -560,33 +560,44 @@ v3dv_CreateInstance(const VkInstanceCreateInfo *pCreateInfo,
    instance->vk.physical_devices.enumerate = enumerate_devices;
    instance->vk.physical_devices.destroy = destroy_physical_device;
 
-   /* We start with the default values for the pipeline_cache envvars */
+   /* We start with the default values for the pipeline_cache envvars.
+    *
+    * FIXME: with so many options now, perhaps we could use parse_debug_string
+    */
    instance->pipeline_cache_enabled = true;
    instance->default_pipeline_cache_enabled = true;
+   instance->meta_cache_enabled = true;
    const char *pipeline_cache_str = getenv("V3DV_ENABLE_PIPELINE_CACHE");
    if (pipeline_cache_str != NULL) {
       if (strncmp(pipeline_cache_str, "full", 4) == 0) {
          /* nothing to do, just to filter correct values */
       } else if (strncmp(pipeline_cache_str, "no-default-cache", 16) == 0) {
          instance->default_pipeline_cache_enabled = false;
+      } else if (strncmp(pipeline_cache_str, "no-meta-cache", 13) == 0) {
+         instance->meta_cache_enabled = false;
       } else if (strncmp(pipeline_cache_str, "off", 3) == 0) {
          instance->pipeline_cache_enabled = false;
          instance->default_pipeline_cache_enabled = false;
+         instance->meta_cache_enabled = false;
       } else {
          fprintf(stderr, "Wrong value for envvar V3DV_ENABLE_PIPELINE_CACHE. "
-                 "Allowed values are: full, no-default-cache, off\n");
+                 "Allowed values are: full, no-default-cache, no-meta-cache, off\n");
       }
    }
 
    if (instance->pipeline_cache_enabled == false) {
       fprintf(stderr, "WARNING: v3dv pipeline cache is disabled. Performance "
               "can be affected negatively\n");
-   } else {
-      if (instance->default_pipeline_cache_enabled == false) {
-        fprintf(stderr, "WARNING: default v3dv pipeline cache is disabled. "
-                "Performance can be affected negatively\n");
-      }
    }
+   if (instance->default_pipeline_cache_enabled == false) {
+      fprintf(stderr, "WARNING: default v3dv pipeline cache is disabled. "
+              "Performance can be affected negatively\n");
+   }
+   if (instance->meta_cache_enabled == false) {
+      fprintf(stderr, "WARNING: custom pipeline cache for meta operations are disabled. "
+              "Performance can be affected negatively\n");
+   }
+
 
    VG(VALGRIND_CREATE_MEMPOOL(instance, 0, false));
 

src/broadcom/vulkan/v3dv_private.h
@@ -235,6 +235,7 @@ struct v3dv_instance {
 
    bool pipeline_cache_enabled;
    bool default_pipeline_cache_enabled;
+   bool meta_cache_enabled;
 };
 
 /* FIXME: In addition to tracking the last job submitted by GPU queue (cl, csd,

commit e1dddfa75a85591abf9f1380a6808d4a4a61b46a

v3dv: fix pipeline leaks when meta pipeline cache is disabled
If the cache is disabled then we need to destroy the pipelines
manually when they are no longer needed. Do that by adding them
as private objects to the command buffer.

Fixes: 4f26303dbb ('v3dv: add debug option to disable custom pipeline caches for meta operations')
Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29544>

src/broadcom/vulkan/v3dv_meta_clear.c
@@ -809,7 +809,7 @@ get_depth_clear_pipeline_cache_key(VkImageAspectFlags aspects,
 }
 
 static VkResult
-get_color_clear_pipeline(struct v3dv_device *device,
+get_color_clear_pipeline(struct v3dv_cmd_buffer *cmd_buffer,
                          struct v3dv_render_pass *pass,
                          uint32_t subpass_idx,
                          uint32_t rt_idx,
@@ -822,6 +822,7 @@ get_color_clear_pipeline(struct v3dv_device *device,
                          struct v3dv_meta_color_clear_pipeline **pipeline)
 {
    assert(vk_format_is_color(format));
+   struct v3dv_device *device = cmd_buffer->device;
 
    VkResult result = VK_SUCCESS;
 
@@ -898,6 +899,10 @@ get_color_clear_pipeline(struct v3dv_device *device,
                               &(*pipeline)->key, *pipeline);
 
       mtx_unlock(&device->meta.mtx);
+   } else {
+      v3dv_cmd_buffer_add_private_obj(
+         cmd_buffer, (uintptr_t)*pipeline,
+         (v3dv_cmd_buffer_private_obj_destroy_cb)destroy_color_clear_pipeline);
    }
 
    return VK_SUCCESS;
@@ -920,7 +925,7 @@ fail:
 }
 
 static VkResult
-get_depth_clear_pipeline(struct v3dv_device *device,
+get_depth_clear_pipeline(struct v3dv_cmd_buffer *cmd_buffer,
                          VkImageAspectFlags aspects,
                          struct v3dv_render_pass *pass,
                          uint32_t subpass_idx,
@@ -934,6 +939,7 @@ get_depth_clear_pipeline(struct v3dv_device *device,
    assert(attachment_idx < pass->attachment_count);
 
    VkResult result = VK_SUCCESS;
+   struct v3dv_device *device = cmd_buffer->device;
 
    const uint32_t samples = pass->attachments[attachment_idx].desc.samples;
    const VkFormat format = pass->attachments[attachment_idx].desc.format;
@@ -977,6 +983,10 @@ get_depth_clear_pipeline(struct v3dv_device *device,
       _mesa_hash_table_insert(device->meta.depth_clear.cache,
                               &(*pipeline)->key, *pipeline);
       mtx_unlock(&device->meta.mtx);
+   } else {
+      v3dv_cmd_buffer_add_private_obj(
+         cmd_buffer, (uintptr_t)*pipeline,
+         (v3dv_cmd_buffer_private_obj_destroy_cb)destroy_depth_clear_pipeline);
    }
 
    return VK_SUCCESS;
@@ -1025,7 +1035,7 @@ emit_subpass_color_clear_rects(struct v3dv_cmd_buffer *cmd_buffer,
                                VK_COLOR_COMPONENT_A_BIT;
 
    struct v3dv_meta_color_clear_pipeline *pipeline = NULL;
-   VkResult result = get_color_clear_pipeline(cmd_buffer->device,
+   VkResult result = get_color_clear_pipeline(cmd_buffer,
                                               pass,
                                               cmd_buffer->state.subpass_idx,
                                               rt_idx,
@@ -1083,15 +1093,6 @@ emit_subpass_color_clear_rects(struct v3dv_cmd_buffer *cmd_buffer,
       }
    }
 
-   /* Subpass pipelines can't be cached because they include a reference to the
-    * render pass currently bound by the application, which means that we need
-    * to destroy them manually here.
-    */
-   assert(!pipeline->cached);
-   v3dv_cmd_buffer_add_private_obj(
-      cmd_buffer, (uintptr_t)pipeline,
-      (v3dv_cmd_buffer_private_obj_destroy_cb) destroy_color_clear_pipeline);
-
    v3dv_cmd_buffer_meta_state_pop(cmd_buffer, false);
 }
 
@@ -1118,7 +1119,7 @@ emit_subpass_ds_clear_rects(struct v3dv_cmd_buffer *cmd_buffer,
    assert(attachment_idx < pass->attachment_count);
    struct v3dv_meta_depth_clear_pipeline *pipeline = NULL;
 
-   VkResult result = get_depth_clear_pipeline(cmd_buffer->device,
+   VkResult result = get_depth_clear_pipeline(cmd_buffer,
                                               aspects,
                                               pass,
                                               cmd_buffer->state.subpass_idx,

src/broadcom/vulkan/v3dv_meta_copy.c
@@ -114,6 +114,19 @@ v3dv_meta_blit_init(struct v3dv_device *device)
                                &device->meta.blit.p_layout);
 }
 
+static void
+destroy_meta_blit_pipeline(VkDevice vk_device,
+                           uint64_t obj,
+                           VkAllocationCallbacks *alloc)
+{
+   struct v3dv_meta_blit_pipeline *p =
+      (struct v3dv_meta_blit_pipeline *)(uintptr_t) obj;
+   v3dv_DestroyPipeline(vk_device, p->pipeline, alloc);
+   v3dv_DestroyRenderPass(vk_device, p->pass, alloc);
+   v3dv_DestroyRenderPass(vk_device, p->pass_no_load, alloc);
+   vk_free(alloc, p);
+}
+
 void
 v3dv_meta_blit_finish(struct v3dv_device *device)
 {
@@ -121,11 +134,8 @@ v3dv_meta_blit_finish(struct v3dv_device *device)
 
    for (uint32_t i = 0; i < 3; i++) {
       hash_table_foreach(device->meta.blit.cache[i], entry) {
-         struct v3dv_meta_blit_pipeline *item = entry->data;
-         v3dv_DestroyPipeline(_device, item->pipeline, &device->vk.alloc);
-         v3dv_DestroyRenderPass(_device, item->pass, &device->vk.alloc);
-         v3dv_DestroyRenderPass(_device, item->pass_no_load, &device->vk.alloc);
-         vk_free(&device->vk.alloc, item);
+         destroy_meta_blit_pipeline(_device, (uintptr_t)entry->data,
+                                    &device->vk.alloc);
       }
       _mesa_hash_table_destroy(device->meta.blit.cache[i], NULL);
    }
@@ -227,6 +237,19 @@ v3dv_meta_texel_buffer_copy_init(struct v3dv_device *device)
       &device->meta.texel_buffer_copy.p_layout);
 }
 
+static void
+destroy_meta_texel_buffer_copy_pipeline(VkDevice vk_device,
+                                        uint64_t obj,
+                                        VkAllocationCallbacks *alloc)
+{
+   struct v3dv_meta_texel_buffer_copy_pipeline *p =
+      (struct v3dv_meta_texel_buffer_copy_pipeline *)(uintptr_t) obj;
+   v3dv_DestroyPipeline(vk_device, p->pipeline, alloc);
+   v3dv_DestroyRenderPass(vk_device, p->pass, alloc);
+   v3dv_DestroyRenderPass(vk_device, p->pass_no_load, alloc);
+   vk_free(alloc, p);
+}
+
 void
 v3dv_meta_texel_buffer_copy_finish(struct v3dv_device *device)
 {
@@ -234,11 +257,8 @@ v3dv_meta_texel_buffer_copy_finish(struct v3dv_device *device)
 
    for (uint32_t i = 0; i < 3; i++) {
       hash_table_foreach(device->meta.texel_buffer_copy.cache[i], entry) {
-         struct v3dv_meta_texel_buffer_copy_pipeline *item = entry->data;
-         v3dv_DestroyPipeline(_device, item->pipeline, &device->vk.alloc);
-         v3dv_DestroyRenderPass(_device, item->pass, &device->vk.alloc);
-         v3dv_DestroyRenderPass(_device, item->pass_no_load, &device->vk.alloc);
-         vk_free(&device->vk.alloc, item);
+         destroy_meta_texel_buffer_copy_pipeline(_device, (uintptr_t)entry->data,
+                                                 &device->vk.alloc);
       }
       _mesa_hash_table_destroy(device->meta.texel_buffer_copy.cache[i], NULL);
    }
@@ -2422,7 +2442,7 @@ create_texel_buffer_copy_pipeline(struct v3dv_device *device,
 
 static bool
 get_copy_texel_buffer_pipeline(
-   struct v3dv_device *device,
+   struct v3dv_cmd_buffer *cmd_buffer,
    VkFormat format,
    VkColorComponentFlags cmask,
    VkComponentMapping *cswizzle,
@@ -2431,6 +2451,7 @@ get_copy_texel_buffer_pipeline(
    struct v3dv_meta_texel_buffer_copy_pipeline **pipeline)
 {
    bool ok = true;
+   struct v3dv_device *device = cmd_buffer->device;
 
    uint8_t key[V3DV_META_TEXEL_BUFFER_COPY_CACHE_KEY_SIZE];
    if (device->instance->meta_cache_enabled) {
@@ -2476,6 +2497,10 @@ get_copy_texel_buffer_pipeline(
       _mesa_hash_table_insert(device->meta.texel_buffer_copy.cache[image_type],
                               dupkey, *pipeline);
       mtx_unlock(&device->meta.mtx);
+   } else {
+      v3dv_cmd_buffer_add_private_obj(
+         cmd_buffer, (uintptr_t)*pipeline,
+         (v3dv_cmd_buffer_private_obj_destroy_cb)destroy_meta_texel_buffer_copy_pipeline);
    }
 
    return true;
@@ -2579,7 +2604,7 @@ texel_buffer_shader_copy(struct v3dv_cmd_buffer *cmd_buffer,
 
    /* Get the texel buffer copy pipeline */
    struct v3dv_meta_texel_buffer_copy_pipeline *pipeline = NULL;
-   bool ok = get_copy_texel_buffer_pipeline(cmd_buffer->device,
+   bool ok = get_copy_texel_buffer_pipeline(cmd_buffer,
                                             dst_format, cmask, cswizzle,
                                             image->vk.image_type, num_layers > 1,
                                             &pipeline);
@@ -4079,7 +4104,7 @@ create_blit_pipeline(struct v3dv_device *device,
  * destination and source formats.
  */
 static bool
-get_blit_pipeline(struct v3dv_device *device,
+get_blit_pipeline(struct v3dv_cmd_buffer *cmd_buffer,
                   VkFormat dst_format,
                   VkFormat src_format,
                   VkColorComponentFlags cmask,
@@ -4089,6 +4114,7 @@ get_blit_pipeline(struct v3dv_device *device,
                   struct v3dv_meta_blit_pipeline **pipeline)
 {
    bool ok = true;
+   struct v3dv_device *device = cmd_buffer->device;
 
    uint8_t key[V3DV_META_BLIT_CACHE_KEY_SIZE];
    if (device->instance->meta_cache_enabled) {
@@ -4137,7 +4163,12 @@ get_blit_pipeline(struct v3dv_device *device,
       _mesa_hash_table_insert(device->meta.blit.cache[src_type],
                               &(*pipeline)->key, *pipeline);
       mtx_unlock(&device->meta.mtx);
+   } else {
+      v3dv_cmd_buffer_add_private_obj(
+         cmd_buffer, (uintptr_t)*pipeline,
+         (v3dv_cmd_buffer_private_obj_destroy_cb)destroy_meta_blit_pipeline);
    }
+
    return true;
 
 fail:
@@ -4486,7 +4517,7 @@ blit_shader(struct v3dv_cmd_buffer *cmd_buffer,
 
    /* Get the blit pipeline */
    struct v3dv_meta_blit_pipeline *pipeline = NULL;
-   bool ok = get_blit_pipeline(cmd_buffer->device,
+   bool ok = get_blit_pipeline(cmd_buffer,
                                dst_format, src_format, cmask, src->vk.image_type,
                                dst->vk.samples, src->vk.samples,
                                &pipeline);
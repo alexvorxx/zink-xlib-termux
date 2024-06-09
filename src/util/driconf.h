99af53c6fd6d0354cdb9bd0f6c71d1f79f60058b
Eric Engestrom <eric@igalia.com>
2024-06-09 22:48:54 +0200

    driconf: drop param for setting default gpu vendor id in DRI_CONF_FORCE_VK_VENDOR()
    The macro was ignoring the param and hard-coding 0, and it doesn't make
    much sense to allow drivers to override it by default, so remove the
    appearance of the ability to do so.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29632>

--- src/util/driconf.h
+++ src/util/driconf.h
@@ -317,7 +317,7 @@
    DRI_CONF_OPT_B(ignore_discard_framebuffer, def, \
                   "Ignore glDiscardFramebuffer/glInvalidateFramebuffer, workaround for games that use it incorrectly")
 
-#define DRI_CONF_FORCE_VK_VENDOR(def) \
+#define DRI_CONF_FORCE_VK_VENDOR() \
    DRI_CONF_OPT_I(force_vk_vendor, 0, -1, 2147483647, "Override GPU vendor id")
 
 #define DRI_CONF_FAKE_SPARSE(def) \

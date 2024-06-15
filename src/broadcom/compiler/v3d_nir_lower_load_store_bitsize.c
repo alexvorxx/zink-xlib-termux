f8ab9c0e9306dfe8e3596189b86c25fe54b8b160
Karol Herbst <kherbst@redhat.com>
2024-06-15 12:55:25 +0200

    broadcom/compiler: handle up to vec16 load_uniforms
    Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25362>

--- src/broadcom/compiler/v3d_nir_lower_load_store_bitsize.c
+++ src/broadcom/compiler/v3d_nir_lower_load_store_bitsize.c
@@ -79,7 +79,7 @@ lower_load_bitsize(nir_builder *b,
         nir_def *offset = intr->src[offset_idx].ssa;
 
         /* Split vector store to multiple scalar loads */
-        nir_def *dest_components[4] = { NULL };
+        nir_def *dest_components[16] = { NULL };
         const nir_intrinsic_info *info = &nir_intrinsic_infos[intr->intrinsic];
         for (int component = 0; component < num_comp; component++) {
                 nir_def *scalar_offset;

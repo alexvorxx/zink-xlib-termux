6db922c0bff907796dcb3c9e0bbcffd5a62a1bc0
Christian Gmeiner <cgmeiner@igalia.com>
2024-06-17 13:36:05 +0200

    etnaviv: isa: Add C function impl
    Implement the following C API's:
     - isa_parse_str(..)
     - isa_parse_file(..)
     - isa_asm_result_destroy(..)
    
    Signed-off-by: Christian Gmeiner <cgmeiner@igalia.com>
    Reviewed-by: @LingMan
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28869>

--- src/etnaviv/isa/isa.h
+++ src/etnaviv/isa/isa.h
@@ -8,6 +8,7 @@
 
 #include "compiler/isaspec/isaspec.h"
 
+struct etna_asm_result;
 struct etna_inst;
 
 #ifdef __cplusplus
@@ -15,6 +16,10 @@ extern "C" {
 #endif
 
 void isa_assemble_instruction(uint32_t *out, const struct etna_inst *instr);
+
+extern struct etna_asm_result *isa_parse_str(const char *str, bool dual_16_mode);
+extern struct etna_asm_result *isa_parse_file(const char *filepath, bool dual_16_mode);
+extern void isa_asm_result_destroy(struct etna_asm_result *result);
 
 #ifdef __cplusplus
 }

af6f82b954a1581e448a1af47de32fd057b81a00
Job Noorman <jnoorman@igalia.com>
2024-06-17 11:34:25 +0200

    ir3: fix handling of early clobbers in calc_min_limit_pressure
    Early clobbers should always add to the register pressure since they
    cannot overlap with sources. handle_instr in ir3_spill.c handles this
    properly but calc_min_limit_pressure did not.
    
    Fixes: 2ff5826f09d ("ir3/ra: Add IR3_REG_EARLY_CLOBBER")
    Signed-off-by: Job Noorman <jnoorman@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29497>

--- src/freedreno/ir3/ir3_ra.c
+++ src/freedreno/ir3/ir3_ra.c
@@ -2452,7 +2452,8 @@ calc_min_limit_pressure(struct ir3_shader_variant *v,
          cur_pressure = (struct ir3_pressure) {0};
 
          ra_foreach_dst (dst, instr) {
-            if (dst->tied && !(dst->tied->flags & IR3_REG_KILL))
+            if ((dst->tied && !(dst->tied->flags & IR3_REG_KILL)) ||
+                (dst->flags & IR3_REG_EARLY_CLOBBER))
                add_pressure(&cur_pressure, dst, v->mergedregs);
          }
 

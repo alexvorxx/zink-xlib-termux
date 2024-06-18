aerisarn/mesa-uwp

commit 613eaac7b53bfbfcd6ef536412be6c9c63cdea4f

ir3: Initial support for spilling non-shared registers
Support for spilling shared registers to normal registers is still TODO.
There are also several improvements to be made, like rematerialization.

Note, there is one behavior change to register pressure accounting: we
now include half registers in the current full pressure directly in
mergedregs mode, rather than adding the max half pressure to the max
full pressure afterwards, which might result in lower calculated max
pressure in some cases with half registers. This is needed for spilling,
since we need to make sure the total pressure including half registers
is below the maximum at each instruction. Because the entire pass is
rewritten, including the register pressure calculating parts, it didn't
seem worth it to separate out this change.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/12033>

src/freedreno/ir3/ir3_merge_regs.c
@@ -198,6 +198,7 @@ get_merge_set(struct ir3_register *def)
    struct ir3_merge_set *set = ralloc(def, struct ir3_merge_set);
    set->preferred_reg = ~0;
    set->interval_start = ~0;
+   set->spill_slot = ~0;
    set->size = reg_size(def);
    set->alignment = (def->flags & IR3_REG_HALF) ? 1 : 2;
    set->regs_count = 1;
@@ -339,6 +340,19 @@ try_merge_defs(struct ir3_liveness *live, struct ir3_register *a,
       merge_merge_sets(a_set, b_set, b_set_offset);
 }
 
+void
+ir3_force_merge(struct ir3_register *a, struct ir3_register *b, int b_offset)
+{
+   struct ir3_merge_set *a_set = get_merge_set(a);
+   struct ir3_merge_set *b_set = get_merge_set(b);
+
+   if (a_set == b_set)
+      return;
+
+   int b_set_offset = a->merge_set_offset + b_offset - b->merge_set_offset;
+   merge_merge_sets(a_set, b_set, b_set_offset);
+}
+
 static void
 coalesce_phi(struct ir3_liveness *live, struct ir3_instruction *phi)
 {
@@ -462,7 +476,7 @@ ir3_create_parallel_copies(struct ir3 *ir)
 }
 
 static void
-index_merge_sets(struct ir3 *ir)
+index_merge_sets(struct ir3_liveness *live, struct ir3 *ir)
 {
    unsigned offset = 0;
    foreach_block (block, &ir->block_list) {
@@ -489,6 +503,8 @@ index_merge_sets(struct ir3 *ir)
          }
       }
    }
+
+   live->interval_offset = offset;
 }
 
 #define RESET      "\x1b[0m"
@@ -559,7 +575,7 @@ ir3_merge_regs(struct ir3_liveness *live, struct ir3 *ir)
       }
    }
 
-   index_merge_sets(ir);
+   index_merge_sets(live, ir);
 
    if (ir3_shader_debug & IR3_DBG_RAMSGS)
       dump_merge_sets(ir);

src/freedreno/ir3/ir3_ra.h
@@ -137,6 +137,7 @@ ra_reg_is_dst(const struct ir3_register *reg)
 
 struct ir3_liveness {
    unsigned block_count;
+   unsigned interval_offset;
    DECLARE_ARRAY(struct ir3_register *, definitions);
    DECLARE_ARRAY(BITSET_WORD *, live_out);
    DECLARE_ARRAY(BITSET_WORD *, live_in);
@@ -151,12 +152,21 @@ void ir3_create_parallel_copies(struct ir3 *ir);
 
 void ir3_merge_regs(struct ir3_liveness *live, struct ir3 *ir);
 
+void ir3_force_merge(struct ir3_register *a, struct ir3_register *b,
+                     int b_offset);
+
 struct ir3_pressure {
    unsigned full, half, shared;
 };
 
 void ir3_calc_pressure(struct ir3_shader_variant *v, struct ir3_liveness *live,
                        struct ir3_pressure *max_pressure);
+
+bool ir3_spill(struct ir3 *ir, struct ir3_shader_variant *v,
+               struct ir3_liveness **live,
+               const struct ir3_pressure *limit_pressure);
+
+bool ir3_lower_spill(struct ir3 *ir);
 
 void ir3_ra_validate(struct ir3_shader_variant *v, unsigned full_size,
                      unsigned half_size, unsigned block_count);

src/freedreno/ir3/disasm-a3xx.c
@@ -348,6 +348,9 @@ static const struct opc_info {
    OPC(6, OPC_GETSPID,      getspid),
    OPC(6, OPC_GETWID,       getwid),
 
+   OPC(6, OPC_SPILL_MACRO,  spill.macro),
+   OPC(6, OPC_RELOAD_MACRO, reload.macro),
+
    OPC(7, OPC_BAR,          bar),
    OPC(7, OPC_FENCE,        fence),
 /* clang-format on */

src/freedreno/ir3/instr-a3xx.h
@@ -308,6 +308,9 @@ typedef enum {
    OPC_LDG_A           = _OPC(6, 55),
    OPC_STG_A           = _OPC(6, 56),
 
+   OPC_SPILL_MACRO     = _OPC(6, 57),
+   OPC_RELOAD_MACRO    = _OPC(6, 58),
+
    /* category 7: */
    OPC_BAR             = _OPC(7, 0),
    OPC_FENCE           = _OPC(7, 1),

src/freedreno/ir3/ir3.h
@@ -89,6 +89,7 @@ struct ir3_merge_set {
    uint16_t alignment;
 
    unsigned interval_start;
+   unsigned spill_slot;
 
    unsigned regs_count;
    struct ir3_register **regs;
@@ -201,6 +202,8 @@ struct ir3_register {
     * they must have "tied" pointing to each other.
     */
    struct ir3_register *tied;
+
+   unsigned spill_slot, next_use;
 
    unsigned merge_set_offset;
    struct ir3_merge_set *merge_set;
@@ -709,6 +712,17 @@ ir3_instr_move_after(struct ir3_instruction *instr,
 {
    list_delinit(&instr->node);
    list_add(&instr->node, &before->node);
+}
+
+/**
+ * Move 'instr' to the beginning of the block:
+ */
+static inline void
+ir3_instr_move_before_block(struct ir3_instruction *instr,
+                            struct ir3_block *block)
+{
+   list_delinit(&instr->node);
+   list_add(&instr->node, &block->instr_list);
 }
 
 void ir3_find_ssa_uses(struct ir3 *ir, void *mem_ctx, bool falsedeps);

src/freedreno/ir3/ir3_compiler.c
@@ -44,6 +44,7 @@ static const struct debug_named_value shader_debug_options[] = {
    {"nouboopt",   IR3_DBG_NOUBOOPT,   "Disable lowering UBO to uniform"},
    {"nofp16",     IR3_DBG_NOFP16,     "Don't lower mediump to fp16"},
    {"nocache",    IR3_DBG_NOCACHE,    "Disable shader cache"},
+   {"spillall",   IR3_DBG_SPILLALL,   "Spill as much as possible to test the spiller"},
 #ifdef DEBUG
    /* DEBUG-only options: */
    {"schedmsgs",  IR3_DBG_SCHEDMSGS,  "Enable scheduler debug messages"},

src/freedreno/ir3/ir3_compiler.h
@@ -194,6 +194,7 @@ enum ir3_shader_debug {
    IR3_DBG_NOUBOOPT = BITFIELD_BIT(9),
    IR3_DBG_NOFP16 = BITFIELD_BIT(10),
    IR3_DBG_NOCACHE = BITFIELD_BIT(11),
+   IR3_DBG_SPILLALL = BITFIELD_BIT(12),
 
    /* DEBUG-only options: */
    IR3_DBG_SCHEDMSGS = BITFIELD_BIT(20),

src/freedreno/ir3/ir3_lower_spill.c
@@ -1,0 +1,163 @@
+/*
+ * Copyright (C) 2021 Valve Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "ir3_ra.h"
+
+/* The spilling pass leaves out a few details required to successfully operate
+ * ldp/stp:
+ *
+ * 1. ldp/stp can only load/store 4 components at a time, but spilling ignores
+ *    that and just spills/restores entire values, including arrays and values
+ *    created for texture setup which can be more than 4 components.
+ * 2. The spiller doesn't add barrier dependencies needed for post-RA
+ *    scheduling.
+ *
+ * The first one, in particular, is much easier to handle after RA because
+ * arrays and normal values can be treated the same way. Therefore this pass
+ * runs after RA, and handles both issues. This keeps the complexity out of the
+ * spiller.
+ */
+
+static void
+split_spill(struct ir3_instruction *spill)
+{
+   unsigned orig_components = spill->srcs[2]->uim_val;
+
+   /* We don't handle splitting dependencies. */
+   assert(spill->deps_count == 0);
+
+   if (orig_components <= 4) {
+      if (spill->srcs[1]->flags & IR3_REG_ARRAY) {
+         spill->srcs[1]->wrmask = MASK(orig_components);
+         spill->srcs[1]->num = spill->srcs[1]->array.base;
+         spill->srcs[1]->flags &= ~IR3_REG_ARRAY;
+      }
+      return;
+   }
+
+   for (unsigned comp = 0; comp < orig_components; comp += 4) {
+      unsigned components = MIN2(orig_components - comp, 4);
+      struct ir3_instruction *clone = ir3_instr_clone(spill);
+      ir3_instr_move_before(clone, spill);
+
+      clone->srcs[1]->wrmask = MASK(components);
+      if (clone->srcs[1]->flags & IR3_REG_ARRAY) {
+         clone->srcs[1]->num = clone->srcs[1]->array.base + comp;
+         clone->srcs[1]->flags &= ~IR3_REG_ARRAY;
+      }
+
+      clone->srcs[2]->uim_val = components;
+      clone->cat6.dst_offset +=
+         comp * ((spill->srcs[1]->flags & IR3_REG_HALF) ? 2 : 4);
+   }
+
+   list_delinit(&spill->node);
+}
+
+static void
+split_reload(struct ir3_instruction *reload)
+{
+   unsigned orig_components = reload->srcs[2]->uim_val;
+
+   assert(reload->deps_count == 0);
+
+   if (orig_components <= 4) {
+      if (reload->dsts[0]->flags & IR3_REG_ARRAY) {
+         reload->dsts[0]->wrmask = MASK(orig_components);
+         reload->dsts[0]->num = reload->dsts[0]->array.base;
+         reload->dsts[0]->flags &= ~IR3_REG_ARRAY;
+      }
+      return;
+   }
+
+   for (unsigned comp = 0; comp < orig_components; comp += 4) {
+      unsigned components = MIN2(orig_components - comp, 4);
+      struct ir3_instruction *clone = ir3_instr_clone(reload);
+      ir3_instr_move_before(clone, reload);
+
+      clone->dsts[0]->wrmask = MASK(components);
+      if (clone->dsts[0]->flags & IR3_REG_ARRAY) {
+         clone->dsts[0]->num = clone->dsts[0]->array.base + comp;
+         clone->dsts[0]->flags &= ~IR3_REG_ARRAY;
+      }
+
+      clone->srcs[2]->uim_val = components;
+      clone->srcs[1]->uim_val +=
+         comp * ((reload->dsts[0]->flags & IR3_REG_HALF) ? 2 : 4);
+   }
+
+   list_delinit(&reload->node);
+}
+
+static void
+add_spill_reload_deps(struct ir3_block *block)
+{
+   struct ir3_instruction *last_spill = NULL;
+
+   foreach_instr (instr, &block->instr_list) {
+      if ((instr->opc == OPC_SPILL_MACRO || instr->opc == OPC_RELOAD_MACRO) &&
+          last_spill) {
+         ir3_instr_add_dep(instr, last_spill);
+      }
+
+      if (instr->opc == OPC_SPILL_MACRO)
+         last_spill = instr;
+   }
+
+
+   last_spill = NULL;
+
+   foreach_instr_rev (instr, &block->instr_list) {
+      if ((instr->opc == OPC_SPILL_MACRO || instr->opc == OPC_RELOAD_MACRO) &&
+          last_spill) {
+         ir3_instr_add_dep(last_spill, instr);
+      }
+
+      if (instr->opc == OPC_SPILL_MACRO)
+         last_spill = instr;
+   }
+}
+
+bool
+ir3_lower_spill(struct ir3 *ir)
+{
+   foreach_block (block, &ir->block_list) {
+      foreach_instr_safe (instr, &block->instr_list) {
+         if (instr->opc == OPC_SPILL_MACRO)
+            split_spill(instr);
+         else if (instr->opc == OPC_RELOAD_MACRO)
+            split_reload(instr);
+      }
+
+      add_spill_reload_deps(block);
+
+      foreach_instr (instr, &block->instr_list) {
+         if (instr->opc == OPC_SPILL_MACRO)
+            instr->opc = OPC_STP;
+         else if (instr->opc == OPC_RELOAD_MACRO)
+            instr->opc = OPC_LDP;
+      }
+   }
+
+   return true;
+}

alexvorxx/zink-xlib-termux

commit 17b155fedec357140fed4184de48e689d3310e31

ir3: expose instruction indexing helper for merge sets
We will need it to fix up merge sets after spilling.

Fixes: 613eaac7b53 ("ir3: Initial support for spilling non-shared registers")
Signed-off-by: Job Noorman <jnoorman@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29497>

src/freedreno/ir3/ir3_merge_regs.c
@@ -80,6 +80,12 @@ index_instrs(struct ir3_block *block, unsigned index)
    return index;
 }
 
+void
+ir3_index_instrs_for_merge_sets(struct ir3 *ir)
+{
+   index_instrs(ir3_start_block(ir), 0);
+}
+
 /* Definitions within a merge set are ordered by instr->ip as set above: */
 
 static bool
@@ -559,7 +565,7 @@ dump_merge_sets(struct ir3 *ir)
 void
 ir3_merge_regs(struct ir3_liveness *live, struct ir3 *ir)
 {
-   index_instrs(ir3_start_block(ir), 0);
+   ir3_index_instrs_for_merge_sets(ir);
 
    /* First pass: coalesce phis, which must be together. */
    foreach_block (block, &ir->block_list) {

src/freedreno/ir3/ir3_ra.h
@@ -176,6 +176,8 @@ void ir3_merge_regs(struct ir3_liveness *live, struct ir3 *ir);
 void ir3_force_merge(struct ir3_register *a, struct ir3_register *b,
                      int b_offset);
 
+void ir3_index_instrs_for_merge_sets(struct ir3 *ir);
+
 struct ir3_pressure {
    unsigned full, half, shared, shared_half;
 };
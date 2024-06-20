alexvorxx/zink-xlib-termux

commit c432fbe534d1b1d3874b612b97d8ed3fe97c8945

wgl: Add no-gdi-single-buffered and gdi-double-buffered PFDs
Reviewed-by: Sil Vilerino <sivileri@microsoft.com>
Reviewed-by: Jose Fonseca <jose.fonseca@broadcom.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27512>

src/gallium/frontends/wgl/stw_pixelformat.c
@@ -107,18 +107,12 @@ stw_pf_depth_stencil[] = {
    { PIPE_FORMAT_S8_UINT_Z24_UNORM, {24, 8} }
 };
 
-
-static const bool
-stw_pf_doublebuffer[] = {
-   false,
-   true,
-};
-
-
 static const stw_pfd_flag
 stw_pf_flag[] = {
+   0,
    stw_pfd_double_buffer,
    stw_pfd_gdi_support,
+   stw_pfd_double_buffer | stw_pfd_gdi_support,
 };
 
 
@@ -247,7 +241,7 @@ add_color_format_variants(const struct stw_pf_color_info *color_formats,
                           unsigned num_color_formats, bool extended)
 {
    struct pipe_screen *screen = stw_dev->screen;
-   unsigned cfmt, ms, db, ds, acc, f;
+   unsigned cfmt, ms, ds, acc, f;
    unsigned bind_flags = PIPE_BIND_RENDER_TARGET;
    unsigned num_added = 0;
    int force_samples = 0;
@@ -292,29 +286,26 @@ add_color_format_variants(const struct stw_pf_color_info *color_formats,
             continue;
          }
 
-         for (db = 0; db < ARRAY_SIZE(stw_pf_doublebuffer); db++) {
-            unsigned doublebuffer = stw_pf_doublebuffer[db];
+         for (ds = 0; ds < ARRAY_SIZE(stw_pf_depth_stencil); ds++) {
+            const struct stw_pf_depth_info *depth = &stw_pf_depth_stencil[ds];
 
-            for (ds = 0; ds < ARRAY_SIZE(stw_pf_depth_stencil); ds++) {
-               const struct stw_pf_depth_info *depth = &stw_pf_depth_stencil[ds];
+            if (!screen->is_format_supported(screen, depth->format,
+                                             PIPE_TEXTURE_2D, samples,
+                                             samples,
+                                             PIPE_BIND_DEPTH_STENCIL)) {
+               continue;
+            }
 
-               if (!screen->is_format_supported(screen, depth->format,
-                                                PIPE_TEXTURE_2D, samples,
-                                                samples,
-                                                PIPE_BIND_DEPTH_STENCIL)) {
+            for (f = 0; f < ARRAY_SIZE(stw_pf_flag); f++) {
+               stw_pfd_flag flag = stw_pf_flag[f];
+               if ((supported_flags & flag) != flag)
                   continue;
-               }
-
-               for (f = 0; f < ARRAY_SIZE(stw_pf_flag); f++) {
-                  stw_pfd_flag flag = stw_pf_flag[f];
-                  if (!(supported_flags & flag) || (flag == stw_pfd_double_buffer && !doublebuffer))
-                     continue;
-                  for (acc = 0; acc < 2; acc++) {
-                     stw_pixelformat_add(stw_dev, extended, &color_formats[cfmt],
-                                         depth, acc * 16, doublebuffer,
-                                         (flag == stw_pfd_gdi_support), samples);
-                     num_added++;
-                  }
+               for (acc = 0; acc < 2; acc++) {
+                  stw_pixelformat_add(stw_dev, extended, &color_formats[cfmt],
+                                       depth, acc * 16,
+                                       (flag & stw_pfd_double_buffer) != 0,
+                                       (flag == stw_pfd_gdi_support) != 0, samples);
+                  num_added++;
                }
             }
          }

commit a02b759f413a589a77d36c9ffe731cb139e2ed1d

wgl: Delete pixelformat support query
This whole thing was just a mess and never really worked the way it was
supposed to. All drivers can support GDI interop and double-buffering
independently at this point, so just remove it.

Fixes: c432fbe5 ("wgl: Add no-gdi-single-buffered and gdi-double-buffered PFDs")
Reviewed-By: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Reviewed-by: Jose Fonseca <jose.fonseca@broadcom.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29819>

src/gallium/frontends/wgl/stw_pixelformat.c
@@ -246,10 +246,6 @@ add_color_format_variants(const struct stw_pf_color_info *color_formats,
    unsigned num_added = 0;
    int force_samples = 0;
 
-   unsigned supported_flags = 0;
-   if (stw_dev->stw_winsys && stw_dev->stw_winsys->get_pfd_flags)
-      supported_flags = stw_dev->stw_winsys->get_pfd_flags(screen);
-
    /* Since GLUT for Windows doesn't support MSAA we have an env var
     * to force all pixel formats to have a particular number of samples.
     */
@@ -298,8 +294,6 @@ add_color_format_variants(const struct stw_pf_color_info *color_formats,
 
             for (f = 0; f < ARRAY_SIZE(stw_pf_flag); f++) {
                stw_pfd_flag flag = stw_pf_flag[f];
-               if ((supported_flags & flag) != flag)
-                  continue;
                for (acc = 0; acc < 2; acc++) {
                   stw_pixelformat_add(stw_dev, extended, &color_formats[cfmt],
                                        depth, acc * 16,

src/gallium/frontends/wgl/stw_winsys.h
@@ -125,13 +125,6 @@ struct stw_winsys
                ULONGLONG PresentHistoryToken );
 
    /**
-    * Query whether the driver can support GDI and/or double-buffering in its
-    * pixel formats (optional).
-    */
-   unsigned
-   (*get_pfd_flags)( struct pipe_screen *screen );
-
-   /**
     * Create a winsys-specific object for a given DC's framebuffer
     */
    struct stw_winsys_framebuffer *

src/gallium/targets/wgl/wgl.c
@@ -221,17 +221,6 @@ wgl_get_adapter_luid(struct pipe_screen* screen,
 #endif
 
 
-static unsigned
-wgl_get_pfd_flags(struct pipe_screen *screen)
-{
-#ifdef GALLIUM_D3D12
-   if (use_d3d12)
-      return d3d12_wgl_get_pfd_flags(screen);
-#endif
-   return stw_pfd_gdi_support;
-}
-
-
 static struct stw_winsys_framebuffer *
 wgl_create_framebuffer(struct pipe_screen *screen,
                        HWND hWnd,
@@ -262,7 +251,6 @@ static const struct stw_winsys stw_winsys = {
    NULL, /* shared_surface_open */
    NULL, /* shared_surface_close */
    NULL, /* compose */
-   &wgl_get_pfd_flags,
    &wgl_create_framebuffer,
    &wgl_get_name,
 };

src/gallium/winsys/d3d12/wgl/d3d12_wgl_public.h
@@ -45,9 +45,6 @@ d3d12_wgl_present(struct pipe_screen *screen,
                   struct pipe_resource *res,
                   HDC hDC);
 
-unsigned
-d3d12_wgl_get_pfd_flags(struct pipe_screen *screen);
-
 struct stw_winsys_framebuffer *
 d3d12_wgl_create_framebuffer(struct pipe_screen *screen,
                              HWND hWnd,

src/gallium/winsys/d3d12/wgl/d3d12_wgl_winsys.c
@@ -49,10 +49,3 @@ d3d12_wgl_present(struct pipe_screen *screen,
 {
    screen->flush_frontbuffer(screen, ctx, res, 0, 0, hDC, 0, NULL);
 }
-
-unsigned
-d3d12_wgl_get_pfd_flags(struct pipe_screen *screen)
-{
-   (void)screen;
-   return stw_pfd_gdi_support | stw_pfd_double_buffer;
-}
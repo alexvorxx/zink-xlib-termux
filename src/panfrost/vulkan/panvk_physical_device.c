alexvorxx/zink-xlib-termux

commit f16481969865b5aaab8739da80e5201da60d07dc

panvk: Advertise VK_EXT_shader_module_identifier
Signed-off-by: Mary Guillemard <mary.guillemard@collabora.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29161>

src/panfrost/vulkan/panvk_physical_device.c
@@ -19,6 +19,7 @@
 #include "vk_format.h"
 #include "vk_limits.h"
 #include "vk_log.h"
+#include "vk_shader_module.h"
 #include "vk_util.h"
 
 #include "panvk_device.h"
@@ -80,6 +81,7 @@ get_device_extensions(const struct panvk_physical_device *device,
       .EXT_index_type_uint8 = true,
       .EXT_pipeline_creation_cache_control = true,
       .EXT_pipeline_creation_feedback = true,
+      .EXT_shader_module_identifier = true,
       .EXT_vertex_attribute_divisor = true,
    };
 }
@@ -592,6 +594,13 @@ get_device_properties(const struct panvk_instance *instance,
    snprintf(properties->driverName, VK_MAX_DRIVER_NAME_SIZE, "panvk");
    snprintf(properties->driverInfo, VK_MAX_DRIVER_INFO_SIZE,
             "Mesa " PACKAGE_VERSION MESA_GIT_SHA1);
+
+   /* VK_EXT_shader_module_identifier */
+   STATIC_ASSERT(sizeof(vk_shaderModuleIdentifierAlgorithmUUID) ==
+                 sizeof(properties->shaderModuleIdentifierAlgorithmUUID));
+   memcpy(properties->shaderModuleIdentifierAlgorithmUUID,
+          vk_shaderModuleIdentifierAlgorithmUUID,
+          sizeof(properties->shaderModuleIdentifierAlgorithmUUID));
 }
 
 void

commit 596306a9846f6a455ae070036b2f9ff6ace0c799

panvk: Advertise shaderModuleIdentifier feature
Was missing from the original commit.

Signed-off-by: Mary Guillemard <mary.guillemard@collabora.com>
Fixes: f1648196986 ("panvk: Advertise VK_EXT_shader_module_identifier")
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29800>

src/panfrost/vulkan/panvk_physical_device.c
@@ -225,6 +225,9 @@ get_features(const struct panvk_physical_device *device,
 
       /* VK_KHR_shader_expect_assume */
       .shaderExpectAssume = true,
+
+      /* VK_EXT_shader_module_identifier */
+      .shaderModuleIdentifier = true,
    };
 }
 
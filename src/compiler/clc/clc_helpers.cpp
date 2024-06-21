aerisarn/mesa-uwp

commit 3a752256f54ce839f6e75d508df51eba6464ef77

compiler/clc: Null extensions should mean all supported, not all
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13177>

src/compiler/clc/clc_helpers.cpp
@@ -890,21 +890,29 @@ llvm_mod_to_spirv(std::unique_ptr<::llvm::Module> mod,
       return -1;
    }
 
-   SPIRV::TranslatorOpts spirv_opts;
-   if (!args || !args->allowed_spirv_extensions) {
-      spirv_opts = SPIRV::TranslatorOpts(version);
-      spirv_opts.enableAllExtensions();
-   } else {
-      SPIRV::TranslatorOpts::ExtensionsStatusMap ext_map;
-      for (int i = 0; args->allowed_spirv_extensions[i]; i++) {
+   const char *const *extensions = NULL;
+   if (args)
+      extensions = args->allowed_spirv_extensions;
+   if (!extensions) {
+      /* The SPIR-V parser doesn't handle all extensions */
+      static const char *default_extensions[] = {
+         "SPV_EXT_shader_atomic_float_add",
+         "SPV_EXT_shader_atomic_float_min_max",
+         "SPV_KHR_float_controls",
+         NULL,
+      };
+      extensions = default_extensions;
+   }
+
+   SPIRV::TranslatorOpts::ExtensionsStatusMap ext_map;
+   for (int i = 0; extensions[i]; i++) {
 #define EXT(X) \
-         if (strcmp(#X, args->allowed_spirv_extensions[i]) == 0) \
-            ext_map.insert(std::make_pair(SPIRV::ExtensionID::X, true));
+      if (strcmp(#X, extensions[i]) == 0) \
+         ext_map.insert(std::make_pair(SPIRV::ExtensionID::X, true));
 #include "LLVMSPIRVLib/LLVMSPIRVExtensions.inc"
 #undef EXT
-      }
-      spirv_opts = SPIRV::TranslatorOpts(version, ext_map);
    }
+   SPIRV::TranslatorOpts spirv_opts = SPIRV::TranslatorOpts(version, ext_map);
 
    std::ostringstream spv_stream;
    if (!::llvm::writeSpirv(mod.get(), spirv_opts, spv_stream, log)) {

alexvorxx/zink-xlib-termux

commit e67a8dc59af44abb4b0349c66cfd5770985ff3ce

clc: remove check for null pointer that cannot be true in llvm_mod_to_spirv
Snce the *args parameter was added it's assumed to be non-null. If it is
null then the function is going off to UB land. As such, a later check
added for args being NULL is useless, and confuses coverity.

fixes: 3a752256f54ce839f6e75d508df51eba6464ef77

Reviewed-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29664>

src/compiler/clc/clc_helpers.cpp
@@ -1039,9 +1039,7 @@ llvm_mod_to_spirv(std::unique_ptr<::llvm::Module> mod,
       return -1;
    }
 
-   const char *const *extensions = NULL;
-   if (args)
-      extensions = args->allowed_spirv_extensions;
+   const char *const *extensions = args->allowed_spirv_extensions;
    if (!extensions) {
       /* The SPIR-V parser doesn't handle all extensions */
       static const char *default_extensions[] = {
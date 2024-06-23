alexvorxx/zink-xlib-termux

commit 3f119a1fd8deaa46c4342837d13096cd7873c8d2

util/disk_cache: Add marker on cache usage.
To be able to delete it later if it isn't used. Checking the mtime
on e.g. the index doesn't work if we always hit the cache so add
something that writes unconditionally.

Only once per day to avoid excessive writes.

(See https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22339)

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27173>

src/util/disk_cache_os.c
@@ -96,6 +96,7 @@ disk_cache_get_function_identifier(void *ptr, struct mesa_sha1 *ctx)
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include "utime.h"
 
 #include "util/blob.h"
 #include "util/crc32.h"
@@ -1042,6 +1043,29 @@ disk_cache_load_cache_index_foz(void *mem_ctx, struct disk_cache *cache)
 {
    /* Load cache index into a hash map (from fossilise files) */
    return foz_prepare(&cache->foz_db, cache->path);
+}
+
+
+void
+disk_cache_touch_cache_user_marker(char *path)
+{
+   char *marker_path = NULL;
+   asprintf(&marker_path, "%s/marker", path);
+   if (!marker_path)
+      return;
+
+   time_t now = time(NULL);
+
+   struct stat attr;
+   if (stat(marker_path, &attr) == -1) {
+      int fd = open(marker_path, O_WRONLY | O_CREAT | O_CLOEXEC, 0644);
+      if (fd != -1) {
+         close(fd);
+      }
+   } else if (now - attr.st_mtime < 60 * 60 * 24 /* One day */) {
+      (void)utime(marker_path, NULL);
+   }
+   free(marker_path);
 }
 
 bool

src/util/disk_cache.c
@@ -146,6 +146,9 @@ disk_cache_type_create(const char *gpu_name,
          goto path_fail;
    }
 
+   if (!getenv("MESA_SHADER_CACHE_DIR") && !getenv("MESA_GLSL_CACHE_DIR"))
+      disk_cache_touch_cache_user_marker(cache->path);
+
    cache->type = cache_type;
 
    cache->stats.enabled = debug_get_bool_option("MESA_SHADER_CACHE_SHOW_STATS",

src/util/disk_cache_os.h
@@ -161,6 +161,9 @@ disk_cache_enabled(void);
 bool
 disk_cache_load_cache_index_foz(void *mem_ctx, struct disk_cache *cache);
 
+void
+disk_cache_touch_cache_user_marker(char *path);
+
 bool
 disk_cache_mmap_cache_index(void *mem_ctx, struct disk_cache *cache,
                             char *path);

commit 9b775d26c49e22dad5da1021ff0b63103669bcae

util/disk_cache: Fix cache marker refresh.
Refresh if older than a day, not less than a day old.

Fixes: 3f119a1fd8d ("util/disk_cache: Add marker on cache usage")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29728>

src/util/disk_cache_os.c
@@ -1077,7 +1077,7 @@ disk_cache_touch_cache_user_marker(char *path)
       if (fd != -1) {
          close(fd);
       }
-   } else if (now - attr.st_mtime < 60 * 60 * 24 /* One day */) {
+   } else if (now - attr.st_mtime > 60 * 60 * 24 /* One day */) {
       (void)utime(marker_path, NULL);
    }
    free(marker_path);
aerisarn/mesa-uwp

commit 597560e27c585d9be17ffdb8731c1c3d88550cf8

broadcom/compiler: always enable per-quad on spill operations
This ensures that any channels used for helper invocations are
also spilled/filled correctly.

Alternatively, we could recursively track all temps that get
involved in computing values that are then used in explicit
(dfdx,dfdy) or implicit (texture coordinates for mipmap or
anisotropic filtering, etc) derivatives, and only enable
per-quad on these (or disable spilling of any of these
values).

Fixes:
dEQP-VK.graphicsfuzz.cov-dfdx-dfdy-after-nested-loops

Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15705>

src/broadcom/compiler/vir_register_allocate.c
@@ -343,10 +343,14 @@ v3d_emit_spill_tmua(struct v3d_compile *c,
         struct qreg offset = vir_uniform_ui(c, spill_offset);
         add_node(c, offset.index, CLASS_BITS_ANY);
 
-        struct qinst *inst =
-                vir_ADD_dest(c, vir_reg(QFILE_MAGIC, V3D_QPU_WADDR_TMUA),
-                             c->spill_base, offset);
+        /* We always enable per-quad on spills/fills to ensure we spill
+         * any channels involved with helper invocations.
+         */
+        struct qreg tmua = vir_reg(QFILE_MAGIC, V3D_QPU_WADDR_TMUAU);
+        struct qinst *inst = vir_ADD_dest(c, tmua, c->spill_base, offset);
         inst->qpu.flags.ac = cond;
+        inst->uniform = vir_get_uniform_index(c, QUNIFORM_CONSTANT,
+                                              0xffffff7f); /* per-quad */
 
         vir_emit_thrsw(c);
 

alexvorxx/zink-xlib-termux

commit d1f8351f3c1c0b9306ed6a7ead500a72d69811d3

broadcom/compiler: fix per-quad spilling
This is not safe when we have conditional spills since we could be
spilling disabled lanes with undefined values that could overwrite
valid data for those lanes from a previous spill of the same temp
that was unconditional (or that condionally enabled those same
lanes).

Fixes some Piglit OpenCL tests as well as the following OpenCL tests:
integer_divideAssign
integer_moduloAssign
integer_mad_sat
integer_ops integer_divideAssign
integer_ops integer_mad_sat
integer_ops integer_moduloAssign
integer_ops quick_char_math
integer_ops quick_short_math
math_brute_force half_powr
math_brute_force pow
math_brute_force pown
math_brute_force powr
math_brute_force rootn

Fixes: 597560e27c ('broadcom/compiler: always enable per-quad on spill operations')
Reviewed-by: Alejandro Piñeiro <apinheiro@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29909>

src/broadcom/compiler/vir_register_allocate.c
@@ -510,14 +510,19 @@ v3d_emit_spill_tmua(struct v3d_compile *c,
         add_node(c, offset.index, get_class_bit_any(c->devinfo));
 
         /* We always enable per-quad on spills/fills to ensure we spill
-         * any channels involved with helper invocations.
+         * any channels involved with helper invocations, but only if
+         * the spill is not conditional, since otherwise we may be spilling
+         * invalida lanes and overwriting valid data from a previous spill
+         * to the same address.
          */
         struct qreg tmua = vir_reg(QFILE_MAGIC, V3D_QPU_WADDR_TMUAU);
         struct qinst *inst = vir_ADD_dest(c, tmua, c->spill_base, offset);
         inst->qpu.flags.ac = cond;
         inst->ldtmu_count = 1;
-        inst->uniform = vir_get_uniform_index(c, QUNIFORM_CONSTANT,
-                                              0xffffff7f); /* per-quad */
+        inst->uniform =
+                vir_get_uniform_index(c, QUNIFORM_CONSTANT,
+                                      cond != V3D_QPU_COND_NONE ?
+                                              0xffffffff : 0xffffff7f /* per-quad*/);
 
         vir_emit_thrsw(c);
 
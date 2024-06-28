alexvorxx/zink-xlib-termux

commit a9f1151de270c5270a9e0280924b70b39d4692e1

intel/hang_replay: use hw image param
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27594>

include/drm-uapi/i915_drm.h
@@ -2155,6 +2155,13 @@ struct drm_i915_gem_context_param {
 };
 
 /*
+ * I915_CONTEXT_PARAM_CONTEXT_IMAGE:
+ *
+ * Allows userspace to provide own context images.
+ */
+#define I915_CONTEXT_PARAM_CONTEXT_IMAGE	0xe
+
+/*
  * Context SSEU programming
  *
  * It may be necessary for either functional or performance reason to configure
@@ -2548,6 +2555,24 @@ struct i915_context_param_engines {
 	__u64 extensions; \
 	struct i915_engine_class_instance engines[N__]; \
 } __attribute__((packed)) name__
+
+struct i915_gem_context_param_context_image {
+	/** @engine: Engine class & instance to be configured. */
+	struct i915_engine_class_instance engine;
+
+	/** @flags: One of the supported flags or zero. */
+	__u32 flags;
+#define I915_CONTEXT_IMAGE_FLAG_ENGINE_INDEX (1u << 0)
+
+	/** @size: Size of the image blob pointed to by @image. */
+	__u32 size;
+
+	/** @mbz: Must be zero. */
+	__u32 mbz;
+
+	/** @image: Userspace memory containing the context image. */
+	__u64 image;
+} __attribute__((packed));
 
 /**
  * struct drm_i915_gem_context_create_ext_setparam - Context parameter

src/intel/tools/intel_hang_replay.c
@@ -110,6 +110,45 @@ gem_context_create(int drm_fd)
    return create.ctx_id;
 }
 
+static bool
+gem_context_set_hw_image(int drm_fd, uint32_t ctx_id,
+                         const void *hw_img_data, uint32_t img_size)
+{
+   /* TODO: add additional information in the intel_hang_dump_block_exec &
+    * intel_hang_dump_block_hw_image structures to specify the engine and use
+    * the correct engine here.
+    */
+   struct i915_gem_context_param_context_image img_param = {
+      .engine = {
+         .engine_class = 0,
+         .engine_instance = 0,
+      },
+      .flags = I915_CONTEXT_IMAGE_FLAG_ENGINE_INDEX,
+      .size = img_size,
+      .image = (uint64_t)(uintptr_t)hw_img_data,
+   };
+   struct drm_i915_gem_context_param param = {
+      .ctx_id = ctx_id,
+      .param = I915_CONTEXT_PARAM_CONTEXT_IMAGE,
+   };
+   uint64_t val = 0;
+   int ret;
+
+   param.ctx_id = ctx_id;
+   param.param = I915_CONTEXT_PARAM_RECOVERABLE;
+   param.value = (uint64_t)(uintptr_t)&val;
+
+   ret = intel_ioctl(drm_fd, DRM_IOCTL_I915_GEM_CONTEXT_SETPARAM, &param);
+   if (ret)
+      return false;
+
+   param.param = I915_CONTEXT_PARAM_CONTEXT_IMAGE;
+   param.size = sizeof(img_param);
+   param.value = (uint64_t)(uintptr_t)&img_param;
+
+   return intel_ioctl(drm_fd, DRM_IOCTL_I915_GEM_CONTEXT_SETPARAM, &param) == 0;
+}
+
 static void*
 gem_mmap_offset(int drm_fd,
                 uint32_t gem_handle,
@@ -138,6 +177,20 @@ gem_mmap_offset(int drm_fd,
    void *map = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED,
                     drm_fd, gem_mmap.offset);
    return map;
+}
+
+static void
+write_malloc_data(void *out_data,
+                  int file_fd,
+                  size_t size)
+{
+   size_t total_read_len = 0;
+   ssize_t read_len;
+   while (total_read_len < size &&
+          (read_len = read(file_fd, out_data + total_read_len, size - total_read_len)) > 0) {
+      total_read_len += read_len;
+   }
+   assert(total_read_len == size);
 }
 
 static void
@@ -488,12 +541,19 @@ main(int argc, char *argv[])
             sizeof(struct gem_bo),
             compare_bos);
 
+      void *hw_img = NULL;
+      uint32_t hw_img_size = 0;
+
       /* Allocate BOs populate them */
       uint64_t gem_allocated = 0;
       util_dynarray_foreach(&buffers, struct gem_bo, bo) {
-         bo->gem_handle = gem_create(drm_fd, bo->size);
-         if (bo->file_offset != 0) {
-            lseek(file_fd, bo->file_offset, SEEK_SET);
+         lseek(file_fd, bo->file_offset, SEEK_SET);
+         if (bo->hw_img) {
+            hw_img = malloc(bo->size);
+            write_malloc_data(hw_img, file_fd, bo->size);
+            hw_img_size = bo->size;
+         } else {
+            bo->gem_handle = gem_create(drm_fd, bo->size);
             write_gem_bo_data(drm_fd, bo->gem_handle, file_fd, bo->size);
          }
 
@@ -504,6 +564,13 @@ main(int argc, char *argv[])
       if (ctx_id == 0) {
          fprintf(stderr, "fail to create context: %s\n", strerror(errno));
          return EXIT_FAILURE;
+      }
+
+      if (hw_img != NULL) {
+         if (!gem_context_set_hw_image(drm_fd, ctx_id, hw_img, hw_img_size)) {
+            fprintf(stderr, "fail to set context hw img: %s\n", strerror(errno));
+            return EXIT_FAILURE;
+         }
       }
 
       struct util_dynarray execbuffer_bos;
@@ -523,6 +590,9 @@ main(int argc, char *argv[])
                continue;
          }
 
+         if (bo->hw_img)
+            continue;
+
          struct drm_i915_gem_exec_object2 *execbuf_bo =
             util_dynarray_grow(&execbuffer_bos, struct drm_i915_gem_exec_object2, 1);
          *execbuf_bo = (struct drm_i915_gem_exec_object2) {
@@ -533,9 +603,6 @@ main(int argc, char *argv[])
                                 EXEC_OBJECT_PINNED,
             .offset           = bo->offset,
          };
-
-         if (bo->hw_img)
-            execbuf_bo->flags |= EXEC_OBJECT_NEEDS_GTT;
       }
 
       assert(batch_bo != NULL);

commit d8c2930da0e36b4d4e952003cbb63d67982cc5aa

drm-uapi: Sync i915_drm.h with a78313bb206e
Sync i915_drm.h with a78313bb206e
commit a78313bb206e0c456a989f380c4cbd8af8af7c76
Author: Dave Airlie <airlied@redhat.com>
Date:   Thu Jun 27 17:21:43 2024 +1000

    Merge tag 'drm-intel-gt-next-2024-06-12' of https://gitlab.freedesktop.org/drm/i915/kernel into drm-next

Fixes: a9f1151de27 ("intel/hang_replay: use hw image param")

Signed-off-by: Sushma Venkatesh Reddy <sushma.venkatesh.reddy@intel.com>
Reviewed-by: Jordan Justen <jordan.l.justen@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29885>

include/drm-uapi/i915_drm.h
@@ -806,6 +806,12 @@ typedef struct drm_i915_irq_wait {
  */
 #define I915_PARAM_PXP_STATUS		 58
 
+/*
+ * Query if kernel allows marking a context to send a Freq hint to SLPC. This
+ * will enable use of the strategies allowed by the SLPC algorithm.
+ */
+#define I915_PARAM_HAS_CONTEXT_FREQ_HINT	59
+
 /* Must be kept compact -- no holes and well documented */
 
 /**
@@ -2148,18 +2154,29 @@ struct drm_i915_gem_context_param {
  * -EIO: The firmware did not succeed in creating the protected context.
  */
 #define I915_CONTEXT_PARAM_PROTECTED_CONTENT    0xd
-/* Must be kept compact -- no holes and well documented */
 
-	/** @value: Context parameter value to be set or queried */
-	__u64 value;
-};
+/*
+ * I915_CONTEXT_PARAM_LOW_LATENCY:
+ *
+ * Mark this context as a low latency workload which requires aggressive GT
+ * frequency scaling. Use I915_PARAM_HAS_CONTEXT_FREQ_HINT to check if the kernel
+ * supports this per context flag.
+ */
+#define I915_CONTEXT_PARAM_LOW_LATENCY		0xe
 
 /*
  * I915_CONTEXT_PARAM_CONTEXT_IMAGE:
  *
  * Allows userspace to provide own context images.
+ *
+ * Note that this is a debug API not available on production kernel builds.
  */
-#define I915_CONTEXT_PARAM_CONTEXT_IMAGE	0xe
+#define I915_CONTEXT_PARAM_CONTEXT_IMAGE	0xf
+/* Must be kept compact -- no holes and well documented */
+
+	/** @value: Context parameter value to be set or queried */
+	__u64 value;
+};
 
 /*
  * Context SSEU programming
@@ -2648,19 +2665,29 @@ struct drm_i915_reg_read {
  *
  */
 
+/*
+ * struct drm_i915_reset_stats - Return global reset and other context stats
+ *
+ * Driver keeps few stats for each contexts and also global reset count.
+ * This struct can be used to query those stats.
+ */
 struct drm_i915_reset_stats {
+	/** @ctx_id: ID of the requested context */
 	__u32 ctx_id;
+
+	/** @flags: MBZ */
 	__u32 flags;
 
-	/* All resets since boot/module reload, for all contexts */
+	/** @reset_count: All resets since boot/module reload, for all contexts */
 	__u32 reset_count;
 
-	/* Number of batches lost when active in GPU, for this context */
+	/** @batch_active: Number of batches lost when active in GPU, for this context */
 	__u32 batch_active;
 
-	/* Number of batches lost pending for execution, for this context */
+	/** @batch_pending: Number of batches lost pending for execution, for this context */
 	__u32 batch_pending;
 
+	/** @pad: MBZ */
 	__u32 pad;
 };
 
@@ -3597,9 +3624,13 @@ struct drm_i915_query_memory_regions {
  * struct drm_i915_query_guc_submission_version - query GuC submission interface version
  */
 struct drm_i915_query_guc_submission_version {
+	/** @branch: Firmware branch version. */
 	__u32 branch;
+	/** @major: Firmware major version. */
 	__u32 major;
+	/** @minor: Firmware minor version. */
 	__u32 minor;
+	/** @patch: Firmware patch version. */
 	__u32 patch;
 };
 
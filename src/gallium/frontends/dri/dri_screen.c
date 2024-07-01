alexvorxx/zink-xlib-termux

commit ad0edea53a732fecf6ddb8340e724b66275a0619

st/dri: Check format properties from format helpers
When populating DRI configs, driconf allows us to exclude
10bpc/RGBA/float configs. Rather than having a hardcoded set of formats
which satisfy those properties, just check the actual format
definitions.

Signed-off-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27709>

src/gallium/frontends/dri/dri_screen.c
@@ -386,26 +386,27 @@ dri_fill_in_modes(struct dri_screen *screen)
 
       /* Expose only BGRA ordering if the loader doesn't support RGBA ordering. */
       if (!allow_rgba_ordering &&
-          (pipe_formats[f] == PIPE_FORMAT_RGBA8888_UNORM ||
-           pipe_formats[f] == PIPE_FORMAT_RGBX8888_UNORM ||
-           pipe_formats[f] == PIPE_FORMAT_RGBA8888_SRGB  ||
-           pipe_formats[f] == PIPE_FORMAT_RGBX8888_SRGB  ||
-           pipe_formats[f] == PIPE_FORMAT_R5G5B5A1_UNORM ||
-           pipe_formats[f] == PIPE_FORMAT_R5G5B5X1_UNORM ||
-           pipe_formats[f] == PIPE_FORMAT_R4G4B4A4_UNORM ||
-           pipe_formats[f] == PIPE_FORMAT_R4G4B4X4_UNORM))
+          util_format_get_component_shift(pipe_formats[f],
+                                          UTIL_FORMAT_COLORSPACE_RGB, 0)
+#if UTIL_ARCH_BIG_ENDIAN
+         >
+#else
+         <
+#endif
+          util_format_get_component_shift(pipe_formats[f],
+                                          UTIL_FORMAT_COLORSPACE_RGB, 2))
          continue;
 
       if (!allow_rgb10 &&
-          (pipe_formats[f] == PIPE_FORMAT_B10G10R10A2_UNORM ||
-           pipe_formats[f] == PIPE_FORMAT_B10G10R10X2_UNORM ||
-           pipe_formats[f] == PIPE_FORMAT_R10G10B10A2_UNORM ||
-           pipe_formats[f] == PIPE_FORMAT_R10G10B10X2_UNORM))
+          util_format_get_component_bits(pipe_formats[f],
+                                         UTIL_FORMAT_COLORSPACE_RGB, 0) == 10 &&
+          util_format_get_component_bits(pipe_formats[f],
+                                         UTIL_FORMAT_COLORSPACE_RGB, 1) == 10 &&
+          util_format_get_component_bits(pipe_formats[f],
+                                         UTIL_FORMAT_COLORSPACE_RGB, 2) == 10)
          continue;
 
-      if (!allow_fp16 &&
-          (pipe_formats[f] == PIPE_FORMAT_R16G16B16A16_FLOAT ||
-           pipe_formats[f] == PIPE_FORMAT_R16G16B16X16_FLOAT))
+      if (!allow_fp16 && util_format_is_float(pipe_formats[f]))
          continue;
 
       if (!p_screen->is_format_supported(p_screen, pipe_formats[f],

commit cbd19e09d1e293b6b34a30312aac3c865f42b244

dri: Go back to hard-coded list of RGBA formats
Catching these programmatically without false positives / negatives is
surprisingly tricky, go back to the known-working list for now.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/11398
Fixes: ad0edea53a73 ("st/dri: Check format properties from format helpers")
Fixes: 5ca85d75c05d ("dri: Fix BGR format exclusion")

v2:
* Also put back lima fails removed by 9eeaa4618f8a ("egl/gbm: Enable
  RGBA configs"), as those tests are now failing again.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29979>

src/gallium/frontends/dri/dri_screen.c
@@ -386,21 +386,16 @@ dri_fill_in_modes(struct dri_screen *screen)
       uint8_t msaa_modes[MSAA_VISUAL_MAX_SAMPLES];
 
       /* Expose only BGRA ordering if the loader doesn't support RGBA ordering. */
-      if (!allow_rgba_ordering) {
-          unsigned sh_ax = util_format_get_component_shift(pipe_formats[f], UTIL_FORMAT_COLORSPACE_RGB, 3);
-          unsigned sh_b = util_format_get_component_shift(pipe_formats[f], UTIL_FORMAT_COLORSPACE_RGB, 2);
-#if UTIL_ARCH_BIG_ENDIAN
-          unsigned sz_b = util_format_get_component_bits(pipe_formats[f], UTIL_FORMAT_COLORSPACE_RGB, 2);
-
-          if (sz_b + sh_b == sh_ax)
-             continue;
-#else
-          unsigned sz_ax = util_format_get_component_bits(pipe_formats[f], UTIL_FORMAT_COLORSPACE_RGB, 3);
-
-          if (sz_ax + sh_ax == sh_b)
-             continue;
-#endif
-       }
+      if (!allow_rgba_ordering &&
+          (pipe_formats[f] == PIPE_FORMAT_RGBA8888_UNORM ||
+           pipe_formats[f] == PIPE_FORMAT_RGBX8888_UNORM ||
+           pipe_formats[f] == PIPE_FORMAT_RGBA8888_SRGB  ||
+           pipe_formats[f] == PIPE_FORMAT_RGBX8888_SRGB  ||
+           pipe_formats[f] == PIPE_FORMAT_R5G5B5A1_UNORM ||
+           pipe_formats[f] == PIPE_FORMAT_R5G5B5X1_UNORM ||
+           pipe_formats[f] == PIPE_FORMAT_R4G4B4A4_UNORM ||
+           pipe_formats[f] == PIPE_FORMAT_R4G4B4X4_UNORM))
+         continue;
 
       if (!allow_rgb10 &&
           util_format_get_component_bits(pipe_formats[f],
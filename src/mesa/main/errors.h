22bd26079fbffcc0fa14b8c943692c55ace15c18
Timothy Arceri <tarceri@itsqueeze.com>
2024-07-03 19:08:10 +1000

    util/mesa: move mesa/main log code to util
    This removes the unrequired dependance on _mesa_init_debug() and moves
    all log code to the util file so that _mesa_log* can now be used without
    creating a dependance on mesa/main. Since the code we are moving depends
    on the code already in the util (as it was moved here previously) this is
    also a much better spot for the code.
    
    Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30012>

--- src/mesa/main/errors.h
+++ src/mesa/main/errors.h
@@ -63,12 +63,6 @@ _mesa_error_no_memory(const char *caller);
 extern void
 _mesa_debug( const struct gl_context *ctx, const char *fmtString, ... ) PRINTFLIKE(2, 3);
 
-extern void
-_mesa_log(const char *fmtString, ...) PRINTFLIKE(1, 2);
-
-extern void
-_mesa_log_direct(const char *string);
-
 
 void
 _mesa_shader_debug(struct gl_context *ctx, GLenum type, GLuint *id,

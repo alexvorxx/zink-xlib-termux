6fd427bf6eb8471683598ade08ddc7588141ed2b
Faith Ekstrand <faith.ekstrand@collabora.com>
2024-07-12 13:37:09 -0500

    nak: Lower fsqrt in NIR on Maxwell A and earlier
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30281>

--- src/nouveau/compiler/nak/api.rs
+++ src/nouveau/compiler/nak/api.rs
@@ -101,6 +101,7 @@ fn nir_options(dev: &nv_device_info) -> nir_shader_compiler_options {
     op.lower_flrp16 = true;
     op.lower_flrp32 = true;
     op.lower_flrp64 = true;
+    op.lower_fsqrt = dev.sm < 52;
     op.lower_bitfield_extract = dev.sm >= 70;
     op.lower_bitfield_insert = true;
     op.lower_pack_half_2x16 = true;

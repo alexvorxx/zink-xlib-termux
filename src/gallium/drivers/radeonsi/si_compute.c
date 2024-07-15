aerisarn/mesa-uwp

commit 3b0bfd254f722e5773f70c6cb367e859876a4208

radeonsi/gfx11: make flat_scratch changes for compute
make flat_scratch gen11 changes for compute

v5: optimize the code for size (Pierre-Eric)
v4: remove type cast from 64bit to 32bit (Marek Olšák)
    use radeon_set_sh_reg_seq (Marek Olšák)
    combine RSRC and scratch reg write packets (Marek Olšák)
v3: fix coding guidelines (Marek Olšák)
v2: do not skip si_resource_reference() call (Marek Olšák)

Signed-off-by: Yogesh mohan marimuthu <yogesh.mohanmarimuthu@amd.com>
Acked-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16328>

src/gallium/drivers/radeonsi/si_compute.c
@@ -472,11 +472,12 @@ static bool si_setup_compute_scratch_buffer(struct si_context *sctx, struct si_s
    }
 
    if (sctx->compute_scratch_buffer != shader->scratch_bo && scratch_needed) {
-      uint64_t scratch_va = sctx->compute_scratch_buffer->gpu_address;
+      if (sctx->chip_class < GFX11) {
+         uint64_t scratch_va = sctx->compute_scratch_buffer->gpu_address;
 
-      if (!si_shader_binary_upload(sctx->screen, shader, scratch_va))
-         return false;
-
+         if (!si_shader_binary_upload(sctx->screen, shader, scratch_va))
+            return false;
+      }
       si_resource_reference(&shader->scratch_bo, sctx->compute_scratch_buffer);
    }
 
@@ -550,7 +551,14 @@ static bool si_switch_compute_shader(struct si_context *sctx, struct si_compute 
    radeon_begin(cs);
    radeon_set_sh_reg(R_00B830_COMPUTE_PGM_LO, shader_va >> 8);
 
-   radeon_set_sh_reg_seq(R_00B848_COMPUTE_PGM_RSRC1, 2);
+   if (sctx->chip_class >= GFX11 && shader->scratch_bo) {
+      radeon_set_sh_reg_seq(R_00B840_COMPUTE_DISPATCH_SCRATCH_BASE_LO, 4);
+      radeon_emit(sctx->compute_scratch_buffer->gpu_address >> 8);
+      radeon_emit(sctx->compute_scratch_buffer->gpu_address >> 40);
+   } else {
+      radeon_set_sh_reg_seq(R_00B848_COMPUTE_PGM_RSRC1, 2);
+   }
+
    radeon_emit(config->rsrc1);
    radeon_emit(config->rsrc2);
 

alexvorxx/zink-xlib-termux

commit 2afd233145460a8de7672f0e2bb7416a16d0f176

radeonsi: lock a mutex when updating scratch_va for compute shaders
Fixes: 3b0bfd254f7 - radeonsi/gfx11: make flat_scratch changes for compute

Reviewed-by: Qiang Yu <yuq825@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30071>

src/gallium/drivers/radeonsi/si_compute.c
@@ -218,6 +218,7 @@ static void *si_create_compute_state(struct pipe_context *ctx, const struct pipe
    pipe_reference_init(&sel->base.reference, 1);
    sel->stage = MESA_SHADER_COMPUTE;
    sel->screen = sscreen;
+   simple_mtx_init(&sel->mutex, mtx_plain);
    sel->const_and_shader_buf_descriptors_index =
       si_const_and_shader_buffer_descriptors_idx(PIPE_SHADER_COMPUTE);
    sel->sampler_and_images_descriptors_index =
@@ -480,6 +481,16 @@ static bool si_switch_compute_shader(struct si_context *sctx, struct si_compute 
    }
 
    if (config->scratch_bytes_per_wave) {
+      /* Prevent race conditions for accesses to shader->scratch_va and shader->bo, which
+       * can change when scratch_va is updated. Any accesses to shader->bo must also be inside
+       * the lock.
+       *
+       * TODO: This lock could be removed if the scratch address was passed via user SGPRs instead
+       *       of the shader binary.
+       */
+      if (!sctx->screen->info.has_scratch_base_registers)
+         simple_mtx_lock(&shader->selector->mutex);
+
       /* Update max_seen_compute_scratch_bytes_per_wave and compute_tmpring_size. */
       ac_get_scratch_tmpring_size(&sctx->screen->info,
                                   config->scratch_bytes_per_wave,
@@ -502,6 +513,12 @@ static bool si_switch_compute_shader(struct si_context *sctx, struct si_compute 
 
    radeon_add_to_buffer_list(sctx, &sctx->gfx_cs, shader->bo,
                              RADEON_USAGE_READ | RADEON_PRIO_SHADER_BINARY);
+
+   /* shader->bo can't be used after this if the scratch address is inserted into the shader
+    * binary.
+    */
+   if (config->scratch_bytes_per_wave && !sctx->screen->info.has_scratch_base_registers)
+      simple_mtx_unlock(&shader->selector->mutex);
 
    if (sctx->gfx_level >= GFX12) {
       unsigned rsrc3 = S_00B8A0_INST_PREF_SIZE_GFX12(si_get_shader_prefetch_size(shader));
@@ -1307,6 +1324,7 @@ void si_destroy_compute(struct si_compute *program)
 
    si_shader_destroy(&program->shader);
    ralloc_free(program->sel.nir);
+   simple_mtx_destroy(&sel->mutex);
    FREE(program);
 }
 
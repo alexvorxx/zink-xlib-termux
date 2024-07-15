aerisarn/mesa-uwp

commit 3b0bfd254f722e5773f70c6cb367e859876a4208

radeonsi/gfx11: make flat_scratch changes for compute
make flat_scratch gen11 changes for compute

v5: optimize the code for size (Pierre-Eric)
v4: remove type cast from 64bit to 32bit (Marek Olšák)
    use radeon_set_sh_reg_seq (Marek Olšák)
    combine RSRC and scratch reg write packets (Marek Olšák)
v3: fix coding guidelines (Marek Olšák)
v2: do not skip si_resource_reference() call (Marek Olšák)

Signed-off-by: Yogesh mohan marimuthu <yogesh.mohanmarimuthu@amd.com>
Acked-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/16328>

src/gallium/drivers/radeonsi/si_compute.c
@@ -472,11 +472,12 @@ static bool si_setup_compute_scratch_buffer(struct si_context *sctx, struct si_s
    }
 
    if (sctx->compute_scratch_buffer != shader->scratch_bo && scratch_needed) {
-      uint64_t scratch_va = sctx->compute_scratch_buffer->gpu_address;
+      if (sctx->chip_class < GFX11) {
+         uint64_t scratch_va = sctx->compute_scratch_buffer->gpu_address;
 
-      if (!si_shader_binary_upload(sctx->screen, shader, scratch_va))
-         return false;
-
+         if (!si_shader_binary_upload(sctx->screen, shader, scratch_va))
+            return false;
+      }
       si_resource_reference(&shader->scratch_bo, sctx->compute_scratch_buffer);
    }
 
@@ -550,7 +551,14 @@ static bool si_switch_compute_shader(struct si_context *sctx, struct si_compute 
    radeon_begin(cs);
    radeon_set_sh_reg(R_00B830_COMPUTE_PGM_LO, shader_va >> 8);
 
-   radeon_set_sh_reg_seq(R_00B848_COMPUTE_PGM_RSRC1, 2);
+   if (sctx->chip_class >= GFX11 && shader->scratch_bo) {
+      radeon_set_sh_reg_seq(R_00B840_COMPUTE_DISPATCH_SCRATCH_BASE_LO, 4);
+      radeon_emit(sctx->compute_scratch_buffer->gpu_address >> 8);
+      radeon_emit(sctx->compute_scratch_buffer->gpu_address >> 40);
+   } else {
+      radeon_set_sh_reg_seq(R_00B848_COMPUTE_PGM_RSRC1, 2);
+   }
+
    radeon_emit(config->rsrc1);
    radeon_emit(config->rsrc2);
 

alexvorxx/zink-xlib-termux

commit c353394a21b90582cff6c461ede160e3eda81462

radeonsi: replace si_shader::scratch_bo with scratch_va, don't set it on gfx11+
This removes the unnecessary buffer reference and improves this fragile
code.

Fixes: 3b0bfd254f7 - radeonsi/gfx11: make flat_scratch changes for compute
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/11463

Reviewed-by: Qiang Yu <yuq825@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30071>

src/gallium/drivers/radeonsi/si_compute.c
@@ -383,13 +383,17 @@ static void si_set_global_binding(struct pipe_context *ctx, unsigned first, unsi
    }
 }
 
-static bool si_setup_compute_scratch_buffer(struct si_context *sctx, struct si_shader *shader)
+static bool si_setup_compute_scratch_buffer(struct si_context *sctx, struct si_shader *shader,
+                                            const struct ac_shader_config *config)
 {
    uint64_t scratch_bo_size, scratch_needed;
    scratch_bo_size = 0;
    scratch_needed = sctx->max_seen_compute_scratch_bytes_per_wave * sctx->screen->info.max_scratch_waves;
    if (sctx->compute_scratch_buffer)
       scratch_bo_size = sctx->compute_scratch_buffer->b.b.width0;
+
+   if (!scratch_needed)
+      return true;
 
    if (scratch_bo_size < scratch_needed) {
       si_resource_reference(&sctx->compute_scratch_buffer, NULL);
@@ -405,15 +409,17 @@ static bool si_setup_compute_scratch_buffer(struct si_context *sctx, struct si_s
          return false;
    }
 
-   if (sctx->compute_scratch_buffer != shader->scratch_bo && scratch_needed) {
-      if (sctx->gfx_level < GFX11 &&
-          (sctx->family < CHIP_GFX940 || sctx->screen->info.has_graphics)) {
-         uint64_t scratch_va = sctx->compute_scratch_buffer->gpu_address;
+   /* Set the scratch address in the shader binary. */
+   if (config->scratch_bytes_per_wave && sctx->gfx_level < GFX11 &&
+       (sctx->family < CHIP_GFX940 || sctx->screen->info.has_graphics)) {
+      uint64_t scratch_va = sctx->compute_scratch_buffer->gpu_address;
 
+      if (shader->scratch_va != scratch_va) {
          if (!si_shader_binary_upload(sctx->screen, shader, scratch_va))
             return false;
+
+         shader->scratch_va = scratch_va;
       }
-      si_resource_reference(&shader->scratch_bo, sctx->compute_scratch_buffer);
    }
 
    return true;
@@ -483,11 +489,11 @@ static bool si_switch_compute_shader(struct si_context *sctx, struct si_compute 
                                config->scratch_bytes_per_wave,
                                &sctx->max_seen_compute_scratch_bytes_per_wave, &tmpring_size);
 
-   if (!si_setup_compute_scratch_buffer(sctx, shader))
+   if (!si_setup_compute_scratch_buffer(sctx, shader, config))
       return false;
 
-   if (shader->scratch_bo) {
-      radeon_add_to_buffer_list(sctx, &sctx->gfx_cs, shader->scratch_bo,
+   if (config->scratch_bytes_per_wave) {
+      radeon_add_to_buffer_list(sctx, &sctx->gfx_cs, sctx->compute_scratch_buffer,
                                 RADEON_USAGE_READWRITE | RADEON_PRIO_SCRATCH_BUFFER);
    }
 
@@ -513,7 +519,7 @@ static bool si_switch_compute_shader(struct si_context *sctx, struct si_compute 
                                     SI_TRACKED_COMPUTE_PGM_RSRC3, rsrc3);
       gfx12_opt_push_compute_sh_reg(R_00B860_COMPUTE_TMPRING_SIZE,
                                     SI_TRACKED_COMPUTE_TMPRING_SIZE, tmpring_size);
-      if (shader->scratch_bo) {
+      if (config->scratch_bytes_per_wave) {
          gfx12_opt_push_compute_sh_reg(R_00B840_COMPUTE_DISPATCH_SCRATCH_BASE_LO,
                                        SI_TRACKED_COMPUTE_DISPATCH_SCRATCH_BASE_LO,
                                        sctx->compute_scratch_buffer->gpu_address >> 8);
@@ -533,7 +539,7 @@ static bool si_switch_compute_shader(struct si_context *sctx, struct si_compute 
                                     SI_TRACKED_COMPUTE_PGM_RSRC3, rsrc3);
       gfx11_opt_push_compute_sh_reg(R_00B860_COMPUTE_TMPRING_SIZE,
                                     SI_TRACKED_COMPUTE_TMPRING_SIZE, tmpring_size);
-      if (shader->scratch_bo) {
+      if (config->scratch_bytes_per_wave) {
          gfx11_opt_push_compute_sh_reg(R_00B840_COMPUTE_DISPATCH_SCRATCH_BASE_LO,
                                        SI_TRACKED_COMPUTE_DISPATCH_SCRATCH_BASE_LO,
                                        sctx->compute_scratch_buffer->gpu_address >> 8);
@@ -550,7 +556,7 @@ static bool si_switch_compute_shader(struct si_context *sctx, struct si_compute 
       radeon_opt_set_sh_reg(R_00B860_COMPUTE_TMPRING_SIZE,
                             SI_TRACKED_COMPUTE_TMPRING_SIZE, tmpring_size);
 
-      if (shader->scratch_bo &&
+      if (config->scratch_bytes_per_wave &&
           (sctx->gfx_level >= GFX11 ||
            (sctx->family >= CHIP_GFX940 && !sctx->screen->info.has_graphics))) {
          radeon_opt_set_sh_reg2(R_00B840_COMPUTE_DISPATCH_SCRATCH_BASE_LO,

src/gallium/drivers/radeonsi/si_shader.c
@@ -3423,7 +3423,7 @@ bool si_create_shader_variant(struct si_screen *sscreen, struct ac_llvm_compiler
             assert(sel->main_shader_part->gs_copy_shader);
             assert(sel->main_shader_part->gs_copy_shader->bo);
             assert(!sel->main_shader_part->gs_copy_shader->previous_stage_sel);
-            assert(!sel->main_shader_part->gs_copy_shader->scratch_bo);
+            assert(!sel->main_shader_part->gs_copy_shader->scratch_va);
 
             shader->gs_copy_shader = CALLOC_STRUCT(si_shader);
             memcpy(shader->gs_copy_shader, sel->main_shader_part->gs_copy_shader,
@@ -3559,9 +3559,6 @@ void si_shader_binary_clean(struct si_shader_binary *binary)
 
 void si_shader_destroy(struct si_shader *shader)
 {
-   if (shader->scratch_bo)
-      si_resource_reference(&shader->scratch_bo, NULL);
-
    si_resource_reference(&shader->bo, NULL);
 
    if (!shader->is_binary_shared)

src/gallium/drivers/radeonsi/si_shader.h
@@ -866,7 +866,10 @@ struct si_shader {
     * in use.
     */
    uint64_t gpu_address;
-   struct si_resource *scratch_bo;
+   /* Only used on GFX6-10 where the scratch address must be inserted into the shader binary.
+    * This is the scratch address that the current shader binary contains.
+    */
+   uint64_t scratch_va;
    union si_shader_key key;
    struct util_queue_fence ready;
    bool compilation_failed;

src/gallium/drivers/radeonsi/si_state_shaders.cpp
@@ -4268,7 +4268,7 @@ static int si_update_scratch_buffer(struct si_context *sctx, struct si_shader *s
       return 0;
 
    /* Prevent race conditions when updating:
-    * - si_shader::scratch_bo
+    * - si_shader::scratch_va
     * - si_shader::binary::code
     * - si_shader::previous_stage::binary::code.
     */
@@ -4276,7 +4276,7 @@ static int si_update_scratch_buffer(struct si_context *sctx, struct si_shader *s
 
    /* This shader is already configured to use the current
     * scratch buffer. */
-   if (shader->scratch_bo == sctx->scratch_buffer) {
+   if (shader->scratch_va == scratch_va) {
       si_shader_unlock(shader);
       return 0;
    }
@@ -4291,8 +4291,7 @@ static int si_update_scratch_buffer(struct si_context *sctx, struct si_shader *s
 
    /* Update the shader state to use the new shader bo. */
    si_shader_init_pm4_state(sctx->screen, shader);
-
-   si_resource_reference(&shader->scratch_bo, sctx->scratch_buffer);
+   shader->scratch_va = scratch_va;
 
    si_shader_unlock(shader);
    return 1;
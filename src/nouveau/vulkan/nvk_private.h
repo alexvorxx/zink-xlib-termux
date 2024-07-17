alexvorxx/zink-xlib-termux

commit 58181b7bbcf915b3262e7707cb88aabb6d7fd673

nvk: Bump the sparse alignment requirement on buffers to 64K
Otherwise, if they live in VRAM, binding might fail.

Fixes: 03f0f01904ce ("nvk: Add support for sparse buffers")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30033>

src/nouveau/vulkan/nvk_private.h
@@ -35,6 +35,8 @@
 /* Max size of a bound cbuf */
 #define NVK_MAX_CBUF_SIZE (1u << 16)
 
+#define NVK_SPARSE_BIND_ALIGN_B (1u << 16)
+
 struct nvk_addr_range {
    uint64_t addr;
    uint64_t range;

src/nouveau/vulkan/nvk_buffer.c
@@ -28,7 +28,7 @@ nvk_get_buffer_alignment(const struct nvk_physical_device *pdev,
 
    if (create_flags & (VK_BUFFER_CREATE_SPARSE_BINDING_BIT |
                        VK_BUFFER_CREATE_DEVICE_ADDRESS_CAPTURE_REPLAY_BIT))
-      alignment = MAX2(alignment, 4096);
+      alignment = MAX2(alignment, NVK_SPARSE_BIND_ALIGN_B);
 
    return alignment;
 }

commit 68c06558be46421ae3357c0062bd0525f804e097

nvk: Drop the sparse alignment back down to 4096
nouveau uses the OS page size which is almost always 4096.  The next
patch will make this properly queried but this version is back-portable.

Fixes: 58181b7bbcf9 ("nvk: Bump the sparse alignment requirement on buffers to 64K")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30138>

src/nouveau/vulkan/nvk_private.h
@@ -35,7 +35,7 @@
 /* Max size of a bound cbuf */
 #define NVK_MAX_CBUF_SIZE (1u << 16)
 
-#define NVK_SPARSE_BIND_ALIGN_B (1u << 16)
+#define NVK_SPARSE_BIND_ALIGN_B (1u << 12)
 
 struct nvk_addr_range {
    uint64_t addr;
cd7128c2e305eb7673dcd1c35baf4f3e41d39d35
Faith Ekstrand <faith.ekstrand@collabora.com>
2024-07-18 11:20:28 -0500

    nak: Add a bare HW shader runner
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30275>

--- src/nouveau/compiler/nak_bindings.h
+++ src/nouveau/compiler/nak_bindings.h
@@ -4,3 +4,15 @@
  */
 
 #include "nak_private.h"
+
+#include "nouveau_bo.h"
+#include "nouveau_context.h"
+#include "nouveau_device.h"
+
+#include <xf86drm.h>
+#include "drm-uapi/nouveau_drm.h"
+
+#define DRM_RS_IOCTL(FOO) \
+   static const unsigned long DRM_RS_IOCTL_##FOO = DRM_IOCTL_##FOO
+
+DRM_RS_IOCTL(NOUVEAU_EXEC);

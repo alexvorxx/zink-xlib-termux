alexvorxx/zink-xlib-termux

commit 50fc7cc290db49288b318c116c6fdc3daaf801e9

glx: directly link to gallium
this eliminates the loader interface, which avoids an entire class
of issues and simplifies a bunch of code

Acked-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29771>

.gitlab-ci/vkd3d-proton/run.sh
@@ -17,7 +17,7 @@ mkdir -p "$RESULTS"
 # Modifiying here directly LD_LIBRARY_PATH may cause problems when
 # using a command wrapper. Hence, we will just set it when running the
 # command.
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$INSTALL/lib/:/vkd3d-proton-tests/x64/"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$INSTALL/lib/:$INSTALL/lib/dri/vkd3d-proton-tests/x64/"
 
 
 # Set the Vulkan driver to use.

.gitlab-ci/common/init-stage2.sh
@@ -94,7 +94,7 @@ fi
 # Fix prefix confusion: the build installs to $CI_PROJECT_DIR, but we expect
 # it in /install
 ln -sf $CI_PROJECT_DIR/install /install
-export LD_LIBRARY_PATH=/install/lib
+export LD_LIBRARY_PATH=/install/lib:/install/lib/dri
 export LIBGL_DRIVERS_PATH=/install/lib/dri
 
 # https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22495#note_1876691
@@ -113,7 +113,7 @@ export PYTHONPATH=$(python3 -c "import sys;print(\":\".join(sys.path))")
 if [ -n "$MESA_LOADER_DRIVER_OVERRIDE" ]; then
   rm /install/lib/dri/!($MESA_LOADER_DRIVER_OVERRIDE)_dri.so
 fi
-ls -1 /install/lib/dri/*_dri.so || true
+ls -1 /install/lib/dri/*.so || true
 
 if [ "$HWCI_FREQ_MAX" = "true" ]; then
   # Ensure initialization of the DRM device (needed by MSM)

.gitlab-ci/common/start-x.sh
@@ -7,7 +7,7 @@ _FLAG_FILE="/xorg-started"
 
 echo "touch ${_FLAG_FILE}; sleep 100000" > "${_XORG_SCRIPT}"
 if [ "x$1" != "x" ]; then
-    export LD_LIBRARY_PATH="${1}/lib"
+    export LD_LIBRARY_PATH="${1}/lib:${1}/lib/dri"
     export LIBGL_DRIVERS_PATH="${1}/lib/dri"
 fi
 xinit /bin/sh "${_XORG_SCRIPT}" -- /usr/bin/Xorg vt45 -noreset -s 0 -dpms -logfile /Xorg.0.log &

.gitlab-ci/cuttlefish-runner.sh
@@ -6,7 +6,7 @@ set -xe
 
 export HOME=/cuttlefish
 export PATH=$PATH:/cuttlefish/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${CI_PROJECT_DIR}/install/lib/:/cuttlefish/lib64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${CI_PROJECT_DIR}/install/lib/:${CI_PROJECT_DIR}/install/lib/dri:/cuttlefish/lib64
 export EGL_PLATFORM=surfaceless
 
 syslogd

.gitlab-ci/deqp-runner.sh
@@ -16,7 +16,7 @@ fi
 INSTALL=$(realpath -s "$PWD"/install)
 
 # Set up the driver environment.
-export LD_LIBRARY_PATH="$INSTALL"/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="$INSTALL"/lib/:"$INSTALL"/lib/dri:$LD_LIBRARY_PATH
 export EGL_PLATFORM=surfaceless
 ARCH=$(uname -m)
 export VK_DRIVER_FILES="$PWD"/install/share/vulkan/icd.d/"$VK_DRIVER"_icd."$ARCH".json

.gitlab-ci/fossilize-runner.sh
@@ -10,7 +10,7 @@ fi
 INSTALL=$PWD/install
 
 # Set up the driver environment.
-export LD_LIBRARY_PATH="$INSTALL/lib/"
+export LD_LIBRARY_PATH="$INSTALL/lib/:$INSTALL/lib/dri"
 export VK_DRIVER_FILES="$INSTALL/share/vulkan/icd.d/${VK_DRIVER}_icd.x86_64.json"
 
 # To store Fossilize logs on failure.

.gitlab-ci/gtest-runner.sh
@@ -6,7 +6,7 @@ set -ex
 INSTALL=$PWD/install
 
 # Set up the driver environment.
-export LD_LIBRARY_PATH=$INSTALL/lib/
+export LD_LIBRARY_PATH=$INSTALL/lib/:$INSTALL/lib/dri
 
 RESULTS="$PWD/${GTEST_RESULTS_DIR:-results}"
 mkdir -p "$RESULTS"

.gitlab-ci/piglit/piglit-runner.sh
@@ -11,7 +11,7 @@ fi
 INSTALL="$PWD/install"
 
 # Set up the driver environment.
-export LD_LIBRARY_PATH="$INSTALL/lib/"
+export LD_LIBRARY_PATH="$INSTALL/lib/:$INSTALL/lib/dri"
 export EGL_PLATFORM=surfaceless
 ARCH=$(uname -m)
 export VK_DRIVER_FILES="$INSTALL/share/vulkan/icd.d/${VK_DRIVER}_icd.$ARCH.json"

commit 23ef65ea3b859368c6ba4fcc66ba4d251b3f5afd

ci/vkd3d: fix LD_LIBRARY_PATH
Fixes: 50fc7cc290db49288b31 ("glx: directly link to gallium")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30258>

.gitlab-ci/vkd3d-proton/run.sh
@@ -17,7 +17,7 @@ mkdir -p "$RESULTS"
 # Modifiying here directly LD_LIBRARY_PATH may cause problems when
 # using a command wrapper. Hence, we will just set it when running the
 # command.
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$INSTALL/lib/:$INSTALL/lib/dri/vkd3d-proton-tests/x64/"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$INSTALL/lib/:$INSTALL/lib/dri:/vkd3d-proton-tests/x64/"
 
 
 # Set the Vulkan driver to use.
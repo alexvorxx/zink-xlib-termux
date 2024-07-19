alexvorxx/zink-xlib-termux

commit fa541a887c8a6732136280dce18fabfb481b34d7

loader: delete loader_open_driver()
no longer used

Acked-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29771>

src/loader/loader.c
@@ -863,52 +863,3 @@ loader_open_driver_lib(const char *driver_name,
 
    return driver;
 }
-
-/**
- * Opens a DRI driver using its driver name, returning the __DRIextension
- * entrypoints.
- *
- * \param driverName - a name like "i965", "radeon", "nouveau", etc.
- * \param out_driver - Address where the dlopen() return value will be stored.
- * \param search_path_vars - NULL-terminated list of env vars that can be used
- * to override the DEFAULT_DRIVER_DIR search path.
- */
-const struct __DRIextensionRec **
-loader_open_driver(const char *driver_name,
-                   void **out_driver_handle,
-                   const char **search_path_vars,
-                   bool driver_name_is_inferred)
-{
-   char *get_extensions_name;
-   const struct __DRIextensionRec **extensions = NULL;
-   const struct __DRIextensionRec **(*get_extensions)(void);
-
-   void *driver = loader_open_driver_lib("libgallium", "", search_path_vars,
-                                         DEFAULT_DRIVER_DIR, !driver_name_is_inferred);
-
-   if (!driver)
-      goto failed;
-
-   get_extensions_name = loader_get_extensions_name(driver_name);
-   if (get_extensions_name) {
-      get_extensions = dlsym(driver, get_extensions_name);
-      if (get_extensions) {
-         extensions = get_extensions();
-      } else {
-         log_(_LOADER_DEBUG, "MESA-LOADER: driver does not expose %s(): %s\n",
-              get_extensions_name, dlerror());
-      }
-      free(get_extensions_name);
-   }
-
-   if (extensions == NULL) {
-      log_(_LOADER_WARNING,
-           "MESA-LOADER: driver exports no extensions (%s)\n", dlerror());
-      dlclose(driver);
-      driver = NULL;
-   }
-
-failed:
-   *out_driver_handle = driver;
-   return extensions;
-}

src/loader/loader.h
@@ -69,12 +69,6 @@ loader_open_driver_lib(const char *driver_name,
                        const char *default_search_path,
                        bool warn_on_fail);
 
-const struct __DRIextensionRec **
-loader_open_driver(const char *driver_name,
-                   void **out_driver_handle,
-                   const char **search_path_vars,
-                   bool driver_name_is_inferred);
-
 char *
 loader_get_device_name_for_fd(int fd);
 

commit dfd70bab4ac2336be6c1085f195aef8e0e10dc13

loader: gc loader_get_extensions_name() and __DRI_DRIVER_{GET_,}EXTENSIONS defines
Leaving the defines in include/GL/internal/dri_interface.h because I'm
not sure if something needs it.

Fixes: fa541a887c8a67321362 ("loader: delete loader_open_driver()")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30266>

src/gallium/include/mesa_interface.h
@@ -669,25 +669,6 @@ struct __DRIuseInvalidateExtensionRec {
 };
 
 /**
- * Dead, do not use; kept only to allow old Xserver to compile since
- * this file is a public API.
- */
-#define __DRI_DRIVER_EXTENSIONS "__driDriverExtensions"
-
-/**
- * This symbol replaces the __DRI_DRIVER_EXTENSIONS symbol, and will be
- * suffixed by "_drivername", allowing multiple drivers to be built into one
- * library, and also giving the driver the chance to return a variable driver
- * extensions struct depending on the driver name being loaded or any other
- * system state.
- *
- * The function prototype is:
- *
- * const __DRIextension **__driDriverGetExtensions_drivername(void);
- */
-#define __DRI_DRIVER_GET_EXTENSIONS "__driDriverGetExtensions"
-
-/**
  * Tokens for __DRIconfig attribs.  A number of attributes defined by
  * GLX or EGL standards are not in the table, as they must be provided
  * by the loader.  For example, FBConfig ID or visual ID, drawable type.

src/loader/loader.c
@@ -732,23 +732,6 @@ loader_set_logger(loader_logger *logger)
    log_ = logger;
 }
 
-char *
-loader_get_extensions_name(const char *driver_name)
-{
-   char *name = NULL;
-
-   if (asprintf(&name, "%s_%s", __DRI_DRIVER_GET_EXTENSIONS, driver_name) < 0)
-      return NULL;
-
-   const size_t len = strlen(name);
-   for (size_t i = 0; i < len; i++) {
-      if (name[i] == '-')
-         name[i] = '_';
-   }
-
-   return name;
-}
-
 bool
 loader_bind_extensions(void *data,
                        const struct dri_extension_match *matches, size_t num_matches,

src/loader/loader.h
@@ -94,9 +94,6 @@ typedef void loader_logger(int level, const char *fmt, ...);
 void
 loader_set_logger(loader_logger *logger);
 
-char *
-loader_get_extensions_name(const char *driver_name);
-
 struct dri_extension_match {
    /* __DRI_* extension name */
    const char *name;
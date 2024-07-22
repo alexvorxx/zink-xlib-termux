alexvorxx/zink-xlib-termux

commit 50fc7cc290db49288b318c116c6fdc3daaf801e9

glx: directly link to gallium
this eliminates the loader interface, which avoids an entire class
of issues and simplifies a bunch of code

Acked-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29771>

src/glx/dri_common.c
@@ -55,12 +55,11 @@
 #define GL_LIB_NAME "libGL.so.1"
 #endif
 
+const __DRIextension **
+dri_loader_get_extensions(const char *driver_name);
+
 /**
  * Try to \c dlopen the named driver.
- *
- * This function adds the "_dri.so" suffix to the driver name and searches the
- * directories specified by the \c LIBGL_DRIVERS_PATH environment variable in
- * order to find the driver.
  *
  * \param driverName - a name like "i965", "radeon", "nouveau", etc.
  * \param out_driver_handle - Address to return the resulting dlopen() handle.
@@ -70,21 +69,19 @@
  * file not found.
  */
 _X_HIDDEN const __DRIextension **
-driOpenDriver(const char *driverName, void **out_driver_handle, bool driver_name_is_inferred)
+driOpenDriver(const char *driverName, bool driver_name_is_inferred)
 {
    void *glhandle;
 
    /* Attempt to make sure libGL symbols will be visible to the driver */
    glhandle = dlopen(GL_LIB_NAME, RTLD_NOW | RTLD_GLOBAL);
 
-   static const char *search_path_vars[] = {
-      "LIBGL_DRIVERS_PATH",
-      "LIBGL_DRIVERS_DIR", /* deprecated */
-      NULL
-   };
+   const __DRIextension **extensions = dri_loader_get_extensions(driverName);
 
-   const __DRIextension **extensions =
-      loader_open_driver(driverName, out_driver_handle, search_path_vars, driver_name_is_inferred);
+   if (driver_name_is_inferred) {
+      glx_message(_LOADER_WARNING,
+           "MESA-LOADER: failed to open %s: driver not built!)\n", driverName);
+   }
 
    if (glhandle)
       dlclose(glhandle);
@@ -742,9 +739,8 @@ clear_driver_config_cache()
 static char *
 get_driver_config(const char *driverName)
 {
-   void *handle;
    char *config = NULL;
-   const __DRIextension **extensions = driOpenDriver(driverName, &handle, false);
+   const __DRIextension **extensions = driOpenDriver(driverName, false);
    if (extensions) {
       for (int i = 0; extensions[i]; i++) {
          if (strcmp(extensions[i]->name, __DRI_CONFIG_OPTIONS) != 0)
@@ -759,8 +755,6 @@ get_driver_config(const char *driverName)
          break;
       }
    }
-
-   dlclose(handle);
 
    return config;
 }

.gitlab-ci/common/init-stage2.sh
@@ -94,7 +94,7 @@ fi
 # Fix prefix confusion: the build installs to $CI_PROJECT_DIR, but we expect
 # it in /install
 ln -sf $CI_PROJECT_DIR/install /install
-export LD_LIBRARY_PATH=/install/lib
+export LD_LIBRARY_PATH=/install/lib:/install/lib/dri
 export LIBGL_DRIVERS_PATH=/install/lib/dri
 
 # https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22495#note_1876691
@@ -113,7 +113,7 @@ export PYTHONPATH=$(python3 -c "import sys;print(\":\".join(sys.path))")
 if [ -n "$MESA_LOADER_DRIVER_OVERRIDE" ]; then
   rm /install/lib/dri/!($MESA_LOADER_DRIVER_OVERRIDE)_dri.so
 fi
-ls -1 /install/lib/dri/*_dri.so || true
+ls -1 /install/lib/dri/*.so || true
 
 if [ "$HWCI_FREQ_MAX" = "true" ]; then
   # Ensure initialization of the DRM device (needed by MSM)

.gitlab-ci/common/start-x.sh
@@ -7,7 +7,7 @@ _FLAG_FILE="/xorg-started"
 
 echo "touch ${_FLAG_FILE}; sleep 100000" > "${_XORG_SCRIPT}"
 if [ "x$1" != "x" ]; then
-    export LD_LIBRARY_PATH="${1}/lib"
+    export LD_LIBRARY_PATH="${1}/lib:${1}/lib/dri"
     export LIBGL_DRIVERS_PATH="${1}/lib/dri"
 fi
 xinit /bin/sh "${_XORG_SCRIPT}" -- /usr/bin/Xorg vt45 -noreset -s 0 -dpms -logfile /Xorg.0.log &

.gitlab-ci/cuttlefish-runner.sh
@@ -6,7 +6,7 @@ set -xe
 
 export HOME=/cuttlefish
 export PATH=$PATH:/cuttlefish/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${CI_PROJECT_DIR}/install/lib/:/cuttlefish/lib64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${CI_PROJECT_DIR}/install/lib/:${CI_PROJECT_DIR}/install/lib/dri:/cuttlefish/lib64
 export EGL_PLATFORM=surfaceless
 
 syslogd

.gitlab-ci/deqp-runner.sh
@@ -16,7 +16,7 @@ fi
 INSTALL=$(realpath -s "$PWD"/install)
 
 # Set up the driver environment.
-export LD_LIBRARY_PATH="$INSTALL"/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="$INSTALL"/lib/:"$INSTALL"/lib/dri:$LD_LIBRARY_PATH
 export EGL_PLATFORM=surfaceless
 ARCH=$(uname -m)
 export VK_DRIVER_FILES="$PWD"/install/share/vulkan/icd.d/"$VK_DRIVER"_icd."$ARCH".json

.gitlab-ci/fossilize-runner.sh
@@ -10,7 +10,7 @@ fi
 INSTALL=$PWD/install
 
 # Set up the driver environment.
-export LD_LIBRARY_PATH="$INSTALL/lib/"
+export LD_LIBRARY_PATH="$INSTALL/lib/:$INSTALL/lib/dri"
 export VK_DRIVER_FILES="$INSTALL/share/vulkan/icd.d/${VK_DRIVER}_icd.x86_64.json"
 
 # To store Fossilize logs on failure.

.gitlab-ci/gtest-runner.sh
@@ -6,7 +6,7 @@ set -ex
 INSTALL=$PWD/install
 
 # Set up the driver environment.
-export LD_LIBRARY_PATH=$INSTALL/lib/
+export LD_LIBRARY_PATH=$INSTALL/lib/:$INSTALL/lib/dri
 
 RESULTS="$PWD/${GTEST_RESULTS_DIR:-results}"
 mkdir -p "$RESULTS"

.gitlab-ci/piglit/piglit-runner.sh
@@ -11,7 +11,7 @@ fi
 INSTALL="$PWD/install"
 
 # Set up the driver environment.
-export LD_LIBRARY_PATH="$INSTALL/lib/"
+export LD_LIBRARY_PATH="$INSTALL/lib/:$INSTALL/lib/dri"
 export EGL_PLATFORM=surfaceless
 ARCH=$(uname -m)
 export VK_DRIVER_FILES="$INSTALL/share/vulkan/icd.d/${VK_DRIVER}_icd.$ARCH.json"

commit 159a3edd80a988dec263708f851ed35eec881a78

egl,gbm,glx: fix log message spam
Based on the other similar logs we only want to log when extensions
is NULL.
Use this opportunity to indicate the source of the log and remove
the extra ')' at the end of each line.

Fixes: 50fc7cc290d ("glx: directly link to gallium")
Reviewed-by: Eric Engestrom <eric@igalia.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30257>

src/egl/drivers/dri2/egl_dri2.c
@@ -666,7 +666,7 @@ dri2_load_driver_common(_EGLDisplay *disp,
    extensions = dri2_open_driver(disp);
    if (!extensions) {
       if (disp->Options.FallbackZink)
-         _eglLog(_LOADER_WARNING, "MESA-LOADER: failed to open %s: driver not built!)\n", dri2_dpy->driver_name);
+         _eglLog(_LOADER_WARNING, "MESA-LOADER: egl: failed to open %s: driver not built!\n", dri2_dpy->driver_name);
       return EGL_FALSE;
    }
 

src/gbm/backends/dri/gbm_dri.c
@@ -275,7 +275,7 @@ dri_screen_create_for_driver(struct gbm_dri_device *dri, char *driver_name, bool
    const __DRIextension **extensions = dri_open_driver(dri);
    if (!extensions) {
       if (driver_name_is_inferred)
-         fprintf(stderr, "MESA-LOADER: failed to open %s: driver not built!)\n", dri->driver_name);
+         fprintf(stderr, "MESA-LOADER: gbm: failed to open %s: driver not built!\n", dri->driver_name);
       goto fail;
    }
 

src/glx/dri_common.c
@@ -78,9 +78,9 @@ driOpenDriver(const char *driverName, bool driver_name_is_inferred)
 
    const __DRIextension **extensions = dri_loader_get_extensions(driverName);
 
-   if (driver_name_is_inferred) {
+   if (!extensions && driver_name_is_inferred) {
       glx_message(_LOADER_WARNING,
-           "MESA-LOADER: failed to open %s: driver not built!)\n", driverName);
+           "MESA-LOADER: glx: failed to open %s: driver not built!\n", driverName);
    }
 
    if (glhandle)
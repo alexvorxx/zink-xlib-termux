alexvorxx/zink-xlib-termux

commit c4b30b604fda53dfaed83bcbe1467ac1023b2c90

venus: support VK_ANDROID_NATIVE_BUFFER_SPEC_VERSION 8
The aliased WSI image creation path is very much like the AHB case. It's
also the same with the deferred ANB binding path to support
VK_EXT_swapchain_maintenance1. Drop the cap as mesa currently only
supports up to spec version 8 because the later versions haven't been
upstreamed to the Vulkan registry yet.

Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29864>

src/virtio/vulkan/vn_image.c
@@ -680,8 +680,12 @@ vn_CreateImage(VkDevice device,
    } else if (ahb_info) {
       result = vn_image_create_deferred(dev, pCreateInfo, alloc, &img);
    } else if (swapchain_info) {
+#if DETECT_OS_ANDROID
+      result = vn_image_create_deferred(dev, pCreateInfo, alloc, &img);
+#else
       result = vn_wsi_create_image_from_swapchain(
          dev, pCreateInfo, swapchain_info, alloc, &img);
+#endif
    } else {
       struct vn_image_create_info local_info;
       if (external_info &&
@@ -781,10 +785,11 @@ vn_image_bind_wsi_memory(struct vn_device *dev,
 
       if (!mem) {
 #if DETECT_OS_ANDROID
-         /* TODO handle VkNativeBufferANDROID when we bump up
-          * VN_ANDROID_NATIVE_BUFFER_SPEC_VERSION
-          */
-         unreachable("VkBindImageMemoryInfo with no memory");
+         mem = vn_android_get_wsi_memory_from_bind_info(dev, info);
+         if (!mem) {
+            STACK_ARRAY_FINISH(local_infos);
+            return VK_ERROR_OUT_OF_HOST_MEMORY;
+         }
 #else
          const VkBindImageMemorySwapchainInfoKHR *swapchain_info =
             vk_find_struct_const(info->pNext,
@@ -795,8 +800,8 @@ vn_image_bind_wsi_memory(struct vn_device *dev,
             vn_image_from_handle(wsi_common_get_image(
                swapchain_info->swapchain, swapchain_info->imageIndex));
          mem = swapchain_img->wsi.memory;
-         info->memory = vn_device_memory_to_handle(mem);
 #endif
+         info->memory = vn_device_memory_to_handle(mem);
       }
       assert(mem && info->memory != VK_NULL_HANDLE);
 

src/virtio/vulkan/vn_android.c
@@ -523,10 +523,20 @@ vn_android_image_from_anb_internal(struct vn_device *dev,
       return result;
 
    /* encoder will strip the Android specific pNext structs */
-   result = vn_image_create(dev, &builder.create, alloc, &img);
-   if (result != VK_SUCCESS) {
-      vn_log(dev->instance, "anb: vn_image_create failed");
-      return result;
+   if (*out_img) {
+      /* driver side img obj has been created for deferred init like ahb */
+      img = *out_img;
+      result = vn_image_init_deferred(dev, &builder.create, img);
+      if (result != VK_SUCCESS) {
+         vn_log(dev->instance, "anb: vn_image_init_deferred failed");
+         return result;
+      }
+   } else {
+      result = vn_image_create(dev, &builder.create, alloc, &img);
+      if (result != VK_SUCCESS) {
+         vn_log(dev->instance, "anb: vn_image_create failed");
+         return result;
+      }
    }
 
    img->wsi.is_wsi = true;
@@ -632,6 +642,25 @@ vn_android_image_from_anb(struct vn_device *dev,
 
    *out_img = img;
    return VK_SUCCESS;
+}
+
+struct vn_device_memory *
+vn_android_get_wsi_memory_from_bind_info(
+   struct vn_device *dev, const VkBindImageMemoryInfo *bind_info)
+{
+   const VkNativeBufferANDROID *anb_info =
+      vk_find_struct_const(bind_info->pNext, NATIVE_BUFFER_ANDROID);
+   assert(anb_info && anb_info->handle);
+
+   struct vn_image *img = vn_image_from_handle(bind_info->image);
+   VkResult result = vn_android_image_from_anb_internal(
+      dev, &img->deferred_info->create, anb_info, &dev->base.base.alloc,
+      &img);
+   if (result != VK_SUCCESS)
+      return NULL;
+
+   assert(img->wsi.memory_owned);
+   return img->wsi.memory;
 }
 
 static VkResult

src/virtio/vulkan/vn_android.h
@@ -17,9 +17,6 @@
 #include <vulkan/vulkan.h>
 #include <vulkan/vulkan_android.h>
 
-/* venus implements VK_ANDROID_native_buffer up to spec version 7 */
-#define VN_ANDROID_NATIVE_BUFFER_SPEC_VERSION 7
-
 #if DETECT_OS_ANDROID
 
 VkResult
@@ -28,6 +25,10 @@ vn_android_image_from_anb(struct vn_device *dev,
                           const VkNativeBufferANDROID *anb_info,
                           const VkAllocationCallbacks *alloc,
                           struct vn_image **out_img);
+
+struct vn_device_memory *
+vn_android_get_wsi_memory_from_bind_info(
+   struct vn_device *dev, const VkBindImageMemoryInfo *bind_info);
 
 bool
 vn_android_get_drm_format_modifier_info(
@@ -66,6 +67,14 @@ vn_android_image_from_anb(UNUSED struct vn_device *dev,
                           UNUSED struct vn_image **out_img)
 {
    return VK_ERROR_OUT_OF_HOST_MEMORY;
+}
+
+static inline struct vn_device_memory *
+vn_android_get_wsi_memory_from_bind_info(
+   UNUSED struct vn_device *dev,
+   UNUSED const VkBindImageMemoryInfo *bind_info)
+{
+   return NULL;
 }
 
 static inline bool

src/virtio/vulkan/vn_physical_device.c
@@ -1169,14 +1169,6 @@ vn_physical_device_init_supported_extensions(
             physical_dev->extension_spec_versions[i], props->specVersion);
       }
    }
-
-   /* override VK_ANDROID_native_buffer spec version */
-   if (native.ANDROID_native_buffer) {
-      const uint32_t index =
-         VN_EXTENSION_TABLE_INDEX(native, ANDROID_native_buffer);
-      physical_dev->extension_spec_versions[index] =
-         VN_ANDROID_NATIVE_BUFFER_SPEC_VERSION;
-   }
 }
 
 static VkResult

commit a27e3c50789260d4651c4f4842afee8cdfebd568

venus: clarify wsi image ownership
Fix to call vn_image_bind_wsi_memory as long as the image is a wsi
image. This is needed so that we track the wsi memory in the wsi image
so that creating from swapchain info works normally on x11/wayland
platforms. This change also make it clear that ANB image owns the wsi
memory

Fixes: c4b30b604fd ("venus: support VK_ANDROID_NATIVE_BUFFER_SPEC_VERSION 8")
Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30278>

src/virtio/vulkan/vn_image.c
@@ -431,6 +431,9 @@ vn_image_deferred_info_init(struct vn_image *img,
             info->from_external_format = true;
          }
       } break;
+      case VK_STRUCTURE_TYPE_IMAGE_SWAPCHAIN_CREATE_INFO_KHR:
+         img->wsi.is_wsi = true;
+         break;
       default:
          break;
       }
@@ -805,8 +808,12 @@ vn_image_bind_wsi_memory(struct vn_device *dev,
       }
       assert(mem && info->memory != VK_NULL_HANDLE);
 
-      if (img->wsi.is_wsi)
-         img->wsi.memory = mem;
+#if DETECT_OS_ANDROID
+      assert(img->wsi.memory);
+#else
+      assert(!img->wsi.memory);
+      img->wsi.memory = mem;
+#endif
    }
 
    vn_async_vkBindImageMemory2(dev->primary_ring, vn_device_to_handle(dev),
@@ -825,7 +832,8 @@ vn_BindImageMemory2(VkDevice device,
    struct vn_device *dev = vn_device_from_handle(device);
 
    for (uint32_t i = 0; i < bindInfoCount; i++) {
-      if (pBindInfos[i].memory == VK_NULL_HANDLE)
+      struct vn_image *img = vn_image_from_handle(pBindInfos[i].image);
+      if (img->wsi.is_wsi)
          return vn_image_bind_wsi_memory(dev, bindInfoCount, pBindInfos);
    }
 
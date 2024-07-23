alexvorxx/zink-xlib-termux

commit 50fc7cc290db49288b318c116c6fdc3daaf801e9

glx: directly link to gallium
this eliminates the loader interface, which avoids an entire class
of issues and simplifies a bunch of code

Acked-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29771>

src/gallium/targets/dri/dri_target.c
@@ -158,3 +158,196 @@ PUBLIC const __DRIextension **__driDriverGetExtensions_zink(void)
 #if defined(GALLIUM_D3D12)
 DEFINE_LOADER_DRM_ENTRYPOINT(d3d12);
 #endif
+
+const __DRIextension **
+dri_loader_get_extensions(const char *driver_name);
+
+PUBLIC const __DRIextension **
+dri_loader_get_extensions(const char *driver_name)
+{
+#if defined(HAVE_I915)
+   if (!strcmp(driver_name, "i915"))
+      return __driDriverGetExtensions_i915();
+#endif
+
+#if defined(HAVE_IRIS)
+   if (!strcmp(driver_name, "iris"))
+      return __driDriverGetExtensions_iris();
+#endif
+
+#if defined(HAVE_CROCUS)
+   if (!strcmp(driver_name, "crocus"))
+      return __driDriverGetExtensions_crocus();
+#endif
+
+#if defined(HAVE_NOUVEAU)
+   if (!strcmp(driver_name, "nouveau"))
+      return __driDriverGetExtensions_nouveau();
+#endif
+
+#if defined(HAVE_R300)
+   if (!strcmp(driver_name, "r300"))
+      return __driDriverGetExtensions_r300();
+#endif
+
+#if defined(HAVE_R600)
+   if (!strcmp(driver_name, "r600"))
+      return __driDriverGetExtensions_r600();
+#endif
+
+#if defined(HAVE_RADEONSI)
+   if (!strcmp(driver_name, "radeonsi"))
+      return __driDriverGetExtensions_radeonsi();
+#endif
+
+#if defined(HAVE_VMWGFX)
+   if (!strcmp(driver_name, "vmwgfx"))
+      return __driDriverGetExtensions_vmwgfx();
+#endif
+
+#if defined(HAVE_FREEDRENO)
+   if (!strcmp(driver_name, "msm"))
+      return __driDriverGetExtensions_msm();
+   if (!strcmp(driver_name, "kgsl"))
+      return __driDriverGetExtensions_kgsl();
+#endif
+
+#if defined(HAVE_VIRGL) || (defined(HAVE_FREEDRENO) && !defined(PIPE_LOADER_DYNAMIC))
+   if (!strcmp(driver_name, "virtio_gpu"))
+      return __driDriverGetExtensions_virtio_gpu();
+#endif
+
+#if defined(HAVE_V3D)
+   if (!strcmp(driver_name, "v3d"))
+      return __driDriverGetExtensions_v3d();
+#endif
+
+#if defined(HAVE_VC4)
+   if (!strcmp(driver_name, "vc4"))
+      return __driDriverGetExtensions_vc4();
+#endif
+
+#if defined(HAVE_PANFROST)
+   if (!strcmp(driver_name, "panfrost"))
+      return __driDriverGetExtensions_panfrost();
+   if (!strcmp(driver_name, "panthor"))
+      return __driDriverGetExtensions_panthor();
+#endif
+
+#if defined(HAVE_ASAHI)
+   if (!strcmp(driver_name, "asahi"))
+      return __driDriverGetExtensions_asahi();
+#endif
+
+#if defined(HAVE_ETNAVIV)
+   if (!strcmp(driver_name, "etnaviv"))
+      return __driDriverGetExtensions_etnaviv();
+#endif
+
+#if defined(HAVE_TEGRA)
+   if (!strcmp(driver_name, "tegra"))
+      return __driDriverGetExtensions_tegra();
+#endif
+
+#if defined(HAVE_KMSRO)
+   if (!strcmp(driver_name, "armada_drm"))
+      return __driDriverGetExtensions_armada_drm();
+   if (!strcmp(driver_name, "exynos"))
+      return __driDriverGetExtensions_exynos();
+   if (!strcmp(driver_name, "gm12u320"))
+      return __driDriverGetExtensions_gm12u320();
+   if (!strcmp(driver_name, "hdlcd"))
+      return __driDriverGetExtensions_hdlcd();
+   if (!strcmp(driver_name, "hx8357d"))
+      return __driDriverGetExtensions_hx8357d();
+   if (!strcmp(driver_name, "ili9163"))
+      return __driDriverGetExtensions_ili9163();
+   if (!strcmp(driver_name, "ili9225"))
+      return __driDriverGetExtensions_ili9225();
+   if (!strcmp(driver_name, "ili9341"))
+      return __driDriverGetExtensions_ili9341();
+   if (!strcmp(driver_name, "ili9486"))
+      return __driDriverGetExtensions_ili9486();
+   if (!strcmp(driver_name, "imx_drm"))
+      return __driDriverGetExtensions_imx_drm();
+   if (!strcmp(driver_name, "imx_dcss"))
+      return __driDriverGetExtensions_imx_dcss();
+   if (!strcmp(driver_name, "imx_lcdif"))
+      return __driDriverGetExtensions_imx_lcdif();
+   if (!strcmp(driver_name, "ingenic_drm"))
+      return __driDriverGetExtensions_ingenic_drm();
+   if (!strcmp(driver_name, "kirin"))
+      return __driDriverGetExtensions_kirin();
+   if (!strcmp(driver_name, "komeda"))
+      return __driDriverGetExtensions_komeda();
+   if (!strcmp(driver_name, "mali_dp"))
+      return __driDriverGetExtensions_mali_dp();
+   if (!strcmp(driver_name, "mcde"))
+      return __driDriverGetExtensions_mcde();
+   if (!strcmp(driver_name, "mediatek"))
+      return __driDriverGetExtensions_mediatek();
+   if (!strcmp(driver_name, "meson"))
+      return __driDriverGetExtensions_meson();
+   if (!strcmp(driver_name, "mi0283qt"))
+      return __driDriverGetExtensions_mi0283qt();
+   if (!strcmp(driver_name, "mxsfb_drm"))
+      return __driDriverGetExtensions_mxsfb_drm();
+   if (!strcmp(driver_name, "panel_mipi_dbi"))
+      return __driDriverGetExtensions_panel_mipi_dbi();
+   if (!strcmp(driver_name, "pl111"))
+      return __driDriverGetExtensions_pl111();
+   if (!strcmp(driver_name, "rcar_du"))
+      return __driDriverGetExtensions_rcar_du();
+   if (!strcmp(driver_name, "repaper"))
+      return __driDriverGetExtensions_repaper();
+   if (!strcmp(driver_name, "rockchip"))
+      return __driDriverGetExtensions_rockchip();
+   if (!strcmp(driver_name, "rzg2l_du"))
+      return __driDriverGetExtensions_rzg2l_du();
+   if (!strcmp(driver_name, "ssd130x"))
+      return __driDriverGetExtensions_ssd130x();
+   if (!strcmp(driver_name, "st7586"))
+      return __driDriverGetExtensions_st7586();
+   if (!strcmp(driver_name, "st7735r"))
+      return __driDriverGetExtensions_st7735r();
+   if (!strcmp(driver_name, "sti"))
+      return __driDriverGetExtensions_sti();
+   if (!strcmp(driver_name, "stm"))
+      return __driDriverGetExtensions_stm();
+   if (!strcmp(driver_name, "sun4i_drm"))
+      return __driDriverGetExtensions_sun4i_drm();
+   if (!strcmp(driver_name, "udl"))
+      return __driDriverGetExtensions_udl();
+   if (!strcmp(driver_name, "vkms"))
+      return __driDriverGetExtensions_vkms();
+   if (!strcmp(driver_name, "zynqmp_dpsub"))
+      return __driDriverGetExtensions_zynqmp_dpsub();
+#endif
+
+#if defined(HAVE_LIMA)
+   if (!strcmp(driver_name, "lima"))
+      return __driDriverGetExtensions_lima();
+#endif
+
+#if defined(HAVE_ZINK)
+   if (!strcmp(driver_name, "zink"))
+      return __driDriverGetExtensions_zink();
+#endif
+
+#if defined(HAVE_D3D12)
+   if (!strcmp(driver_name, "d3d12"))
+      return __driDriverGetExtensions_d3d12();
+#endif
+
+#if defined(HAVE_SWRAST)
+   if (!strcmp(driver_name, "swrast"))
+      return __driDriverGetExtensions_swrast();
+
+#if defined(HAVE_LIBDRM)
+   if (!strcmp(driver_name, "kms_swrast"))
+      return __driDriverGetExtensions_kms_swrast();
+#endif
+#endif
+
+   return NULL;
+}

.gitlab-ci/common/init-stage2.sh
@@ -94,7 +94,7 @@ fi
 # Fix prefix confusion: the build installs to $CI_PROJECT_DIR, but we expect
 # it in /install
 ln -sf $CI_PROJECT_DIR/install /install
-export LD_LIBRARY_PATH=/install/lib
+export LD_LIBRARY_PATH=/install/lib:/install/lib/dri
 export LIBGL_DRIVERS_PATH=/install/lib/dri
 
 # https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22495#note_1876691
@@ -113,7 +113,7 @@ export PYTHONPATH=$(python3 -c "import sys;print(\":\".join(sys.path))")
 if [ -n "$MESA_LOADER_DRIVER_OVERRIDE" ]; then
   rm /install/lib/dri/!($MESA_LOADER_DRIVER_OVERRIDE)_dri.so
 fi
-ls -1 /install/lib/dri/*_dri.so || true
+ls -1 /install/lib/dri/*.so || true
 
 if [ "$HWCI_FREQ_MAX" = "true" ]; then
   # Ensure initialization of the DRM device (needed by MSM)

.gitlab-ci/common/start-x.sh
@@ -7,7 +7,7 @@ _FLAG_FILE="/xorg-started"
 
 echo "touch ${_FLAG_FILE}; sleep 100000" > "${_XORG_SCRIPT}"
 if [ "x$1" != "x" ]; then
-    export LD_LIBRARY_PATH="${1}/lib"
+    export LD_LIBRARY_PATH="${1}/lib:${1}/lib/dri"
     export LIBGL_DRIVERS_PATH="${1}/lib/dri"
 fi
 xinit /bin/sh "${_XORG_SCRIPT}" -- /usr/bin/Xorg vt45 -noreset -s 0 -dpms -logfile /Xorg.0.log &

.gitlab-ci/cuttlefish-runner.sh
@@ -6,7 +6,7 @@ set -xe
 
 export HOME=/cuttlefish
 export PATH=$PATH:/cuttlefish/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${CI_PROJECT_DIR}/install/lib/:/cuttlefish/lib64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${CI_PROJECT_DIR}/install/lib/:${CI_PROJECT_DIR}/install/lib/dri:/cuttlefish/lib64
 export EGL_PLATFORM=surfaceless
 
 syslogd

.gitlab-ci/deqp-runner.sh
@@ -16,7 +16,7 @@ fi
 INSTALL=$(realpath -s "$PWD"/install)
 
 # Set up the driver environment.
-export LD_LIBRARY_PATH="$INSTALL"/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="$INSTALL"/lib/:"$INSTALL"/lib/dri:$LD_LIBRARY_PATH
 export EGL_PLATFORM=surfaceless
 ARCH=$(uname -m)
 export VK_DRIVER_FILES="$PWD"/install/share/vulkan/icd.d/"$VK_DRIVER"_icd."$ARCH".json

.gitlab-ci/fossilize-runner.sh
@@ -10,7 +10,7 @@ fi
 INSTALL=$PWD/install
 
 # Set up the driver environment.
-export LD_LIBRARY_PATH="$INSTALL/lib/"
+export LD_LIBRARY_PATH="$INSTALL/lib/:$INSTALL/lib/dri"
 export VK_DRIVER_FILES="$INSTALL/share/vulkan/icd.d/${VK_DRIVER}_icd.x86_64.json"
 
 # To store Fossilize logs on failure.

.gitlab-ci/gtest-runner.sh
@@ -6,7 +6,7 @@ set -ex
 INSTALL=$PWD/install
 
 # Set up the driver environment.
-export LD_LIBRARY_PATH=$INSTALL/lib/
+export LD_LIBRARY_PATH=$INSTALL/lib/:$INSTALL/lib/dri
 
 RESULTS="$PWD/${GTEST_RESULTS_DIR:-results}"
 mkdir -p "$RESULTS"

.gitlab-ci/piglit/piglit-runner.sh
@@ -11,7 +11,7 @@ fi
 INSTALL="$PWD/install"
 
 # Set up the driver environment.
-export LD_LIBRARY_PATH="$INSTALL/lib/"
+export LD_LIBRARY_PATH="$INSTALL/lib/:$INSTALL/lib/dri"
 export EGL_PLATFORM=surfaceless
 ARCH=$(uname -m)
 export VK_DRIVER_FILES="$INSTALL/share/vulkan/icd.d/${VK_DRIVER}_icd.$ARCH.json"

commit 8f9a157daa30a27165b916f5e6fd0ff5b39e5348

dri: fix macro name check to detect svga driver
svga driver is detected via HAVE_SVGA.

Since commit 50fc7cc290d, svga driver was not loading at all

Fixes: 50fc7cc290d ("glx: directly link to gallium")

Acked-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
Reviewed-by: Zack Rusin <zack.rusin@broadcom.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30307>

src/gallium/targets/dri/dri_target.c
@@ -200,7 +200,7 @@ dri_loader_get_extensions(const char *driver_name)
       return __driDriverGetExtensions_radeonsi();
 #endif
 
-#if defined(HAVE_VMWGFX)
+#if defined(HAVE_SVGA)
    if (!strcmp(driver_name, "vmwgfx"))
       return __driDriverGetExtensions_vmwgfx();
 #endif